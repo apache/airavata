5d24957c655f18b587b284fff5de66026395062a
Suresh Marru <smarru@apache.org>
2011-08-05 12:22:03 +0000

    validating and applied contribution from Patanachai to address AIRAVATA-70
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1154185 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/commons/gfac-schema/src/main/java/org/apache/airavata/core/gfac/type/app/ShellApplicationDeployment.java
@@ -0,0 +1,148 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.core.gfac.type.app;
+
+import java.util.Map;
+
+import org.apache.airavata.core.gfac.type.ApplicationDeploymentDescription;
+
+public class ShellApplicationDeployment extends ApplicationDeploymentDescription {    
+    private String executable;
+    private String stdOut;
+    private String stdErr;
+    private String stdIn;
+    private Map<String, String> env;
+    
+    public String getExecutable() {
+        return executable;
+    }
+
+    public void setExecutable(String executable) {
+        this.executable = executable;
+    }
+
+    public Map<String, String> getEnv() {
+        return env;
+    }
+
+    public void setEnv(Map<String, String> env) {
+        this.env = env;
+    }
+
+    public String getStdOut() {
+        return stdOut;
+    }
+
+    public void setStdOut(String stdOut) {
+        this.stdOut = stdOut;
+    }
+
+    public String getStdErr() {
+        return stdErr;
+    }
+
+    public void setStdErr(String stderr) {
+        this.stdErr = stderr;
+    }
+
+    public String getStdIn() {
+        return stdIn;
+    }
+
+    public void setStdIn(String stdIn) {
+        this.stdIn = stdIn;
+    }    
+}
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.core.gfac.type.app;
+
+import java.util.Map;
+
+import org.apache.airavata.core.gfac.type.ApplicationDeploymentDescription;
+
+public class ShellApplicationDeployment extends ApplicationDeploymentDescription {    
+    private String executable;
+    private String stdOut;
+    private String stdErr;
+    private String stdIn;
+    private Map<String, String> env;
+    
+    public String getExecutable() {
+        return executable;
+    }
+
+    public void setExecutable(String executable) {
+        this.executable = executable;
+    }
+
+    public Map<String, String> getEnv() {
+        return env;
+    }
+
+    public void setEnv(Map<String, String> env) {
+        this.env = env;
+    }
+
+    public String getStdOut() {
+        return stdOut;
+    }
+
+    public void setStdOut(String stdOut) {
+        this.stdOut = stdOut;
+    }
+
+    public String getStdErr() {
+        return stdErr;
+    }
+
+    public void setStdErr(String stderr) {
+        this.stdErr = stderr;
+    }
+
+    public String getStdIn() {
+        return stdIn;
+    }
+
+    public void setStdIn(String stdIn) {
+        this.stdIn = stdIn;
+    }    
+}
