3c82e229338190f14c0df16c3dcf837b54cb1ef3
Suresh Marru <smarru@apache.org>
2011-08-25 20:44:05 +0000

    Adding the license header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1161746 13f79535-47bb-0310-9956-ffa450edef68

--- samples/simple_math_service/src/main/java/org/apache/airavata/samples/SimpleMathService.java
+++ samples/simple_math_service/src/main/java/org/apache/airavata/samples/SimpleMathService.java
@@ -1,14 +1,35 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package org.apache.airavata.samples;
 
 public class SimpleMathService {
-	/**
-	 * 
-	 * @param x
-	 * @param y
-	 * @return x plus y
-	 */
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return x plus y
+     */
     public int add(int x, int y) {
-    	return x + y;
+        return x + y;
     }
 
     /**
@@ -18,9 +39,9 @@ public class SimpleMathService {
      * @return x minus y
      */
     public int subtract(int x, int y) {
-    	return x + y;
+        return x + y;
     }
-    
+
     /**
      * 
      * @param x
@@ -28,9 +49,9 @@ public class SimpleMathService {
      * @return x multiply y
      */
     public int multiply(int x, int y) {
-    	return x * y;
+        return x * y;
     }
-    
+
     /**
      * 
      * @param x
@@ -38,13 +59,13 @@ public class SimpleMathService {
      * @return a string array with size contains x
      */
     public String[] stringArrayGenerate(String x, int size) {
-    	String[] result = new String[size];
-    	for (int i = 0; i < result.length; i++) {
-			result[i] = x;
-		}
-    	return result;
+        String[] result = new String[size];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = x;
+        }
+        return result;
     }
-    
+
     /**
      * 
      * @param x
@@ -52,10 +73,10 @@ public class SimpleMathService {
      * @return an integer array with size contains x
      */
     public int[] intArrayGenerate(int x, int size) {
-    	int[] result = new int[size];
-    	for (int i = 0; i < result.length; i++) {
-			result[i] = x;
-		}
-    	return result;
+        int[] result = new int[size];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = x;
+        }
+        return result;
     }
 }
