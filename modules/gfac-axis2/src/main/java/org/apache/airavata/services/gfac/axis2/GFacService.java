a28d55e7327e53174a4598bb7f25cb94f6b32691
Lahiru Gunathilake <lahiru@apache.org>
2011-10-06 03:19:46 +0000

    code cleanup and fixing https://issues.apache.org/jira/browse/AIRAVATA-127.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1179491 13f79535-47bb-0310-9956-ffa450edef68

--- modules/gfac-axis2/src/main/java/org/apache/airavata/services/gfac/axis2/GFacService.java
+++ modules/gfac-axis2/src/main/java/org/apache/airavata/services/gfac/axis2/GFacService.java
@@ -22,18 +22,16 @@
 package org.apache.airavata.services.gfac.axis2;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 import javax.jcr.Credentials;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryFactory;
 import javax.jcr.SimpleCredentials;
 import javax.security.auth.login.Configuration;
+import javax.servlet.ServletContext;
 
 import org.apache.airavata.core.gfac.services.GenericService;
 import org.apache.airavata.registry.api.Registry;
@@ -43,15 +41,18 @@ import org.apache.airavata.services.gfac.axis2.handlers.MyProxySecurityHandler;
 import org.apache.airavata.services.gfac.axis2.util.WSConstants;
 import org.apache.axis2.addressing.EndpointReference;
 import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.deployment.WarBasedAxisConfigurator;
 import org.apache.axis2.description.AxisService;
+import org.apache.axis2.description.TransportInDescription;
 import org.apache.axis2.engine.AxisConfiguration;
+import org.apache.axis2.engine.AxisConfigurator;
 import org.apache.axis2.engine.Phase;
 import org.apache.axis2.engine.ServiceLifeCycle;
 import org.apache.axis2.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GFacService extends Thread implements ServiceLifeCycle {
+public class GFacService implements ServiceLifeCycle {
     
     private static final Logger log = LoggerFactory.getLogger(GFacService.class);
 
@@ -70,6 +71,7 @@ public class GFacService extends Thread implements ServiceLifeCycle {
     public static GenericService service;
     public static final int GFAC_URL_UPDATE_INTERVAL = 1000 * 60 * 60 * 3;
     public ConfigurationContext context;
+    public GFacThread thread;
     public static final String GFAC_URL = "GFacURL";
 
     public void startUp(ConfigurationContext configctx, AxisService service){
@@ -94,6 +96,7 @@ public class GFacService extends Thread implements ServiceLifeCycle {
 
     private void initializeRepository(ConfigurationContext context) {
         Properties properties = new Properties();
+        String port = null;
         try {
             URL url = this.getClass().getClassLoader().getResource(REPOSITORY_PROPERTIES);
             properties.load(url.openStream());
@@ -106,14 +109,20 @@ public class GFacService extends Thread implements ServiceLifeCycle {
 
             Registry registry = new JCRRegistry(repository, credentials);
             String localAddress = Utils.getIpAddress(context.getAxisConfiguration());
-            String port = (String) context.getAxisConfiguration().getTransportsIn().get("http").getParameter("port").getValue();
+            TransportInDescription transportInDescription = context.getAxisConfiguration().getTransportsIn().get("http");
+            if(transportInDescription != null){
+                port = (String)transportInDescription.getParameter("port").getValue();
+            }else{
+                port = getTomcatPort(context);
+            }
             localAddress = "http://" + localAddress + ":" + port;
             localAddress = localAddress + "/" +
                     context.getContextRoot() + "/" + context.getServicePath() + "/" + WSConstants.GFAC_SERVICE_NAME;
             System.out.println(localAddress);
             context.setProperty(CONFIGURATION_CONTEXT_REGISTRY, registry);
-            context.setProperty(GFAC_URL,localAddress);
-            this.start();
+            context.setProperty(GFAC_URL, localAddress);
+            thread = new GFacThread(context);
+            thread.start();
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
@@ -123,15 +132,62 @@ public class GFacService extends Thread implements ServiceLifeCycle {
         Registry registry = (JCRRegistry) configctx.getProperty(CONFIGURATION_CONTEXT_REGISTRY);
         String gfacURL = (String) configctx.getProperty(GFAC_URL);
         registry.deleteGFacDescriptor(gfacURL);
-            this.interrupt();
+            thread.interrupt();
         try {
-            this.join();
+            thread.join();
         } catch (InterruptedException e) {
             log.info("GFacURL update thread is interrupted");
         }
     }
 
-    public void run() {
+
+
+    private String getTomcatPort(ConfigurationContext context) {
+        String port = null;
+        try {
+            Object obj = getNextObject(getNextObject(getNextObject(context.getProperty("transport.http.servletContext").getClass(),"context"),"context"),"context");
+            Field field = obj.getClass().getSuperclass().getDeclaredField("listeners");
+            field.setAccessible(true);
+            ArrayList list = (ArrayList) field.get(obj);
+            for(int i=0;i<list.size();i++){
+                obj = list.get(i);
+                obj = getNextObject(obj,"connector");
+                field = obj.getClass().getDeclaredField("scheme");
+                field.setAccessible(true);
+                String scheme = (String)field.get(obj);
+                if("http".equals(scheme)){
+                    field = obj.getClass().getDeclaredField("port");
+                    field.setAccessible(true);
+                    port = Integer.toString(field.getInt(obj));
+                    break;
+                }
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return port;
+    }
+
+    private Object getNextObject(Object obj,String fieldName){
+        try {
+            Field field = obj.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field.get(obj);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+    class GFacThread extends Thread{
+        private ConfigurationContext context = null;
+
+        GFacThread(ConfigurationContext context){
+            this.context = context;
+        }
+        public void run() {
         try {
             while (true) {
                 Registry registry = (Registry) this.context.getProperty(CONFIGURATION_CONTEXT_REGISTRY);
@@ -143,5 +199,6 @@ public class GFacService extends Thread implements ServiceLifeCycle {
         } catch (InterruptedException e) {
             log.info("GFacURL update thread is interrupted");
         }
+    }
     }
 }
