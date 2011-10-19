abdfd862f14865047ea3a1831074bec5002d8e34
Lahiru Gunathilake <lahiru@apache.org>
2011-10-19 20:00:50 +0000

    https://issues.apache.org/jira/browse/AIRAVATA-148
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1186438 13f79535-47bb-0310-9956-ffa450edef68

--- modules/xbaya-gui/src/main/java/org/apache/airavata/xbaya/registrybrowser/JCRBrowserPanel.java
+++ modules/xbaya-gui/src/main/java/org/apache/airavata/xbaya/registrybrowser/JCRBrowserPanel.java
@@ -1,10 +1,17 @@
 package org.apache.airavata.xbaya.registrybrowser;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.border.EmptyBorder;
@@ -13,6 +20,7 @@ import javax.swing.tree.DefaultTreeModel;
 import org.apache.airavata.registry.api.Registry;
 import org.apache.airavata.xbaya.XBayaEngine;
 import org.apache.airavata.xbaya.component.registry.JCRComponentRegistry;
+import org.apache.airavata.xbaya.registrybrowser.nodes.AbstractAiravataTreeNode;
 import org.apache.airavata.xbaya.registrybrowser.nodes.AiravataTreeNodeFactory;
 import org.apache.airavata.xbaya.registrybrowser.nodes.RegistryTreeCellRenderer;
 
@@ -20,6 +28,7 @@ public class JCRBrowserPanel extends JPanel implements Observer{
 
 	private XBayaEngine engine;
 	private JTree tree;
+	private JPopupMenu popupMenu;
 	
 	/**
 	 * Create the dialog.
@@ -38,8 +47,37 @@ public class JCRBrowserPanel extends JPanel implements Observer{
 			this.add(scrollPane, BorderLayout.CENTER);
 			{
 				tree = new JTree(AiravataTreeNodeFactory.getTreeNode(getJCRRegistry()==null?"No registry specified":getJCRRegistry(),null));
+//				tree.addMouseListener(new MouseAdapter() {
+//					@Override
+//					public void mousePressed(MouseEvent e) {
+//						int selRow = tree.getRowForLocation(e.getX(), e.getY());
+//						
+//				         if(selRow != -1 && e.isPopupTrigger()) {
+//				        	 tree.setSelectionRow(selRow);
+//				        	 popupMenu.show((Component)e.getSource(),e.getX(), e.getY());
+//				         }
+//					}
+//				});
 				tree.setCellRenderer(new RegistryTreeCellRenderer());
 				scrollPane.setViewportView(tree);
+				
+				popupMenu = new JPopupMenu();
+				popupMenu.setLabel("");
+				addPopup(tree, popupMenu);
+				
+				JMenuItem mntmRefresh = new JMenuItem("Refresh");
+				mntmRefresh.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						Object o = tree.getLastSelectedPathComponent();
+						if (o instanceof AbstractAiravataTreeNode){
+							AbstractAiravataTreeNode node=((AbstractAiravataTreeNode)o);
+							node.refresh();
+//							((DefaultTreeModel)tree.getModel()).nodeChanged(node);
+							((DefaultTreeModel)tree.getModel()).reload(node);
+						}
+					}
+				});
+				popupMenu.add(mntmRefresh);
 			}
 		}
 	}
@@ -90,4 +128,25 @@ public class JCRBrowserPanel extends JPanel implements Observer{
 		tree.setModel(new DefaultTreeModel(AiravataTreeNodeFactory.getTreeNode(getJCRRegistry()==null?"No registry specified":getJCRRegistry(),null)));
 	}
 	
+	private void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				int selRow = tree.getRowForLocation(e.getX(), e.getY());
+				if(selRow != -1 && e.isPopupTrigger()) {
+					tree.setSelectionRow(selRow);
+					popup.show(e.getComponent(), e.getX(), e.getY());
+				}
+			}
+		});
+	}
 }
