bf096c499c21ebaa8262b3c6bb1bdc4aaa9d1e02
Lahiru Gunathilake <lahiru@apache.org>
2011-10-20 22:27:32 +0000

    Applying the patch (patch-to-fix-build-break.difffor the issue AIRAVATA-105 Thanks Heshan for the contribution
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1187104 13f79535-47bb-0310-9956-ffa450edef68

--- modules/commons/gfac-schema/src/main/java/org/apache/airavata/commons/gfac/type/Parameter.java
+++ modules/commons/gfac-schema/src/main/java/org/apache/airavata/commons/gfac/type/Parameter.java
@@ -21,35 +21,33 @@
 
 package org.apache.airavata.commons.gfac.type;
 
-import org.apache.airavata.schemas.gfac.ParameterType;
-
 public class Parameter implements Type {
-    private ParameterType parameterType;
+    private org.apache.airavata.schemas.gfac.Parameter parameterType;
     private DataType type;
 
     public Parameter() {
-        this.parameterType = ParameterType.Factory.newInstance();
+        this.parameterType = org.apache.airavata.schemas.gfac.Parameter.Factory.newInstance();
     }
 
-    public Parameter(ParameterType pt) {
+    public Parameter(org.apache.airavata.schemas.gfac.Parameter pt) {
         this.parameterType = pt;
     }
 
-//    public String getName() {
-//        return parameterType.getName();
-//    }
-//
-//    public void setName(String name) {
-//        this.parameterType.setName(name);
-//    }
-//
-//    public String getDescription() {
-//        return parameterType.getDescription();
-//    }
-//
-//    public void setDescription(String description) {
-//        this.parameterType.setDescription(description);
-//    }
+    public String getName() {
+        return parameterType.getName();
+    }
+
+    public void setName(String name) {
+        this.parameterType.setName(name);
+    }
+
+    public String getDescription() {
+        return parameterType.getDescription();
+    }
+
+    public void setDescription(String description) {
+        this.parameterType.setDescription(description);
+    }
 
     // TODO
     public DataType getType() {
