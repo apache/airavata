1bf848de5f308722e0d2025b0352780bc4a4726d
Patanachai Tangchaisin <patanachai@apache.org>
2011-10-25 03:27:21 +0000

    AIRAVATA-138 service test case for JCR
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1188504 13f79535-47bb-0310-9956-ffa450edef68

--- modules/commons/registry-api/src/test/java/org/apache/airavata/registry/api/impl/JCRRegistryTest.java
+++ modules/commons/registry-api/src/test/java/org/apache/airavata/registry/api/impl/JCRRegistryTest.java
@@ -23,7 +23,14 @@ package org.apache.airavata.registry.api.impl;
 
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.airavata.commons.gfac.type.HostDescription;
+import org.apache.airavata.commons.gfac.type.ServiceDescription;
+import org.apache.airavata.commons.gfac.type.host.GlobusHost;
+import org.apache.airavata.schemas.gfac.DataType;
+import org.apache.airavata.schemas.gfac.Parameter;
 import org.junit.Test;
 
 public class JCRRegistryTest {
@@ -31,34 +38,113 @@ public class JCRRegistryTest {
     @Test
     public void testSaveLoadHostDescription() {
         try {
-        	/*
+            /*
              * Create database
              */
-            JCRRegistry jcrRegistry = new JCRRegistry(null, "org.apache.jackrabbit.core.RepositoryFactoryImpl", "admin",
-                    "admin", null);
+            JCRRegistry jcrRegistry = new JCRRegistry(null, "org.apache.jackrabbit.core.RepositoryFactoryImpl",
+                    "admin", "admin", null);
 
             String hostId = "localhost";
             String address = "127.0.0.1";
             
+            String hostId2 = "localhost2";
+
             /*
              * Host
              */
             HostDescription host = new HostDescription();
             host.setId(hostId);
             host.setAddress(address);
-            
+
             jcrRegistry.saveHostDescription(host);
-            
-            
+
             HostDescription hostR = jcrRegistry.getHostDescription(hostId);
-            
-            if(!(hostR.getId().equals(hostId) && hostR.getAddress().equals(address))){
-            	fail("Save and Load Host Description Fail with Different Value");	
+
+            if (!(hostR.getId().equals(hostId) && hostR.getAddress().equals(address))) {
+                fail("Save and Load Host Description Fail with Different Value");
             }
-          
-        }catch(Exception e){
+            
+            /*
+             * Test for polymorphism
+             */
+            GlobusHost globus = new GlobusHost();
+            globus.setId(hostId2);
+            globus.setAddress(address);
+
+            jcrRegistry.saveHostDescription(globus);
+
+            HostDescription hg = jcrRegistry.getHostDescription(hostId2);
+
+            if (!(hg.getId().equals(hostId2) && hg.getAddress().equals(address))) {
+                fail("Save and Load Host Description Fail with Different Value");
+            }
+            
+            if(!(hg instanceof GlobusHost))
+                fail("Save and Load Host Type Fail with Different Type when loading");
+
+        } catch (Exception e) {
             e.printStackTrace();
-        	fail(e.getMessage());
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSaveLoadServiceDescription() {
+        try {
+
+            JCRRegistry jcrRegistry = new JCRRegistry(null, "org.apache.jackrabbit.core.RepositoryFactoryImpl",
+                    "admin", "admin", null);
+            
+            String serviceId = "SimpleEcho";            
+            
+            ServiceDescription serv = new ServiceDescription();
+            serv.setId(serviceId);
+
+            Parameter input = Parameter.Factory.newInstance();
+            input.setName("echo_input");
+            input.addNewType().setType(DataType.STRING);
+            List<Parameter> inputList = new ArrayList<Parameter>();
+            inputList.add(input);
+            org.apache.airavata.schemas.gfac.Parameter[] inputParamList = inputList
+                    .toArray(new org.apache.airavata.schemas.gfac.Parameter[inputList.size()]);
+
+            Parameter output = Parameter.Factory.newInstance();
+            output.setName("echo_output");
+            output.addNewType().setType(DataType.STRING);
+            List<Parameter> outputList = new ArrayList<Parameter>();
+            outputList.add(output);
+            org.apache.airavata.schemas.gfac.Parameter[] outputParamList = outputList
+                    .toArray(new org.apache.airavata.schemas.gfac.Parameter[outputList.size()]);
+            serv.setInputParameters(inputParamList);
+            serv.setOutputParameters(outputParamList);
+
+            /*
+             * Save to registry
+             */
+            jcrRegistry.saveServiceDescription(serv);
+                        
+            /*
+             * Load
+             */
+            ServiceDescription service = jcrRegistry.getServiceDescription(serviceId);
+            
+            if(service == null){
+                fail("Service is null");
+            }
+            
+            if(service.getInputParameters() == null || service.getInputParameters().length != 1){
+                fail("Input Parameters is missing");
+            }
+            
+            if(service.getOutputParameters()== null || service.getOutputParameters().length != 1){
+                fail("Input Parameters is missing");
+            }
+            
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
         }
     }
 }
