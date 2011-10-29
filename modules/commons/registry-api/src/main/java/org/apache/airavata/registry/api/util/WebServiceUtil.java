2efdcadeaa276f7b93d06f91b925458f7b9d9cdc
Lahiru Gunathilake <lahiru@apache.org>
2011-10-29 16:23:20 +0000

    https://issues.apache.org/jira/browse/AIRAVATA-175
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1194921 13f79535-47bb-0310-9956-ffa450edef68

--- modules/commons/registry-api/src/main/java/org/apache/airavata/registry/api/util/WebServiceUtil.java
+++ modules/commons/registry-api/src/main/java/org/apache/airavata/registry/api/util/WebServiceUtil.java
@@ -1,9 +1,29 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package org.apache.airavata.registry.api.util;
 
+import org.apache.airavata.commons.gfac.type.ServiceDescription;
 import org.apache.airavata.schemas.gfac.InputParameterType;
 import org.apache.airavata.schemas.gfac.OutputParameterType;
-import org.apache.airavata.schemas.gfac.Parameter;
-import org.apache.airavata.commons.gfac.type.ServiceDescription;
 import org.apache.airavata.schemas.gfac.ServiceDescriptionType;
 
 public class WebServiceUtil {
