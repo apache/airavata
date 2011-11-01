b8c6233871463d4147e81b9e156e579ecbce3b0c
Lahiru Gunathilake <lahiru@apache.org>
2011-11-01 19:45:49 +0000

    reverting the change of return type.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1196240 13f79535-47bb-0310-9956-ffa450edef68

--- modules/commons/gfac-schema/src/main/java/org/apache/airavata/commons/gfac/wsdl/TypesGenerator.java
+++ modules/commons/gfac-schema/src/main/java/org/apache/airavata/commons/gfac/wsdl/TypesGenerator.java
@@ -79,7 +79,7 @@ public class TypesGenerator implements WSDLConstants {
             inParamNames.add(inputParams[k].getParameterName());
             inParamDesc.add(inputParams[k].getParameterDescription());            
             // XMLBEANS specific way to get type
-            inParamTypes.add(MappingFactory.getActualParameterType(inputParams[k].getParameterType().getType()).getName().getLocalPart());
+            inParamTypes.add(MappingFactory.getActualParameterType(inputParams[k].getParameterType().getType()));
             inputParams[k].getParameterValueArray();
         }
 
@@ -92,7 +92,7 @@ public class TypesGenerator implements WSDLConstants {
             outParamNames.add(outputParams[k].getParameterName());
             outParamDesc.add(outputParams[k].getParameterDescription());
             // XMLBEANS specific way to get type
-            outParamTypes.add(MappingFactory.getActualParameterType(outputParams[k].getParameterType().getType()).getName().getLocalPart());
+            outParamTypes.add(MappingFactory.getActualParameterType(outputParams[k].getParameterType().getType()));
         }
 
         String methodName = serviceDesc.getPortType().getMethod().getMethodName();
@@ -133,8 +133,7 @@ public class TypesGenerator implements WSDLConstants {
             paramName = paramName.replaceAll(HYPHEN, HYPHEN_REPLACEMENT);
             elem.setAttribute("name", paramName);
             String dataType = (String) inParamTypes.get(j);            
-            elem.setAttribute("type", "gfac:" + dataType);
-            
+            elem.setAttribute("type", "gfac:" + dataType);            
 
             Element annotation = doc.createElement(ANNOTATION);
 
