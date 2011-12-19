7179ffe53d7f3b4d421a117a8b6a2c1fc08c78f6
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2011-12-19 18:41:06 +0000

    Updating license headers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1220889 13f79535-47bb-0310-9956-ffa450edef68

--- modules/xbaya-gui/src/test/java/org/apache/airavata/xbaya/interpreter/LevenshteinDistanceService.java
+++ modules/xbaya-gui/src/test/java/org/apache/airavata/xbaya/interpreter/LevenshteinDistanceService.java
@@ -1,12 +1,26 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package org.apache.airavata.xbaya.interpreter;
 
-/**
- * Created by IntelliJ IDEA.
- * User: heshan
- * Date: 12/18/11
- * Time: 4:15 PM
- * To change this template use File | Settings | File Templates.
- */
 public class LevenshteinDistanceService {
     public static int computeDistance(CharSequence sequence1, CharSequence sequence2) {
         int[][] distance = new int[sequence1.length() + 1][sequence2.length() + 1];
