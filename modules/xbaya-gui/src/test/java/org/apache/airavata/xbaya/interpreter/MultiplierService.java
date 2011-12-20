19ac6e3295eab504fe803d42572b18c892fe1a29
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2011-12-20 18:41:11 +0000

    AIRAVATA-225 Adding AdderService and MultiplierServices for the test workflows.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1221418 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/xbaya-gui/src/test/java/org/apache/airavata/xbaya/interpreter/MultiplierService.java
@@ -0,0 +1,12 @@
+package org.apache.airavata.xbaya.interpreter;
+
+public class MultiplierService {
+    public int adder(int param1, int param2) {
+        return param1 + param2;
+    }
+
+    public int multiplier(int param1, int param2) {
+        return param1 * param2;
+    }
+
+}
