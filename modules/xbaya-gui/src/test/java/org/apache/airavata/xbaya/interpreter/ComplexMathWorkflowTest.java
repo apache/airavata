9e448e56431d45a846b991c96b4cb983cf01d06c
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2011-12-20 18:47:57 +0000

    Adding complex math workflow test. AIRAVATA-225
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1221427 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/xbaya-gui/src/test/java/org/apache/airavata/xbaya/interpreter/ComplexMathWorkflowTest.java
@@ -0,0 +1,27 @@
+package org.apache.airavata.xbaya.interpreter;
+
+import org.apache.airavata.xbaya.XBayaException;
+import org.apache.airavata.xbaya.interpreter.utils.WorkflowTestUtils;
+import org.apache.airavata.xbaya.interpretor.WorkflowInterpreter;
+import org.apache.airavata.xbaya.wf.Workflow;
+import org.apache.axis2.engine.ListenerManager;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.UUID;
+
+public class ComplexMathWorkflowTest {
+
+    @Test
+    public void testScheduleDynamically() throws IOException, URISyntaxException, XBayaException {
+        URL systemResource = this.getClass().getClassLoader().getSystemResource("ComplexMath.xwf");
+        Workflow workflow = new Workflow(WorkflowTestUtils.readWorkflow(systemResource));
+        ListenerManager manager = WorkflowTestUtils.axis2ServiceStarter();
+        WorkflowInterpreter interpretor = new WorkflowInterpreter(WorkflowTestUtils.getConfiguration(), UUID.randomUUID().toString(),
+                workflow, "NA", "NA",true);
+        interpretor.scheduleDynamically();
+        manager.stop();
+    }
+}
