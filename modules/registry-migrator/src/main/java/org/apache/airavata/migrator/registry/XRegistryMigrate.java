a3f08ac5ebef2c879bada1b927fd70cc4853cdd5
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-02-24 16:42:59 +0000

    AIRAVATA-309 Fixing the save order.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1293334 13f79535-47bb-0310-9956-ffa450edef68

--- modules/registry-migrator/src/main/java/org/apache/airavata/migrator/registry/XRegistryMigrate.java
+++ modules/registry-migrator/src/main/java/org/apache/airavata/migrator/registry/XRegistryMigrate.java
@@ -26,12 +26,12 @@ public class XRegistryMigrate {
 //    private static String propertyfile = "xregistry.properties";
 //    private static String propertyfile = "xregistry-dropbox.properties";
     private static String propertyfile = "xregistry-local.properties";
+    private static AiravataJCRRegistry jcrRegistry = null;
 
     public static void main(String[] args) throws XRegistryClientException {
         /* Create database */
         Map<String,String> config = new HashMap<String,String>();
         config.put("org.apache.jackrabbit.repository.home","target");
-        AiravataJCRRegistry jcrRegistry = null;
         try {
             jcrRegistry = new AiravataJCRRegistry(null,
                     "org.apache.jackrabbit.core.RepositoryFactoryImpl", "admin",
@@ -40,12 +40,13 @@ public class XRegistryMigrate {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
-        HostDescription host = null;
-        ApplicationDeploymentDescription app = null;
-        ServiceDescription service = null;
-
         XRegistryClient client = XRegistryClientUtil.CreateGSISecureRegistryInstance(propertyfile);
+        saveAllHostDescriptions(client);
+        saveAllServiceDescriptions(client);
+    }
 
+    private static HostDescription saveAllHostDescriptions(XRegistryClient client) throws XRegistryClientException {
+        HostDescription host = null;
         HostDescData[] hostDescs = client.findHosts("");
         Map<QName, HostDescData> val = new HashMap<QName, HostDescData>();
         for (HostDescData hostDesc : hostDescs) {
@@ -63,8 +64,87 @@ public class XRegistryMigrate {
                 host = MigrationUtil.createHostDescription(hostBean);
             }
 
+            try {
+                jcrRegistry.saveHostDescription(host);
+            } catch (RegistryException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+        }
+        return host;
+    }
+
+    private static ServiceDescription saveAllServiceDescriptions(XRegistryClient client) throws XRegistryClientException {
+        ServiceDescription service = null;
+        ServiceDescData[] serviceDescDatas = client.findServiceDesc("");
+        Map<QName, ServiceDescData> val3 = new HashMap<QName, ServiceDescData>();
+
+        for (ServiceDescData serviceDesc : serviceDescDatas) {
+            val3.put(serviceDesc.getName(), serviceDesc);
+            String serviceDescStr = client.getServiceDesc(serviceDesc.getName());
+            System.out.println(serviceDescStr);
+            ServiceBean serviceBean = null;
+            String applicationName = null;
+
+            try {
+                serviceBean = org.ogce.schemas.gfac.beans.utils.ServiceUtils.serviceBeanRequest(serviceDescStr);
+                applicationName = serviceBean.getApplicationName();
+            } catch (XmlException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+            if(serviceBean != null) {
+                service = MigrationUtil.createServiceDescription(serviceBean);
+            }
+
+            try {
+                jcrRegistry.saveServiceDescription(service);
+                ApplicationBean appBean = saveApplicationDescriptionWithName(client, applicationName, service);
+                jcrRegistry.deployServiceOnHost(service.getType().getName(), appBean.getHostName());
+            } catch (RegistryException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+        return service;
+    }
+
+    private static ApplicationBean saveApplicationDescriptionWithName(XRegistryClient client, String applicationName, ServiceDescription service) throws XRegistryClientException {
+        ApplicationDeploymentDescription app = null;
+        FindAppDescResponseDocument.FindAppDescResponse.AppData[] appDatas = client.findAppDesc(applicationName);
+        Map<QName, FindAppDescResponseDocument.FindAppDescResponse.AppData> val2 =
+                new HashMap<QName, FindAppDescResponseDocument.FindAppDescResponse.AppData>();
+        ApplicationBean appBean = null;
+        for (FindAppDescResponseDocument.FindAppDescResponse.AppData appDesc : appDatas) {
+            val2.put(appDesc.getName(), appDesc);
+            String appDescStr = client.getAppDesc(appDesc.getName().toString(),appDesc.getHostName());
+            System.out.println(appDescStr);
+            try {
+                appBean = org.ogce.schemas.gfac.beans.utils.ApplicationUtils.simpleApplicationBeanRequest(appDescStr);
+
+            } catch (XmlException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+            if(appBean != null){
+                app = MigrationUtil.createAppDeploymentDescription(appBean);
+                try {
+                    jcrRegistry.saveDeploymentDescription(service.getType().getName(), appBean.getHostName(), app);
+//            jcrRegistry.saveDeploymentDescription(service.getType().getName(), host.getType().getHostName(), app);
+                } catch (RegistryException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+
+
         }
 
+        return appBean;
+    }
+
+    private static void saveAllApplicationDescriptions(XRegistryClient client, ServiceDescription service) throws XRegistryClientException {
+        ApplicationDeploymentDescription app = null;
         FindAppDescResponseDocument.FindAppDescResponse.AppData[] appDatas = client.findAppDesc("");
         Map<QName, FindAppDescResponseDocument.FindAppDescResponse.AppData> val2 =
                 new HashMap<QName, FindAppDescResponseDocument.FindAppDescResponse.AppData>();
@@ -82,48 +162,10 @@ public class XRegistryMigrate {
             if(appBean != null){
                 app = MigrationUtil.createAppDeploymentDescription(appBean);
             }
+
+            //jcrRegistry.saveDeploymentDescription(service.getType().getName(), host.getType().getHostName(), app);
+
         }
-
-
-        ServiceDescData[] serviceDescDatas = client.findServiceDesc("");
-        Map<QName, ServiceDescData> val3 = new HashMap<QName, ServiceDescData>();
-        for (ServiceDescData serviceDesc : serviceDescDatas) {
-            val3.put(serviceDesc.getName(), serviceDesc);
-            String serviceDescStr = client.getServiceDesc(serviceDesc.getName());
-            System.out.println(serviceDescStr);
-            ServiceBean serviceBean = null;
-            try {
-                serviceBean = org.ogce.schemas.gfac.beans.utils.ServiceUtils.serviceBeanRequest(serviceDescStr);
-            } catch (XmlException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-
-            if(serviceBean != null) {
-                service = MigrationUtil.createServiceDescription(serviceBean);
-            }
-        }
-
-        /* Save to registry */
-        try {
-            if (service != null) {
-                jcrRegistry.saveServiceDescription(service);
-            }
-
-            if(host != null) {
-                jcrRegistry.saveHostDescription(host);
-
-                if (service != null) {
-                    jcrRegistry.saveDeploymentDescription(service.getType().getName(), host
-                            .getType().getHostName(), app);
-                    jcrRegistry.deployServiceOnHost(service.getType().getName(), host.getType().getHostName());
-                }
-            }
-        } catch (RegistryException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
     }
 
 }
