f3709bf4a8135adf437f24fd0755c14170614c6d
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-03-09 15:32:59 +0000

    Updating the migration script.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1298876 13f79535-47bb-0310-9956-ffa450edef68

--- modules/distribution/src/main/resources/bin/xregistry-migrator.sh
+++ modules/distribution/src/main/resources/bin/xregistry-migrator.sh
@@ -20,19 +20,15 @@
 echo "\n      ** Build Script for XRegistry Migration Tool **"
 echo "        ===============================================\n"
 
-# Absolute path of this script.
 SCRIPT=$(readlink -f $0)
-# Full path of the directory where current script is in.
 TOOL_CLASSPATH=`dirname $SCRIPT`
 
-#---------- Add jars to classpath ------------------
 for f in ../standalone-server/lib/*.jar
 do
   TOOL_CLASSPATH="$TOOL_CLASSPATH":$f
 done
-#echo "classpath : $TOOL_CLASSPATH"
+echo "classpath : $TOOL_CLASSPATH"
 
-#---------- Running the migration client -----------
 java -cp "$TOOL_CLASSPATH" org.apache.airavata.migrator.registry.XRegistryMigrationManager $1
 
 
