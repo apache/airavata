23dad8bcb3968290b8b5b2f83667efb3cb7523c7
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-03-09 00:13:27 +0000

    AIRAVATA-336 Adding the Exception class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1298663 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/registry-migrator/src/main/java/org/apache/airavata/migrator/registry/XRegistryMigrationException.java
@@ -0,0 +1,12 @@
+package org.apache.airavata.migrator.registry;
+
+public class XRegistryMigrationException extends Exception{
+
+    public XRegistryMigrationException(String message) {
+        super(message);
+    }
+
+    public XRegistryMigrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
