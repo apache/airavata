9defb24d54b805de02bc3bd48a3ae308abb4b006
Lahiru Gunathilake <lahiru@apache.org>
2012-05-29 19:04:23 +0000

    fixing https://issues.apache.org/jira/browse/AIRAVATA-425.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/branches/0.3-incubating-snapshot@1343927 13f79535-47bb-0310-9956-ffa450edef68

--- modules/distribution/src/main/resources/bin/setenv.sh
+++ modules/distribution/src/main/resources/bin/setenv.sh
@@ -54,11 +54,6 @@ PRGDIR=`dirname "$PRG"`
 
 XBAYA_CLASSPATH=""
 
-for f in "$AIRAVATA_HOME"/lib/*.jar
-do
-  XBAYA_CLASSPATH="$XBAYA_CLASSPATH":$f
-done
-
 for f in "$AIRAVATA_HOME"/standalone-server/lib/*.jar
 do
   XBAYA_CLASSPATH="$XBAYA_CLASSPATH":$f
