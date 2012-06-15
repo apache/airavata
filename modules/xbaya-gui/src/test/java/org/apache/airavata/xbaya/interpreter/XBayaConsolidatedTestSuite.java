ad3a6ad189778258df92b8719277fc67f8625eac
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-06-15 18:27:25 +0000

    Adding a test case to the test-suite
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1350729 13f79535-47bb-0310-9956-ffa450edef68

--- modules/xbaya-gui/src/test/java/org/apache/airavata/xbaya/interpreter/XBayaConsolidatedTestSuite.java
+++ modules/xbaya-gui/src/test/java/org/apache/airavata/xbaya/interpreter/XBayaConsolidatedTestSuite.java
@@ -18,8 +18,8 @@ import org.slf4j.LoggerFactory;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({XBayaClientTest.class, SimpleMathWorkflowTest.class, WorkflowTest.class,
         ComplexMathWorkflowTest.class, CrossProductWorkflowTest.class, ForEachWorkflowTest.class,
-        SimpleForEachWorkflowTest.class/*,
-        WorkflowTrackingTest.class, RegistryServiceTest.class*/})
+        SimpleForEachWorkflowTest.class,
+        WorkflowTrackingTest.class, RegistryServiceTest.class})
 public class XBayaConsolidatedTestSuite {
     private static ListenerManager manager = null;
 
