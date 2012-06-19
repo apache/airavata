0e3edba92d3f918e2752178e3863a7b8c6ba66e1
samindaw <samindaw@unknown>
2012-06-19 16:36:35 +0000

    branch fix applied to trunk - updating optional params
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1351769 13f79535-47bb-0310-9956-ffa450edef68

--- modules/airavata-client/src/main/java/org/apache/airavata/client/airavata/AiravataClient.java
+++ modules/airavata-client/src/main/java/org/apache/airavata/client/airavata/AiravataClient.java
@@ -58,6 +58,7 @@ import org.apache.airavata.workflow.model.component.ws.WSComponentPort;
 import org.apache.airavata.workflow.model.graph.GraphException;
 import org.apache.airavata.workflow.model.graph.impl.NodeImpl;
 import org.apache.airavata.workflow.model.graph.system.InputNode;
+import org.apache.airavata.workflow.model.graph.util.GraphUtil;
 import org.apache.airavata.workflow.model.wf.Workflow;
 import org.apache.airavata.xbaya.interpretor.NameValue;
 import org.apache.airavata.xbaya.interpretor.WorkflowInterpretorStub;
@@ -565,10 +566,17 @@ public class AiravataClient {
 	 */
 	public List<WorkflowInput> getWorkflowInputs(String workflowTemplateId) throws Exception{
 		try {
-			List<WSComponentPort> inputs = getWSComponentPortInputs(workflowTemplateId);
+			Workflow workflowTemplate = getWorkflow(workflowTemplateId);
+			List<WSComponentPort> inputs = getWSComponentPortInputs(workflowTemplate);
+			List<InputNode> inputNodes = getInputNodes(workflowTemplate);
 			List<WorkflowInput> results=new ArrayList<WorkflowInput>();
-			for (WSComponentPort port : inputs) {
-				results.add(new WorkflowInput(port.getName(), port.getType().getLocalPart(), port.getDefaultValue(), port.getValue(),port.isOptional()));
+			for (InputNode port : inputNodes) {
+				Object value=null;
+				WSComponentPort wsComponentPort = getWSComponentPort(port.getName(), inputs);
+				if (wsComponentPort!=null){
+					value=wsComponentPort.getValue();
+				}
+				results.add(new WorkflowInput(port.getName(), port.getParameterType().getLocalPart(), port.getDefaultValue(), value, !port.isVisibility()));
 			}
 			return results;
 		} catch (RegistryException e) {
@@ -598,11 +606,35 @@ public class AiravataClient {
 			String workflowTemplateId) throws RegistryException,
 			PathNotFoundException, RepositoryException, GraphException,
 			ComponentException, ValueFormatException {
+		Workflow workflow = getWorkflow(workflowTemplateId);
+		return getWSComponentPortInputs(workflow);
+	}
+
+	private Workflow getWorkflow(String workflowTemplateId)
+			throws RegistryException, PathNotFoundException,
+			RepositoryException, GraphException, ComponentException,
+			ValueFormatException {
 		Property workflowAsString = getWorkflowAsString(workflowTemplateId);
 		Workflow workflow = new Workflow(workflowAsString.getString());
+		return workflow;
+	}
+
+	private List<WSComponentPort> getWSComponentPortInputs(Workflow workflow)
+			throws GraphException, ComponentException {
 		workflow.createScript();
 		List<WSComponentPort> inputs = workflow.getInputs();
 		return inputs;
+	}
+	
+	private List<InputNode> getInputNodes(
+		String workflowTemplateId) throws PathNotFoundException, GraphException, ComponentException, ValueFormatException, RegistryException, RepositoryException{
+		Workflow workflow = getWorkflow(workflowTemplateId);
+		return getInputNodes(workflow);
+	}
+	
+	private List<InputNode> getInputNodes(Workflow workflow) {
+		List<InputNode> inputNodes = GraphUtil.getInputNodes(workflow.getGraph());
+		return inputNodes;
 	}
 	
 	private WSComponentPort getWSComponentPort(String name,List<WSComponentPort> ports){
