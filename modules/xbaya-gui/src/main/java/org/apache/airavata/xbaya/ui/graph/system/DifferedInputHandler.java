96f4cd69598fe11e43cdd316012c9f7acd553251
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-06-29 19:29:27 +0000

    Adding missing licenses.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1355522 13f79535-47bb-0310-9956-ffa450edef68

--- modules/xbaya-gui/src/main/java/org/apache/airavata/xbaya/ui/graph/system/DifferedInputHandler.java
+++ modules/xbaya-gui/src/main/java/org/apache/airavata/xbaya/ui/graph/system/DifferedInputHandler.java
@@ -1,9 +1,22 @@
 /*
- * Copyright (c) 2012 Extreme! Lab, Indiana University. All rights reserved.
  *
- * This software is open source. See the bottom of this file for the license.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * $Id: $
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
  */
 package org.apache.airavata.xbaya.ui.graph.system;
 
@@ -15,9 +28,6 @@ import org.apache.airavata.workflow.model.graph.system.DifferedInputNode;
 import org.apache.airavata.xbaya.graph.controller.NodeController;
 import org.apache.airavata.xbaya.ui.XBayaGUI;
 
-/**
- * @author Chathura Herath
- */
 public class DifferedInputHandler {
 	
 	
