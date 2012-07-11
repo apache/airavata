fa1ee7b75080131f2688944916bdbe299355d32f
Suresh Marru <smarru@apache.org>
2012-07-11 14:51:32 +0000

    removing the jnlp module, fixing the xbaya-gui build to have a jnlp
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1360213 13f79535-47bb-0310-9956-ffa450edef68

--- modules/xbaya-jnlp/src/main/resources/run.bat
+++
@@ -1,9 +0,0 @@
-::---code borrowed from http://stackoverflow.com/a/524491 ---
-@echo off
-setLocal EnableDelayedExpansion
-set CLASSPATH=
-for %%jar in (lib\*.jar) do (
-	set CLASSPATH=!CLASSPATH!%%jar;
-)
-::-----------------------------------------------------------
-::java -classpath !CLASSPATH! cct.JMolEditor
