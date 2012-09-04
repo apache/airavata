d793649acbfb583fd9a2a2f29cb73f566f075a88
samindaw <samindaw@unknown>
2012-09-04 20:31:10 +0000

    adding new registry API for review
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1380866 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/commons/registry-api/src/main/java/org/apache/airavata/registry/api/DescriptorRegistry.java
@@ -0,0 +1,58 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.registry.api;
+
+import java.util.Map;
+
+import org.apache.airavata.commons.gfac.type.ApplicationDeploymentDescription;
+import org.apache.airavata.commons.gfac.type.HostDescription;
+import org.apache.airavata.commons.gfac.type.ServiceDescription;
+
+public interface DescriptorRegistry extends AiravataSubRegistry {
+	
+	/*Note
+	 * Name changes of the descriptors should not be allowed
+	 */
+	
+	//---------Host Descriptor data------------
+	public void addHostDescriptor(HostDescription descriptor);
+	public void updateHostDescriptor(HostDescription descriptor);
+	public HostDescription getHostDescriptor(String hostName);
+	public void removeHostDescriptor(String hostName);
+
+	//---------Service Descriptor data------------
+	public void addServiceDescriptor(ServiceDescription descriptor);
+	public void updateServiceDescriptor(ServiceDescription descriptor);
+	public ServiceDescription getServiceDescriptor(String serviceName);
+	public void removeServiceDescriptor(String serviceName);
+	
+	//---------Service Descriptor data------------
+	public void addApplicationDescriptor(ServiceDescription serviceDescription, HostDescription hostDescriptor, ApplicationDeploymentDescription descriptor);
+	public void addApplicationDescriptor(String serviceName, String hostName, ApplicationDeploymentDescription descriptor);
+	public void udpateApplicationDescriptor(ServiceDescription serviceDescription, HostDescription hostDescriptor, ApplicationDeploymentDescription descriptor);
+	public void updateApplicationDescriptor(String serviceName, String hostName, ApplicationDeploymentDescription descriptor);
+	public ApplicationDeploymentDescription getApplicationDescriptors(String serviceName, String hostname);
+	public Map<String,ApplicationDeploymentDescription> getApplicationDescriptors(String serviceName);
+	public void removeApplicationDescriptor(String serviceName, String hostName, String applicationName);
+
+
+}
