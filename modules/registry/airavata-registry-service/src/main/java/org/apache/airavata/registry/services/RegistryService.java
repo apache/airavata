81fce1d27bb9d6c8c51d1b133ba36284c0f5c5b6
Lahiru Gunathilake <lahiru@apache.org>
2012-09-10 22:31:08 +0000

    adding new regstry-service to handle regisry initialization.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/trunk@1383138 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/registry/airavata-registry-service/src/main/java/org/apache/airavata/registry/services/RegistryService.java
@@ -0,0 +1,69 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+package org.apache.airavata.registry.services;
+
+import com.sun.tools.corba.se.idl.toJavaPortable.StringGen;
+import org.apache.airavata.registry.services.utils.DatabaseCreator;
+import org.apache.airavata.registry.services.utils.JdbcStorage;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.description.AxisService;
+import org.apache.axis2.engine.ServiceLifeCycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+
+public class RegistryService implements ServiceLifeCycle {
+    private static final Logger logger = LoggerFactory.getLogger(RegistryService.class);
+
+    public static final String PERSISTANT_DATA = "persistant_data";
+    private JdbcStorage db;
+
+    @Override
+    public void startUp(ConfigurationContext configurationContext, AxisService axisService) {
+        //todo have to read these properties from some configuration
+        String jdbcUrl = null;
+        String jdbcDriver = null;
+
+        db = new JdbcStorage(10, 50, jdbcUrl, jdbcDriver, true);
+
+        Connection conn = null;
+        try {
+            conn = db.connect();
+            if (!DatabaseCreator.isDatabaseStructureCreated(PERSISTANT_DATA, conn)) {
+                DatabaseCreator.createMsgBoxDatabase(conn);
+                logger.info("New Database created for Registry");
+            } else {
+                logger.info("Database already created for Registry!");
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException("Database failure");
+        } finally {
+            db.closeConnection(conn);
+        }
+    }
+
+    @Override
+    public void shutDown(ConfigurationContext configurationContext, AxisService axisService) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
