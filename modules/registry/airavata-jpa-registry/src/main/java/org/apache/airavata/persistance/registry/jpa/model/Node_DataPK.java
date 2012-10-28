6de090a5b81beabacecc92b50166f0329af7a796
Suresh Marru <smarru@apache.org>
2012-10-28 00:09:53 +0000

    adding missing license header and excludes for rat check
    git-svn-id: https://svn.apache.org/repos/asf/airavata/branches/0.5@1402912 13f79535-47bb-0310-9956-ffa450edef68

--- modules/registry/airavata-jpa-registry/src/main/java/org/apache/airavata/persistance/registry/jpa/model/Node_DataPK.java
+++ modules/registry/airavata-jpa-registry/src/main/java/org/apache/airavata/persistance/registry/jpa/model/Node_DataPK.java
@@ -1,13 +1,33 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
 package org.apache.airavata.persistance.registry.jpa.model;
 
-
 public class Node_DataPK {
-	private String workflow_instanceID;
-	private String node_id;
+    private String workflow_instanceID;
+    private String node_id;
 
-	public Node_DataPK() {
-		;
-	}
+    public Node_DataPK() {
+        ;
+    }
 
     public Node_DataPK(String workflow_instanceID, String node_id) {
         this.workflow_instanceID = workflow_instanceID;
@@ -15,14 +35,14 @@ public class Node_DataPK {
     }
 
     @Override
-	public boolean equals(Object o) {
-		return false;
-	}
+    public boolean equals(Object o) {
+        return false;
+    }
 
-	@Override
-	public int hashCode() {
-		return 1;
-	}
+    @Override
+    public int hashCode() {
+        return 1;
+    }
 
     public String getWorkflow_instanceID() {
         return workflow_instanceID;
@@ -33,10 +53,10 @@ public class Node_DataPK {
     }
 
     public String getNode_id() {
-		return node_id;
-	}
+        return node_id;
+    }
 
-	public void setNode_id(String node_id) {
-		this.node_id = node_id;
-	}
+    public void setNode_id(String node_id) {
+        this.node_id = node_id;
+    }
 }
