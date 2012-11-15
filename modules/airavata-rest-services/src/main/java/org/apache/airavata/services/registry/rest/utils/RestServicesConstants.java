be56a0c1bbd369a3d148f6fd1e1c9f3f9f008717
Lahiru Gunathilake <lahiru@apache.org>
2012-11-15 20:07:11 +0000

    Applying patch provided by Amila for issue https://issues.apache.org/jira/browse/AIRAVATA-645. Thanks Amila for the contribution.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1409960 13f79535-47bb-0310-9956-ffa450edef68

--- modules/airavata-rest-services/src/main/java/org/apache/airavata/services/registry/rest/utils/RestServicesConstants.java
+++ modules/airavata-rest-services/src/main/java/org/apache/airavata/services/registry/rest/utils/RestServicesConstants.java
@@ -1,5 +1,7 @@
 package org.apache.airavata.services.registry.rest.utils;
 
+import org.apache.commons.codec.binary.Base64;
+
 public class RestServicesConstants {
     public static final String AIRAVATA_SERVER_PROPERTIES = "airavata-server.properties";
     public static final String GATEWAY_ID = "gateway.id";
@@ -8,4 +10,20 @@ public class RestServicesConstants {
     public static final String REGISTRY_USER = "airavata.user";
     public static final String AIRAVATA_REGISTRY = "airavataRegistry";
     public static final String AIRAVATA_API = "airavataAPI";
+
+
+    /**
+     * A method to use by clients in the case of Basic Access authentication.
+     * Creates Basic Auth header structure.
+     * Reference - http://en.wikipedia.org/wiki/Basic_access_authentication
+     * @param userName The user name.
+     * @param password Password as credentials.
+     * @return  Base64 encoded authorisation header.
+     */
+    public static String getBasicAuthHeader(String userName, String password) {
+
+        String credentials = userName + ":" + password;
+        String encodedString = new String(Base64.encodeBase64(credentials.getBytes()));
+        return "Basic " + encodedString;
+    }
 }
