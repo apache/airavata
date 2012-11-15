593559e1f881d095e8aa4b5c5d986f418aeacf9d
chathuri <chathuri@unknown>
2012-11-15 21:06:01 +0000

    updating rest service methods
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1410012 13f79535-47bb-0310-9956-ffa450edef68

--- modules/airavata-rest-services/src/main/java/org/apache/airavata/services/registry/rest/utils/RestServicesConstants.java
+++ modules/airavata-rest-services/src/main/java/org/apache/airavata/services/registry/rest/utils/RestServicesConstants.java
@@ -1,3 +1,24 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package org.apache.airavata.services.registry.rest.utils;
 
 import org.apache.commons.codec.binary.Base64;
@@ -9,8 +30,6 @@ public class RestServicesConstants {
     public static final String REGISTRY_USERNAME = "registry.user";
     public static final String REGISTRY_USER = "airavata.user";
     public static final String AIRAVATA_REGISTRY = "airavataRegistry";
-    public static final String AIRAVATA_API = "airavataAPI";
-
 
     /**
      * A method to use by clients in the case of Basic Access authentication.
