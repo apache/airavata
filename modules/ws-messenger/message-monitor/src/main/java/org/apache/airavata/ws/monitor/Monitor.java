bdb65849508d2295ba2ae295483216f85f4fb2d7
samindaw <samindaw@unknown>
2012-11-19 17:05:10 +0000

    adding monitoring module
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1411310 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/ws-messenger/message-monitor/src/main/java/org/apache/airavata/ws/monitor/Monitor.java
@@ -0,0 +1,246 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.ws.monitor;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.airavata.common.utils.XMLUtil;
+import org.apache.airavata.workflow.model.exceptions.WorkflowException;
+import org.apache.airavata.ws.monitor.event.Event;
+import org.apache.airavata.ws.monitor.event.EventProducer;
+import org.apache.airavata.ws.monitor.event.Event.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmlpull.infoset.XmlElement;
+
+public class Monitor extends EventProducer {
+
+    protected static final Logger logger = LoggerFactory.getLogger(Monitor.class);
+
+    protected MonitorConfiguration configuration;
+
+    protected static final String DEFAULT_MODEL_KEY = "_DEFAULT_MODEL_KEY";
+
+    protected Map<String, MonitorEventData> eventDataMap = new HashMap<String, MonitorEventData>();
+
+    protected WsmgClient wsmgClient;
+
+    protected boolean print;
+
+    protected long timeout = 20000L;
+
+    protected boolean status = false;
+    
+    /**
+     * Constructs a Monitor.
+     * 
+     * @param configuration
+     */
+    public Monitor(MonitorConfiguration configuration) {
+        this.configuration = configuration;
+        // The first one is special and it is for the main event panel display
+        // and
+        // it does not have and filters
+        this.eventDataMap.put(DEFAULT_MODEL_KEY, new MonitorEventData());
+
+
+    }
+
+    /**
+     * @return The configuration
+     */
+    public MonitorConfiguration getConfiguration() {
+        return this.configuration;
+    }
+
+    /**
+     * @return The event data;
+     */
+    public MonitorEventData getEventData() {
+        // send the first one cos that is the default one
+        return this.eventDataMap.get(DEFAULT_MODEL_KEY);
+    }
+
+    /**
+     * @return The event data;
+     */
+    public MonitorEventData getEventData(String nodeID) {
+        // send the first one cos that is the default one
+        return this.eventDataMap.get(nodeID);
+    }
+
+    /**
+     * @throws MonitorException
+     */
+    public synchronized void start() throws MonitorException {
+        // Stop the previous monitoring if any.
+        asynchronousStop();
+
+        subscribe();
+
+        if (null != this.configuration.getInteractiveNodeIDs()) {
+            List<String> interactiveNodeIDs = this.configuration.getInteractiveNodeIDs();
+            // now add models for this as well
+            for (String string : interactiveNodeIDs) {
+
+                final String nodeID = string;
+                // for each wsnode there is one data model which
+                this.eventDataMap.put(nodeID, new MonitorEventData(new EventFilter() {
+                    /**
+                     * @see org.apache.airavata.ws.monitor.EventFilter#isAcceptable(org.apache.airavata.ws.monitor.MonitorEvent)
+                     */
+                    public boolean isAcceptable(MonitorEvent event) {
+                        return event != null && event.getNodeID() != null && event.getNodeID().equals(nodeID);
+                    }
+                }));
+            }
+
+        }
+    }
+    
+    public void startMonitoring(){
+    	final Monitor m=this;
+    	new Thread(){
+    		@Override
+    		public void run() {
+    			try {
+					m.start();
+				} catch (MonitorException e) {
+					e.printStackTrace();
+				}
+    		}
+    	}.start();
+    }
+
+    /**
+     * Stops monitoring.
+     */
+    public synchronized void asynchronousStop() {
+        if (this.wsmgClient != null) {
+            // To make thread safe.
+            final WsmgClient client = this.wsmgClient;
+            this.wsmgClient = null;
+
+            // Users don't need to know the end of unsubscription.
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        unsubscribe(client);
+                    } catch (WorkflowException e) {
+                        // Ignore the error in unsubscription.
+                        logger.error(e.getMessage(), e);
+                    }
+                }
+            }.start();
+        }
+    }
+
+    /**
+     * Stops monitoring without using a thread.
+     * 
+     * @throws MonitorException
+     */
+    public synchronized void stop() throws MonitorException {
+        if (this.wsmgClient != null) {
+            unsubscribe(this.wsmgClient);
+            this.wsmgClient = null;
+        }
+    }
+
+    /**
+     * Resets the graph and clear the monitoring table. Remove all the extra tablemodels available
+     */
+    public void reset() {
+        Set<String> keys = this.eventDataMap.keySet();
+        LinkedList<String> keysToBeRemoved = new LinkedList<String>();
+        // Remove everthing leaving only the last one
+        for (String key : keys) {
+            MonitorEventData monitorEventData = this.eventDataMap.get(key);
+            monitorEventData.removeAllEvents();
+            if (!key.equals(DEFAULT_MODEL_KEY)) {
+                keysToBeRemoved.add(key);
+            }
+        }
+        for (String key : keysToBeRemoved) {
+            this.eventDataMap.remove(key);
+        }
+
+    }
+
+    /**
+     * @param event
+     */
+    public synchronized void handleNotification(XmlElement event) {
+        Set<String> keys = this.eventDataMap.keySet();
+        // Remove everthing leaving only the last one
+        if(print){
+            System.out.println(XMLUtil.xmlElementToString(event));
+        }
+        for (String key : keys) {
+            this.eventDataMap.get(key).addEvent(event);
+        }
+    }
+
+    private void subscribe() throws MonitorException {
+        this.wsmgClient = new WsmgClient(this);
+        this.wsmgClient.setTimeout(this.getTimeout());
+        //Users can set the timeout and interval for the subscription using wsmg setter methods, here we use the default values
+        this.wsmgClient.subscribe();
+        this.status = true;
+
+        // Enable/disable some menu items and show the monitor panel.
+        sendSafeEvent(new Event(Type.MONITOR_STARTED));
+    }
+
+    private void unsubscribe(WsmgClient client) throws MonitorException {
+        // Enable/disable some menu items.
+        sendSafeEvent(new Event(Type.MONITOR_STOPED));
+
+        client.unsubscribe();
+        this.status = false;
+    }
+
+    public void setPrint(boolean print) {
+        this.print = print;
+    }
+
+    public long getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(long timeout) {
+        this.timeout = timeout;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
---
+++ modules/ws-messenger/message-monitor/src/main/java/org/apache/airavata/ws/monitor/MonitorConfiguration.java
@@ -0,0 +1,191 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.ws.monitor;
+
+import java.net.URI;
+import java.util.List;
+
+import org.apache.airavata.ws.monitor.event.Event;
+import org.apache.airavata.ws.monitor.event.EventProducer;
+import org.apache.airavata.ws.monitor.event.Event.Type;
+import org.apache.airavata.common.utils.StringUtil;
+
+public class MonitorConfiguration extends EventProducer implements Cloneable{
+
+    // private static final Logger logger = LoggerFactory.getLogger();
+
+    private URI brokerURL;
+
+    private String topic;
+
+    private boolean pullMode;
+
+    private URI messageBoxURL;
+
+    private List<String> interactiveNodeIDs = null;
+
+    /**
+     * Constructs a NotificationConfiguration.
+     * 
+     * @param brokerURL
+     * @param topic
+     * @param pullMode
+     * @param messageBoxURL
+     */
+    public MonitorConfiguration(URI brokerURL, String topic, boolean pullMode, URI messageBoxURL) {
+        set(brokerURL, topic, pullMode, messageBoxURL);
+    }
+
+    /**
+     * @param brokerURL
+     * @param topic
+     * @param pullMode
+     * @param messageBoxURL
+     */
+    public void set(URI brokerURL, String topic, boolean pullMode, URI messageBoxURL) {
+        this.brokerURL = brokerURL;
+        this.topic = topic;
+        this.pullMode = pullMode;
+        this.messageBoxURL = messageBoxURL;
+        sendSafeEvent(new Event(Type.MONITOR_CONFIGURATION_CHANGED));
+    }
+
+    /**
+     * @param brokerURL
+     *            The brokerLocation to set.
+     */
+    public void setBrokerURL(URI brokerURL) {
+        this.brokerURL = brokerURL;
+        sendSafeEvent(new Event(Type.MONITOR_CONFIGURATION_CHANGED));
+    }
+
+    /**
+     * Returns the URL of the notification broker.
+     * 
+     * @return The URL of the notification broker
+     */
+    public URI getBrokerURL() {
+        return this.brokerURL;
+    }
+
+    /**
+     * @param topic
+     *            The topic to set
+     */
+    public void setTopic(String topic) {
+        this.topic = StringUtil.trimAndNullify(topic);
+        sendSafeEvent(new Event(Type.MONITOR_CONFIGURATION_CHANGED));
+    }
+
+    /**
+     * @return The userId.
+     */
+    public String getTopic() {
+        return this.topic;
+    }
+
+    /**
+     * Returns the messageBoxUrl.
+     * 
+     * @return The messageBoxUrl
+     */
+    public URI getMessageBoxURL() {
+        return this.messageBoxURL;
+    }
+
+    /**
+     * Sets messageBoxUrl.
+     * 
+     * @param messageBoxURL
+     *            The messageBoxUrl to set.
+     */
+    public void setMessageBoxURL(URI messageBoxURL) {
+        this.messageBoxURL = messageBoxURL;
+        sendSafeEvent(new Event(Type.MONITOR_CONFIGURATION_CHANGED));
+    }
+
+    /**
+     * Returns the pullMode.
+     * 
+     * @return The pullMode
+     */
+    public boolean isPullMode() {
+        return this.pullMode;
+    }
+
+    /**
+     * Sets pullMode.
+     * 
+     * @param pullMode
+     *            The pullMode to set.
+     */
+    public void setPullMode(boolean pullMode) {
+        this.pullMode = pullMode;
+        sendSafeEvent(new Event(Type.MONITOR_CONFIGURATION_CHANGED));
+    }
+
+    /**
+     * @return true if the configuration is valid; false otherwise.
+     */
+    public boolean isValid() {
+        if (this.brokerURL == null) {
+            return false;
+        }
+        if (this.topic == null || this.topic.length() == 0) {
+            return false;
+        }
+        if (this.pullMode == true) {
+            if (this.messageBoxURL == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @see java.lang.Object#clone()
+     */
+    @Override
+    public MonitorConfiguration clone() {
+        return new MonitorConfiguration(this.brokerURL, this.topic, this.pullMode, this.messageBoxURL);
+    }
+
+    /**
+     * Returns the interactiveNodeIDs.
+     * 
+     * @return The interactiveNodeIDs
+     */
+    public List<String> getInteractiveNodeIDs() {
+        return this.interactiveNodeIDs;
+    }
+
+    /**
+     * Sets interactiveNodeIDs.
+     * 
+     * @param interactiveNodeIDs
+     *            The interactiveNodeIDs to set.
+     */
+    public void setInteractiveNodeIDs(List<String> interactiveNodeIDs) {
+        this.interactiveNodeIDs = interactiveNodeIDs;
+    }
+
+}
