1f697fa9aa215c73d57592bab3a04fab8afc45b5
Lahiru Gunathilake <lahiru@apache.org>
2012-11-26 16:21:55 +0000

    committing amilas patch for https://issues.apache.org/jira/browse/AIRAVATA-658.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1413707 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/commons/utils/src/main/java/org/apache/airavata/common/context/WorkflowContext.java
@@ -0,0 +1,51 @@
+package org.apache.airavata.common.context;
+
+/**
+ * The workflow context class. This will be local to a thread.
+ * Workflow data that needs to propagate relevant to a request will be stored here.
+ * We use thread local globals to store request data.
+ * Currently we only store user identity.
+ */
+public class WorkflowContext {
+
+    private static final ThreadLocal userThreadLocal = new ThreadLocal();
+
+    /**
+     * Sets the context.
+     * @param context The context to be set. - Careful when calling this.
+     *                Make sure other data relevant to context is preserved.
+     */
+    public static void set(RequestContext context) {
+        userThreadLocal.set(context);
+    }
+
+    /**
+     * Clears the context
+     */
+    public static void unset() {
+        userThreadLocal.remove();
+    }
+
+    /**
+     * Gets the context associated with current context.
+     * @return The context associated with current thread.
+     */
+    public static RequestContext get() {
+        return (RequestContext) userThreadLocal.get();
+    }
+
+    /**
+     * Gets the user associated with current user.
+     * @return User id associated with current request.
+     */
+    public static synchronized String getRequestUser() {
+
+        RequestContext requestContext = (RequestContext) userThreadLocal.get();
+
+        if (requestContext != null) {
+            return requestContext.getUserIdentity();
+        }
+
+        return null;
+    }
+}
