1ff97534925db07b34ab27e3facc4e456213d819
Lahiru Gunathilake <lahiru@apache.org>
2012-11-26 16:21:55 +0000

    committing amilas patch for https://issues.apache.org/jira/browse/AIRAVATA-658.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1413707 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/commons/utils/src/main/java/org/apache/airavata/common/context/RequestContext.java
@@ -0,0 +1,26 @@
+package org.apache.airavata.common.context;
+
+/**
+ * The request context class. This will be local to a thread.
+ * User data that needs to propagate relevant to a request will be stored here.
+ * We use thread local globals to store request data.
+ * Currently we only store user identity.
+ */
+public class RequestContext {
+
+    public String getUserIdentity() {
+        return userIdentity;
+    }
+
+    public void setUserIdentity(String userIdentity) {
+        this.userIdentity = userIdentity;
+    }
+
+    /**
+     * User associated with current request.
+     */
+    private String userIdentity;
+
+
+
+}
