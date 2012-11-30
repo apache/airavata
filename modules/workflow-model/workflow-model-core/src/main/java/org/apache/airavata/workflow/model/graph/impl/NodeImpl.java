05bc4585d6da6654ba1d401f0cdedb11afaf9176
Lahiru Gunathilake <lahiru@apache.org>
2012-11-30 16:44:41 +0000

    Fixing workflwo to run rather breaking before start running the workflow.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1415734 13f79535-47bb-0310-9956-ffa450edef68

--- modules/workflow-model/workflow-model-core/src/main/java/org/apache/airavata/workflow/model/graph/impl/NodeImpl.java
+++ modules/workflow-model/workflow-model-core/src/main/java/org/apache/airavata/workflow/model/graph/impl/NodeImpl.java
@@ -93,7 +93,7 @@ public abstract class NodeImpl implements Node {
 
     protected transient boolean requireJoin = false;
     
-    private NodeExecutionState state;
+    private NodeExecutionState state = NodeExecutionState.WAITING;
     
     private List<NodeObserver> observers;
 
