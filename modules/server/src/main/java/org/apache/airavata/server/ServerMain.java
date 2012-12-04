cde9740aad7222f6576dc41a72344e09d03e3b95
Lahiru Gunathilake <lahiru@apache.org>
2012-12-04 19:34:56 +0000

    adding rest service in to embedded tomcat instance.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1417135 13f79535-47bb-0310-9956-ffa450edef68

--- modules/server/src/main/java/org/apache/airavata/server/ServerMain.java
+++ modules/server/src/main/java/org/apache/airavata/server/ServerMain.java
@@ -24,6 +24,9 @@ import org.apache.airavata.common.utils.ServerSettings;
 import org.apache.catalina.Wrapper;
 import java.io.File;
 import org.apache.catalina.Host;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.deploy.FilterDef;
+import org.apache.catalina.deploy.FilterMap;
 import org.apache.catalina.startup.Tomcat;
 
 public class ServerMain {
@@ -55,9 +58,25 @@ public class ServerMain {
                 File.separator + "bin" + File.separator + "axis2.xml");
         axis2Servlet.setLoadOnStartup(1);
 
-//        Wrapper registryServlet = tomcat.addServlet("/registry", "Airavata Web Application", "com.sun.jersey.spi.container.servlet.ServletContainer");
-//        registryServlet.addInitParameter("com.sun.jersey.config.property.packages","org.apache.airavata.services.registry.rest;org.codehaus.jackson.jaxrs");
-//        registryServlet.setLoadOnStartup(1);
+        StandardContext context = (StandardContext)tomcat.getTomcat().addContext("/registry", System.getenv("AIRAVATA_HOME"));
+        Wrapper registryServlet = tomcat.addServlet("/registry", "Airavata Web Application", "com.sun.jersey.spi.container.servlet.ServletContainer");
+        registryServlet.addInitParameter("com.sun.jersey.config.property.packages", "org.apache.airavata.services.registry.rest;org.codehaus.jackson.jaxrs");
+        registryServlet.setLoadOnStartup(1);
+
+        FilterDef filter1definition = new FilterDef();
+        filter1definition.setFilterName("AuthenticationFilter");
+        filter1definition.setFilterClass("org.apache.airavata.services.registry.rest.security.HttpAuthenticatorFilter");
+        filter1definition.addInitParameter("authenticatorConfigurations","authenticators.xml");
+        context.addFilterDef(filter1definition);
+
+        FilterMap filter1mapping = new FilterMap();
+        filter1mapping.setFilterName("AuthenticationFilter");
+        filter1mapping.addURLPattern("/user-store/*");
+        filter1mapping.addURLPattern("/api/*");
+        context.addFilterMap(filter1mapping);
+        registryServlet.addMapping("/api/*");
+        context.addApplicationListener("org.apache.airavata.rest.mappings.utils.RegistryListener");
+
         tomcat.start();
     }
 
