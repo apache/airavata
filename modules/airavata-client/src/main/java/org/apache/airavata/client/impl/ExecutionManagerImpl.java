414426a68a26a43c3160c17119206f2b3b0acfce
samindaw <samindaw@unknown>
2012-12-20 14:53:00 +0000

    adding execution advance options impl + updating the API + api impl
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1424502 13f79535-47bb-0310-9956-ffa450edef68

--- modules/airavata-client/src/main/java/org/apache/airavata/client/impl/ExecutionManagerImpl.java
+++ modules/airavata-client/src/main/java/org/apache/airavata/client/impl/ExecutionManagerImpl.java
@@ -21,18 +21,32 @@
 
 package org.apache.airavata.client.impl;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.UUID;
+
+import javax.xml.stream.XMLStreamException;
+
 import org.apache.airavata.client.AiravataClient;
 import org.apache.airavata.client.AiravataClientConfiguration;
 import org.apache.airavata.client.api.AiravataAPIInvocationException;
 import org.apache.airavata.client.api.ExecutionManager;
+import org.apache.airavata.client.api.ExperimentAdvanceOptions;
+import org.apache.airavata.client.stub.interpretor.NameValue;
+import org.apache.airavata.client.stub.interpretor.WorkflowInterpretorStub;
+import org.apache.airavata.common.utils.XMLUtil;
 import org.apache.airavata.common.workflow.execution.context.WorkflowContextHeaderBuilder;
+import org.apache.airavata.workflow.model.component.ComponentException;
+import org.apache.airavata.workflow.model.component.ws.WSComponentPort;
+import org.apache.airavata.workflow.model.graph.GraphException;
 import org.apache.airavata.workflow.model.wf.Workflow;
 import org.apache.airavata.workflow.model.wf.WorkflowInput;
 import org.apache.airavata.ws.monitor.Monitor;
 import org.apache.airavata.ws.monitor.MonitorEventListener;
-
-import java.util.Calendar;
-import java.util.List;
+import org.apache.axiom.om.impl.llom.util.AXIOMUtil;
+import org.apache.axis2.AxisFault;
 
 public class ExecutionManagerImpl implements ExecutionManager {
 	private AiravataClient client;
@@ -120,19 +134,6 @@ public class ExecutionManagerImpl implements ExecutionManager {
 		}
 	}
 
-//    @Override
-//    public DefaultExecutionContext createDefaultExecutionContext() throws AiravataAPIInvocationException {
-//        DefaultExecutionContext ec = new DefaultExecutionContext();
-////        ec.addNotifiable(new LoggingNotification());
-////        try {
-//            // TODO : Fix me
-////            ec.setRegistryService(getClient().getRegistry());
-////        } catch (RegistryException e) {
-////            throw new AiravataAPIInvocationException(e);
-////        }
-//        return  ec;
-//    }
-
 	@Override
 	public String runExperiment(String workflowName,
 			List<WorkflowInput> inputs, String user, String metadata,
@@ -143,6 +144,143 @@ public class ExecutionManagerImpl implements ExecutionManager {
 		} catch (Exception e) {
 			throw new AiravataAPIInvocationException(e);
 		}
+	}
+
+	@Override
+	public String runExperiment(String workflow,
+			List<WorkflowInput> inputs, ExperimentAdvanceOptions options)
+			throws AiravataAPIInvocationException {
+		return runWorkflow(workflow, inputs, options);
+	}
+	
+	private String runWorkflow(String workflowName, List<WorkflowInput> inputs, ExperimentAdvanceOptions options) throws AiravataAPIInvocationException {
+		Workflow workflowObj = extractWorkflow(workflowName);
+		return runWorkflow(workflowName, workflowObj, inputs, options);
+	}
+	
+	private String runWorkflow(String workflowName, Workflow workflowObj, List<WorkflowInput> inputs, ExperimentAdvanceOptions options) throws AiravataAPIInvocationException {
+		try {
+	        
+			String workflowString = XMLUtil.xmlElementToString(workflowObj.toXML());
+			List<WSComponentPort> ports = getWSComponentPortInputs(workflowObj);
+			for (WorkflowInput input : inputs) {
+				WSComponentPort port = getWSComponentPort(input.getName(),
+						ports);
+				if (port != null) {
+					port.setValue(input.getValue());
+				}
+			}
+			List<NameValue> inputValues = new ArrayList<NameValue>();
+			for (WSComponentPort port : ports) {
+				NameValue nameValue = new NameValue();
+				nameValue.setName(port.getName());
+				if (port.getValue() == null) {
+					nameValue.setValue(port.getDefaultValue());
+				} else {
+					nameValue.setValue(port.getValue().toString());
+				}
+				inputValues.add(nameValue);
+			}
+			String experimentID=options.getCustomExperimentId();
+			String workflowTemplateName = workflowObj.getName();
+			if (experimentID == null || experimentID.isEmpty()) {
+				experimentID = workflowTemplateName + "_" + UUID.randomUUID();
+			}
+	        getClient().getProvenanceManager().setWorkflowInstanceTemplateName(experimentID,workflowTemplateName);
+	        
+			WorkflowContextHeaderBuilder builder=options.getCustomWorkflowContext();
+			if (builder==null){
+				builder=options.newCustomWorkflowContext();
+			}
+			
+			//TODO - fix user passing
+	        String submissionUser = getClient().getUserManager().getAiravataUser();
+			builder.setUserIdentifier(submissionUser);
+			String executionUser=options.getExperimentExecutionUser();
+			if (executionUser==null){
+				executionUser=submissionUser;
+			}
+			runPreWorkflowExecutionTasks(experimentID, executionUser, options.getExperimentMetadata(), options.getExperimentName());
+			NameValue[] inputVals = inputValues.toArray(new NameValue[] {});
+			launchWorkflow(experimentID, workflowString, inputVals, builder);
+			return experimentID;
+			
+		}  catch (GraphException e) {
+			throw new AiravataAPIInvocationException(e);
+		} catch (ComponentException e) {
+			throw new AiravataAPIInvocationException(e);
+		} catch (Exception e) {
+	        throw new AiravataAPIInvocationException("Error working with Airavata Registry: " + e.getLocalizedMessage(), e);
+	    }
+	}
+    private Workflow extractWorkflow(String workflowName) throws AiravataAPIInvocationException {
+        Workflow workflowObj = null;
+        if(getClient().getWorkflowManager().isWorkflowExists(workflowName)) {
+            workflowObj = getClient().getWorkflowManager().getWorkflow(workflowName);
+        }else {
+            try{
+                workflowObj = getClient().getWorkflowManager().getWorkflowFromString(workflowName);
+            }catch (AiravataAPIInvocationException e){
+            	getClient().getWorkflowManager().getWorkflow(workflowName);
+            }
+
+        }
+        return workflowObj;
+    }
+    
+	private List<WSComponentPort> getWSComponentPortInputs(Workflow workflow)
+			throws GraphException, ComponentException {
+		workflow.createScript();
+		List<WSComponentPort> inputs = workflow.getInputs();
+		return inputs;
+	}
+
+	private WSComponentPort getWSComponentPort(String name,
+			List<WSComponentPort> ports) {
+		for (WSComponentPort port : ports) {
+			if (port.getName().equals(name)) {
+				return port;
+			}
+		}
+		return null;
+	}
+	
+	private void launchWorkflow(String experimentId, String workflowGraph, NameValue[] inputs,
+			WorkflowContextHeaderBuilder builder) throws AiravataAPIInvocationException {
+		try {
+			builder.getWorkflowMonitoringContext().setExperimentId(experimentId);
+			WorkflowInterpretorStub stub = new WorkflowInterpretorStub(getClient().getAiravataManager().getWorkflowInterpreterServiceURL().toString());
+			stub._getServiceClient().addHeader(
+					AXIOMUtil.stringToOM(XMLUtil.xmlElementToString(builder
+							.getXml())));
+			stub.launchWorkflow(workflowGraph, experimentId, inputs);
+		} catch (AxisFault e) {
+			e.printStackTrace();
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void runPreWorkflowExecutionTasks(String experimentId, String user,
+			String metadata, String experimentName) throws AiravataAPIInvocationException {
+		if (user != null) {
+			getClient().getProvenanceManager().setExperimentUser(experimentId, user);
+		}
+		if (metadata != null) {
+			getClient().getProvenanceManager().setExperimentMetadata(experimentId, metadata);
+		}
+		if (experimentName == null) {
+			experimentName = experimentId;
+		}
+		getClient().getProvenanceManager().setExperimentName(experimentId, experimentName);
+	}
+	
+	@Override
+	public ExperimentAdvanceOptions createExperimentAdvanceOptions()
+			throws AiravataAPIInvocationException {
+		return new ExperimentAdavanceOptionsImpl(getClient());
 	}
 
 }
