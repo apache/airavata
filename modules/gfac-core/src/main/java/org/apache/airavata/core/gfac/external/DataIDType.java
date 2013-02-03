83ed3e0713d2c33cd83adeda0f09645417ece049
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2013-02-03 04:04:13 +0000

    AIRAVATA-202 Adding logic necessary to make ssh connection with the Amazon instances (Required by the EC2Provider). I'm waiting till Lahiru finishes the GFac refactoring. Once it is done will use the refactored classes with the EC2Provider. Then I can start testing the EC2Provider with Airavata.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1441854 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/gfac-core/src/main/java/org/apache/airavata/core/gfac/external/DataIDType.java
@@ -0,0 +1,74 @@
+package org.apache.airavata.core.gfac.external;
+
+import org.apache.airavata.core.gfac.exception.GfacException;
+import org.xmlpull.v1.builder.XmlElement;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+/**
+ * Represents a DataID (A schema with real names), currently it only sends a one
+ * location value.
+ */
+public class DataIDType {
+    public static final String LOCATION_ATTRIBUTE = "location";
+
+    private URI dataID;
+
+    private ArrayList<URI> dataLocations = new ArrayList<URI>();
+
+    public URI getRealLocation() {
+        if (dataLocations.size() > 0) {
+            return dataLocations.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public DataIDType(XmlElement ele) throws GfacException {
+        try {
+            String value = ele.requiredTextContent();
+            if (value != null) {
+                this.dataID = new URI(value);
+            } else {
+                throw new GfacException(
+                        "Illegal InputMessage, No value content found for the parameter "
+                                + ele.getName() + "/value. Invalid Local Argument");
+            }
+            String location = ele.getAttributeValue(null, DataIDType.LOCATION_ATTRIBUTE);
+            if (location != null) {
+                addDataLocation(new URI(location));
+            }
+        } catch (URISyntaxException e) {
+            throw new GfacException("Invalid Local Argument", e);
+        }
+    }
+
+    public DataIDType(URI dataID) {
+        super();
+        this.dataID = dataID;
+    }
+
+    public void addDataLocation(URI dataLocation) {
+        dataLocations.add(dataLocation);
+    }
+
+    public ArrayList<URI> getDataLocations() {
+        return dataLocations;
+    }
+
+    public URI getDataID() {
+        return dataID;
+    }
+
+    public void fillData(XmlElement ele) {
+        ele.addChild(dataID.toString());
+        URI location = getRealLocation();
+        if (location != null) {
+            ele.addAttribute(DataIDType.LOCATION_ATTRIBUTE, location.toString());
+        }
+    }
+
+}
+
---
+++ modules/gfac-core/src/main/java/org/apache/airavata/core/gfac/external/FileTransferService.java
@@ -0,0 +1,35 @@
+package org.apache.airavata.core.gfac.external;
+
+import org.apache.airavata.core.gfac.exception.GfacException;
+import org.ietf.jgss.GSSCredential;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Vector;
+
+public interface FileTransferService {
+    public URI copy(URI src, URI dest, GSSCredential gssCred) throws GfacException;
+
+    public URI forcedCopy(URI src, URI dest, GSSCredential gssCred) throws GfacException;
+
+    public URI copyToDir(URI src, URI destDir, GSSCredential gssCred) throws GfacException;
+
+    public void makeDir(URI destURI, GSSCredential gssCred) throws GfacException;
+
+    public Vector<URI> listDir(URI srcURI, GSSCredential gssCred) throws GfacException;
+
+    public String readRemoteFile(URI destURI, GSSCredential gsCredential,
+                                 File localFile) throws GfacException;
+
+    public boolean isExisits(URI uri, GSSCredential gssCred) throws GfacException;
+
+    public URI copyWithDataID(DataIDType src, URI destURL, GSSCredential gssCred)
+            throws GfacException;
+
+    public DataIDType store(URI src) throws GfacException;
+
+    public ContactInfo findContact(URI uri) throws GfacException;
+
+    public URI[] copyToDir(URI[] srcList, URI destDir, GSSCredential gssCred) throws GfacException;
+}
+
