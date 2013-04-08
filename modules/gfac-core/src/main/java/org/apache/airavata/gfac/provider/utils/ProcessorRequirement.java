1f2ea817483921337259c18540f8403b2ca8aa3c
Suresh Marru <smarru@apache.org>
2013-04-08 23:53:32 +0000

    Adding missing license headers
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1465833 13f79535-47bb-0310-9956-ffa450edef68

--- modules/gfac-core/src/main/java/org/apache/airavata/gfac/provider/utils/ProcessorRequirement.java
+++ modules/gfac-core/src/main/java/org/apache/airavata/gfac/provider/utils/ProcessorRequirement.java
@@ -1,9 +1,26 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
 package org.apache.airavata.gfac.provider.utils;
 
-/**
- * @version $Id: ProcessorType.java,v 1.2 2006/07/25 10:43:39 baklushin Exp $
- * @author Alexei Baklushin
- */
 public enum ProcessorRequirement{
 	sparc("sparc"), //$NON-NLS-1$
 	powerpc("powerpc"), //$NON-NLS-1$
@@ -15,7 +32,6 @@ public enum ProcessorRequirement{
 	ia64("ia64"), //$NON-NLS-1$
 	arm("arm"), //$NON-NLS-1$
 	other("other"); //$NON-NLS-1$
-
 
 	ProcessorRequirement(String value) {
 		this.value = value; 
