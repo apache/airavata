31b3ce9c1d5f7b8b5012d61b4591ec08efbdc27d
Raminderjeet Singh <raminder@apache.org>
2013-04-23 20:55:42 +0000

    fixed AIRAVATA-832. Its not application name its service name not to be set in gfac-config
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1471142 13f79535-47bb-0310-9956-ffa450edef68

--- modules/gfac-core/src/main/java/org/apache/airavata/gfac/GFacAPI.java
+++ modules/gfac-core/src/main/java/org/apache/airavata/gfac/GFacAPI.java
@@ -21,20 +21,19 @@
 
 package org.apache.airavata.gfac;
 
+import java.util.List;
+
 import org.apache.airavata.gfac.context.JobExecutionContext;
 import org.apache.airavata.gfac.handler.GFacHandler;
 import org.apache.airavata.gfac.handler.GFacHandlerConfig;
 import org.apache.airavata.gfac.handler.GFacHandlerException;
 import org.apache.airavata.gfac.notification.events.ExecutionFailEvent;
-import org.apache.airavata.gfac.notification.events.FinishExecutionEvent;
 import org.apache.airavata.gfac.notification.listeners.LoggingListener;
 import org.apache.airavata.gfac.notification.listeners.WorkflowTrackingListener;
 import org.apache.airavata.gfac.provider.GFacProvider;
 import org.apache.airavata.gfac.provider.GFacProviderException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 public class GFacAPI {
     private static final Logger log = LoggerFactory.getLogger(GFacAPI.class);
