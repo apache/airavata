d6a570ed36135de281502086d17508267067ab6d
Raminderjeet Singh <raminder@apache.org>
2013-04-24 21:38:08 +0000

    Fixed AIRAVATA-836. I am not able to find the reason of this code so i just commented context setting code. getting the output staging dir is done in GramOutputHandler
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1471702 13f79535-47bb-0310-9956-ffa450edef68

--- modules/gfac-core/src/main/java/org/apache/airavata/gfac/utils/GramRSLGenerator.java
+++ modules/gfac-core/src/main/java/org/apache/airavata/gfac/utils/GramRSLGenerator.java
@@ -148,10 +148,10 @@ public class GramRSLGenerator {
                 }
             }
         }
-        if(currentContextHeader.getWorkflowOutputDataHandling() != null){
-            if(currentContextHeader.getWorkflowOutputDataHandling().getApplicationOutputDataHandlingArray().length != 0)
-            app.setOutputDataDirectory(currentContextHeader.getWorkflowOutputDataHandling().getApplicationOutputDataHandlingArray()[0].getOutputDataDirectory());
-        }
+//        if(currentContextHeader.getWorkflowOutputDataHandling() != null){
+//            if(currentContextHeader.getWorkflowOutputDataHandling().getApplicationOutputDataHandlingArray().length != 0)
+//            app.setOutputDataDirectory(currentContextHeader.getWorkflowOutputDataHandling().getApplicationOutputDataHandlingArray()[0].getOutputDataDirectory());
+//        }
         }
         if (app.getNodeCount() > 0) {
             jobAttr.set("hostCount", String.valueOf(app.getNodeCount()));
