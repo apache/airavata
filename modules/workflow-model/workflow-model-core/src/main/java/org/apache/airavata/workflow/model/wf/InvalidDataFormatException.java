67ae741932219bfbdaa1d82780d89994b3ca6742
samindaw <samindaw@unknown>
2013-05-23 12:31:29 +0000

    refactoring exception classes for the API etc.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1485663 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/workflow-model/workflow-model-core/src/main/java/org/apache/airavata/workflow/model/wf/InvalidDataFormatException.java
@@ -0,0 +1,34 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.workflow.model.wf;
+
+import org.apache.airavata.common.exception.AiravataException;
+
+public class InvalidDataFormatException extends AiravataException {
+
+	private static final long serialVersionUID = 7633132808620067021L;
+	
+	public InvalidDataFormatException(String type, String data) {
+		super("'"+data+"'"+" is not of type "+type);
+	}
+
+}
