6cdd1923d47df0f2f63653f1cfaf94c98f203eef
chathuri <chathuri@unknown>
2013-06-07 14:52:53 +0000

    adding test class for GFac Job Data
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1490668 13f79535-47bb-0310-9956-ffa450edef68

--- tools/registry-tool/src/main/resources/db-scripts/0.8/migrate_derby.sql
+++ tools/registry-tool/src/main/resources/db-scripts/0.8/migrate_derby.sql
@@ -61,4 +61,4 @@ create table GFac_Job_Data
 
 INSERT INTO GFac_Job_Data(experiment_ID, workflow_instanceID, node_id, application_descriptor_ID, host_descriptor_ID, service_descriptor_ID,
 job_data, local_Job_ID, submitted_time, statusUpdateTime, status, metadata) SELECT null, workflow_instanceID, node_id, null, invoked_host,
-null, rsl, local_Job_ID, null, null, null, null FROM Gram_Data;
+null, null, local_Job_ID, null, null, null, null FROM Gram_Data;
