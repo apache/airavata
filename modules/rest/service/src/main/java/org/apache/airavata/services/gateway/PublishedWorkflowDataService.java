322941d2935d52476c7ac7c0a618f742af3ea1eb
samindaw <samindaw@unknown>
2013-09-06 19:44:51 +0000

    revert commit 1520672
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1520675 13f79535-47bb-0310-9956-ffa450edef68

--- modules/rest/service/src/main/java/org/apache/airavata/services/gateway/PublishedWorkflowDataService.java
+++ modules/rest/service/src/main/java/org/apache/airavata/services/gateway/PublishedWorkflowDataService.java
@@ -2,10 +2,10 @@ package org.apache.airavata.services.gateway;
 
 import javax.ws.rs.Path;
 
+import org.apache.airavata.persistance.registry.jpa.resources.UserWorkflowResource;
 import org.apache.airavata.rest.mappings.utils.ResourcePathConstants.WorkflowDataConstants;
-import org.apache.airavata.services.registry.rest.resources.PublishWorkflowRegistryResource;
 
 @Path(WorkflowDataConstants.PATH)
-public class PublishedWorkflowDataService extends PublishWorkflowRegistryResource {
+public class PublishedWorkflowDataService extends UserWorkflowResource {
 
 }
