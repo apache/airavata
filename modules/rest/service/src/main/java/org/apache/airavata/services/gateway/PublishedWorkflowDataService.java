6aa8e5c4a65237b4b43fca1f0daa6347d3623880
samindaw <samindaw@unknown>
2013-10-21 18:28:42 +0000

    disabling the execution REST service
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1534302 13f79535-47bb-0310-9956-ffa450edef68

--- modules/rest/service/src/main/java/org/apache/airavata/services/gateway/PublishedWorkflowDataService.java
+++ modules/rest/service/src/main/java/org/apache/airavata/services/gateway/PublishedWorkflowDataService.java
@@ -23,10 +23,9 @@ package org.apache.airavata.services.gateway;
 
 import javax.ws.rs.Path;
 
-import org.apache.airavata.persistance.registry.jpa.resources.UserWorkflowResource;
 import org.apache.airavata.rest.mappings.utils.ResourcePathConstants.WorkflowDataConstants;
 
 @Path(WorkflowDataConstants.PATH)
-public class PublishedWorkflowDataService extends UserWorkflowResource {
+public class PublishedWorkflowDataService {
 
 }
