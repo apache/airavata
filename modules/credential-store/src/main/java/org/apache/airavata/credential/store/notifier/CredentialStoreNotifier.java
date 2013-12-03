0d033ba1869b3cd085f85d12790a73ff11f7832e
amilaj <amilaj@unknown>
2013-12-03 22:14:55 +0000

    Adding preliminary files for email notifier in credential store
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1547606 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/credential-store/src/main/java/org/apache/airavata/credential/store/notifier/CredentialStoreNotifier.java
@@ -0,0 +1,42 @@
+package org.apache.airavata.credential.store.notifier;/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+
+import org.apache.airavata.credential.store.store.CredentialStoreException;
+
+/**
+ * This class is used to notify particular entity with expiring credentials.
+ * The default implementation uses email messages.
+ * User: AmilaJ (amilaj@apache.org)
+ * Date: 12/3/13
+ * Time: 4:17 PM
+ */
+public interface CredentialStoreNotifier {
+
+    /**
+     * The specific notifier implementation needs to implement following method.
+     * This method should actually deliver message to desired entity.
+     * @param message The actual message encapsulated
+     * @throws CredentialStoreException
+     */
+    void notifyMessage(NotificationMessage message) throws CredentialStoreException;
+
+}
---
+++ modules/credential-store/src/main/java/org/apache/airavata/credential/store/notifier/impl/EmailNotifier.java
@@ -0,0 +1,71 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.credential.store.notifier.impl;
+
+import org.apache.airavata.credential.store.notifier.CredentialStoreNotifier;
+import org.apache.airavata.credential.store.notifier.NotificationMessage;
+import org.apache.airavata.credential.store.store.CredentialStoreException;
+import org.apache.commons.mail.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * User: AmilaJ (amilaj@apache.org)
+ * Date: 12/3/13
+ * Time: 4:25 PM
+ */
+
+public class EmailNotifier implements CredentialStoreNotifier {
+
+    protected static Logger log = LoggerFactory.getLogger(EmailNotifier.class);
+
+    private EmailNotifierConfiguration emailNotifierConfiguration;
+
+    public EmailNotifier(EmailNotifierConfiguration notifierConfiguration) {
+        this.emailNotifierConfiguration = notifierConfiguration;
+    }
+
+    public void notifyMessage(NotificationMessage message) throws CredentialStoreException {
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName(this.emailNotifierConfiguration.getEmailServer());
+            email.setSmtpPort(this.emailNotifierConfiguration.getEmailServerPort());
+            email.setAuthenticator(new DefaultAuthenticator(this.emailNotifierConfiguration.getEmailUserName(),
+                    this.emailNotifierConfiguration.getEmailPassword()));
+            email.setSSLOnConnect(this.emailNotifierConfiguration.isSslConnect());
+            email.setFrom(this.emailNotifierConfiguration.getFromAddress());
+
+            EmailNotificationMessage emailMessage = (EmailNotificationMessage)message;
+
+            email.setSubject(emailMessage.getSubject());
+            email.setMsg(emailMessage.getMessage());
+            email.addTo(emailMessage.getSenderEmail());
+            email.send();
+
+        } catch (EmailException e) {
+            log.error("[CredentialStore]Error sending email notification message.");
+            throw new CredentialStoreException("Error sending email notification message", e);
+        }
+
+
+    }
+}
