661d6eb25912df613815e860451a460b59e355d2
Raminderjeet Singh <raminder@apache.org>
2014-01-09 19:12:27 +0000

    orchestrator data
    git-svn-id: https://svn.apache.org/repos/asf/airavata/trunk@1556922 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/registry/registry-api/src/main/java/org/apache/airavata/registry/api/orchestrator/OrchestratorData.java
@@ -0,0 +1,85 @@
+package org.apache.airavata.registry.api.orchestrator;
+
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+import org.apache.airavata.registry.api.orchestrator.impl.OrchestratorDataImpl;
+
+@WebService
+@XmlSeeAlso(OrchestratorDataImpl.class)
+public interface OrchestratorData {
+
+	/**
+	 * Returns the orchestrator run id
+	 * 
+	 * @return
+	 */
+	public String getOrchestratorId();
+
+	/**
+	 * 
+	 * @return the unique experiment id
+	 */
+	public String getExperimentId();
+
+	/**
+	 * Returns the user of the run
+	 * 
+	 * @return
+	 */
+	public String getUser();
+
+	/**
+	 * Returns GFAC service URL
+	 * 
+	 * @return
+	 */
+	public String getGFACServiceEPR();
+
+	/**
+	 * Returns state of processing
+	 * 
+	 * @return
+	 */
+	public String getState();
+
+	/**
+	 * Returns run status
+	 * 
+	 * @return
+	 */
+	public String getStatus();
+
+	/**
+	 * 
+	 * @param experimentId
+	 */
+	public void setExperimentId(String experimentId);
+
+	/**
+	 * 
+	 * @param user
+	 */
+	public void setUser(String user);
+
+	/**
+	 * 
+	 * @param gfacEPR
+	 */
+	public void setGFACServiceEPR(String gfacEPR);
+	
+	/**
+	 * 
+	 * @param state
+	 */
+	public void setState(String state);
+	
+	/**
+	 * 
+	 * @param status
+	 */
+	public void setStatus(String status);
+
+
+	
+}
