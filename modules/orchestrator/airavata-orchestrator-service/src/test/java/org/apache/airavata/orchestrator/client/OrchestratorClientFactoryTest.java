bee69b46a1a39a4ed7f063d066294292f28c0e0a
lahiru <lahiru@apache.org>
2014-03-03 13:15:09 -0500

    fixing a todo after output data saving is fixed by chathuri - AIRAVATA-1028

--- modules/orchestrator/airavata-orchestrator-service/src/test/java/org/apache/airavata/orchestrator/client/OrchestratorClientFactoryTest.java
+++ modules/orchestrator/airavata-orchestrator-service/src/test/java/org/apache/airavata/orchestrator/client/OrchestratorClientFactoryTest.java
@@ -100,7 +100,8 @@ public class OrchestratorClientFactoryTest {
                         output.setValue("");
                         exOut.add(output);
 
-                        Experiment simpleExperiment = ExperimentModelUtil.createSimpleExperiment("project1", "admin", "echoExperiment", "SimpleEcho2", "SimpleEcho2", exInputs);
+                        Experiment simpleExperiment = ExperimentModelUtil.createSimpleExperiment("project1",
+                                "admin", "echoExperiment", "SimpleEcho2", "SimpleEcho2", exInputs);
                         simpleExperiment.setExperimentOutputs(exOut);
 
                         ComputationalResourceScheduling scheduling = ExperimentModelUtil.createComputationResourceScheduling("trestles.sdsc.edu", 1, 1, 1, "normal", 0, 0, 1, "sds128");
