a750ddd023996f4195fc351ef3a51fd167b4f727
lahiru <lahiru@apache.org>
2014-03-04 11:14:48 -0500

    Removing duplicated configuration - AIRAVATA-1028

--- airavata-api/airavata-api-server/src/main/java/org/apache/airavata/api/server/handler/AiravataServerHandler.java
+++ airavata-api/airavata-api-server/src/main/java/org/apache/airavata/api/server/handler/AiravataServerHandler.java
@@ -43,7 +43,6 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class AiravataServerHandler implements Airavata.Iface {
 
