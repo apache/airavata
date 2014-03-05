7cf63c7cccdbd83f7567efd32e80b83bef036deb
Saminda Wijeratne <samindaw@gmail.com>
2014-03-04 22:27:23 -0500

    https://issues.apache.org/jira/browse/AIRAVATA-1046

---
+++ modules/commons/utils/src/main/java/org/apache/airavata/common/utils/IServer.java
@@ -0,0 +1,46 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.common.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public interface IServer {
+	public enum ServerStatus{
+		STOPPED,
+		STARTING,
+		STARTED,
+		FAILED;
+		public void updateTime(){
+			now=Calendar.getInstance().getTime();
+		}
+		private Date now;
+		public Date getTime(){
+			return now;
+		}
+	}
+	public void start() throws Exception;
+	public void stop() throws Exception;
+	public void restart() throws Exception;
+	public void configure() throws Exception;
+	public ServerStatus getStatus() throws Exception;
+}
