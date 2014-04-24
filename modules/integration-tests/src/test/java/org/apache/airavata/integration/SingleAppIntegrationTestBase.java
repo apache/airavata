740bf8cd88c95ee4ba3327a98a825ffd41084218
Saminda Wijeratne <samindaw@gmail.com>
2014-04-24 01:47:06 -0700

    adding new integration tests to retrieve project and experiment related data

--- modules/integration-tests/src/test/java/org/apache/airavata/integration/SingleAppIntegrationTestBase.java
+++ modules/integration-tests/src/test/java/org/apache/airavata/integration/SingleAppIntegrationTestBase.java
@@ -20,19 +20,11 @@
  */
 package org.apache.airavata.integration;
 
-import java.io.IOException;
 import java.util.Date;
 
-import org.apache.airavata.api.Airavata;
-import org.apache.airavata.api.client.AiravataClientFactory;
-import org.apache.airavata.api.error.AiravataClientException;
 import org.apache.airavata.api.error.AiravataSystemException;
 import org.apache.airavata.api.error.ExperimentNotFoundException;
 import org.apache.airavata.api.error.InvalidRequestException;
-import org.apache.airavata.client.AiravataAPIFactory;
-import org.apache.airavata.client.api.AiravataAPI;
-import org.apache.airavata.client.api.exception.AiravataAPIInvocationException;
-import org.apache.airavata.common.utils.ClientSettings;
 import org.apache.airavata.model.workspace.experiment.Experiment;
 import org.apache.airavata.model.workspace.experiment.ExperimentState;
 import org.apache.airavata.model.workspace.experiment.ExperimentStatus;
@@ -43,75 +35,14 @@ import org.slf4j.LoggerFactory;
 /**
  * this class contains the common utils across the single application integration tests
  */
-public class SingleAppIntegrationTestBase {
+public class SingleAppIntegrationTestBase extends AbstractIntegrationTest {
 
-    private final static Logger log = LoggerFactory.getLogger(SingleAppIntegrationTestBase.class);
-    private static String THRIFT_SERVER_HOST;
-    private static int THRIFT_SERVER_PORT;
-    protected AiravataAPI airavataAPI;
-    protected Airavata.Client client;
-    private final int TRIES = 5;
-    private final int TIME_OUT = 10000;
-
-    //initializes the server
-    protected void init() {
-
-        try {
-            THRIFT_SERVER_HOST = ClientSettings.getSetting("thrift.server.host");
-            THRIFT_SERVER_PORT = Integer.parseInt(ClientSettings.getSetting("thrift.server.port"));
-
-            //check the server startup + initialize the thrift client
-            initClient();
-
-            //getting the Airavata API ( to add the descriptors
-            this.airavataAPI = getAiravataAPI();
-        } catch (IOException e) {
-            log.error("Error loading client-properties ..." + e.getMessage());
-        } catch (AiravataAPIInvocationException e) {
-            log.error("Error initializing the Airavata API ... " + e.getMessage());
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    protected AiravataAPI getAiravataAPI() throws AiravataAPIInvocationException {
-        if (airavataAPI == null) {
-            airavataAPI = AiravataAPIFactory.getAPI("default", "admin");
-        }
-        return airavataAPI;
-    }
-
-    /*
-    * Check if the thrift server has started.
-    * If so, initialize the client
-    * */
-    protected void initClient() throws Exception {
-        int tries = 0;
-        while (client==null) {
-        	log.info("Waiting till server initializes ........[try "+ (++tries) + " of "+TRIES+"]");
-            try {
-                client = AiravataClientFactory.createAiravataClient(THRIFT_SERVER_HOST, THRIFT_SERVER_PORT);
-            } catch (Exception e) { 
-            	if (tries == TRIES) {
-					log("Server not responding. Cannot continue with integration tests ...");
-					throw e;
-				} else {
-					Thread.sleep(TIME_OUT);
-				}
-            }
-        }
-    }
-
-    protected String createExperiment(Experiment experiment) throws AiravataSystemException, InvalidRequestException, AiravataClientException, TException {
+    protected String createExperiment(Experiment experiment) throws AiravataSystemException, InvalidRequestException, TException {
         return getClient().createExperiment(experiment);
     }
 
-    protected void launchExperiment(String expId) throws ExperimentNotFoundException, AiravataSystemException, InvalidRequestException, AiravataClientException, TException {
+    protected void launchExperiment(String expId) throws ExperimentNotFoundException, AiravataSystemException, InvalidRequestException, TException {
         getClient().launchExperiment(expId, "testToken");
-    }
-
-    protected Airavata.Client getClient() {
-        return client;
     }
 
     //monitoring the job
@@ -145,10 +76,6 @@ public class SingleAppIntegrationTestBase {
         } catch (InterruptedException e) {
             log.error("Thread interrupted..", e.getMessage());
         }
-    }
-
-    public void log(String message) {
-        log.info(message);
     }
 
 }
