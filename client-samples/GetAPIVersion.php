2c6ccedd706eaab7a8cf2b8a54a265bfeb34064e
Suresh Marru <smarru@apache.org>
2014-05-22 14:32:18 -0400

    Adding the client php to required

--- client-samples/GetAPIVersion.php
+++ client-samples/GetAPIVersion.php
@@ -5,12 +5,14 @@ require_once 'samples_config.php';
 
 $GLOBALS['THRIFT_ROOT'] = '../lib/Thrift/';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Transport/TTransport.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Transport/TBufferedTransport.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Transport/TSocket.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Protocol/TProtocol.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Protocol/TBinaryProtocol.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TException.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TApplicationException.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TProtocolException.php';
+require_once $GLOBALS['THRIFT_ROOT'] . 'Exception/TTransportException.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Base/TBase.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Type/TType.php';
 require_once $GLOBALS['THRIFT_ROOT'] . 'Type/TMessageType.php';
@@ -20,8 +22,11 @@ require_once $GLOBALS['THRIFT_ROOT'] . 'StringFunc/Core.php';
 
 $GLOBALS['AIRAVATA_ROOT'] = '../lib/Airavata/';
 require_once $GLOBALS['AIRAVATA_ROOT'] . 'API/Airavata.php';
+require_once $GLOBALS['AIRAVATA_ROOT'] . 'Model/Workspace/Types.php';
 require_once $GLOBALS['AIRAVATA_ROOT'] . 'Model/Workspace/Experiment/Types.php';
 require_once $GLOBALS['AIRAVATA_ROOT'] . 'API/Error/Types.php';
+
+require_once '../lib/AiravataClientFactory.php';
 
 use Airavata\API\Error\AiravataClientException;
 use Airavata\API\Error\AiravataSystemException;
@@ -35,22 +40,29 @@ use Airavata\API\AiravataClient;
 /* this is the same as the factory */
 $transport = new TSocket(AIRAVATA_SERVER, AIRAVATA_PORT);
 $transport->setRecvTimeout(AIRAVATA_TIMEOUT);
+
+$protocol = new TBinaryProtocol($transport);
 $transport->open();
 $airavataclient = new AiravataClient($protocol);
-
 
 try
 {
     $version = $airavataclient->GetAPIVersion();
 }
-catch (TException $texp)
+catch (InvalidRequestException $ire)
 {
-    print 'Exception: ' . $texp->getMessage()."\n";
+    print 'InvalidRequestException: ' . $ire->getMessage()."\n";
+}
+catch (AiravataClientException $ace)
+{
+    print 'Airavata System Exception: ' . $ace->getMessage()."\n";
+}
+catch (AiravataSystemException $ase)
+{
+    print 'Airavata System Exception: ' . $ase->getMessage()."\n";
 }
 
-
-echo 'Airavata server version is ' . $version;
-
+echo 'Airavata API Version is ' . $version;
 
 $transport->close();
 
