cc8d7cbbcc5b85cd17c6375d34736bddec3b9bb0
Sachith Withana <sachith@apache.org>
2014-06-20 09:19:30 +0530

    fixed scheduling info

--- modules/app-catalog/app-catalog-jpa/src/main/java/org/apache/airavata/persistence/appcatalog/jpa/model/ApplicationDeployment.java
+++ modules/app-catalog/app-catalog-jpa/src/main/java/org/apache/airavata/persistence/appcatalog/jpa/model/ApplicationDeployment.java
@@ -26,4 +26,44 @@ public class ApplicationDeployment {
     @ManyToOne(cascade = CascadeType.REMOVE)
     @JoinColumn(name = "APPLICATION_ID")
     private Application application;
+
+    public String getDeploymentID() {
+        return deploymentID;
+    }
+
+    public void setDeploymentID(String deploymentID) {
+        this.deploymentID = deploymentID;
+    }
+
+    public String getApplicationID() {
+        return applicationID;
+    }
+
+    public void setApplicationID(String applicationID) {
+        this.applicationID = applicationID;
+    }
+
+    public String getDeploymentHostName() {
+        return deploymentHostName;
+    }
+
+    public void setDeploymentHostName(String deploymentHostName) {
+        this.deploymentHostName = deploymentHostName;
+    }
+
+    public Deployment getDeployment() {
+        return deployment;
+    }
+
+    public void setDeployment(Deployment deployment) {
+        this.deployment = deployment;
+    }
+
+    public Application getApplication() {
+        return application;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
 }
