9e225f8b9f49b74f2d2cf60f8e2a6fe94661307a
Chathuri Wimalasena <kamalasini@gmail.com>
2014-06-27 15:25:02 -0400

    adding app deployement related resource classes - AIRAVATA-1302

--- modules/app-catalog/app-catalog-data/src/main/java/org/apache/aiaravata/application/catalog/data/resources/AppDeploymentResource.java
+++ modules/app-catalog/app-catalog-data/src/main/java/org/apache/aiaravata/application/catalog/data/resources/AppDeploymentResource.java
@@ -22,6 +22,8 @@
 package org.apache.aiaravata.application.catalog.data.resources;
 
 import org.airavata.appcatalog.cpi.AppCatalogException;
+import org.apache.aiaravata.application.catalog.data.model.ApplicationDeployment;
+import org.apache.aiaravata.application.catalog.data.model.ApplicationModule;
 import org.apache.aiaravata.application.catalog.data.model.ComputeResource;
 import org.apache.aiaravata.application.catalog.data.util.AppCatalogJPAUtils;
 import org.apache.aiaravata.application.catalog.data.util.AppCatalogQueryGenerator;
@@ -35,52 +37,79 @@ import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ComputeHostResource extends AbstractResource {
-    private final static Logger logger = LoggerFactory.getLogger(ComputeHostResource.class);
-    private String resoureId;
-    private String hostName;
-    private String description;
-    private String scratchLocation;
-    private String preferredJobSubmissionProtocol;
+public class AppDeploymentResource extends AbstractResource {
+    private final static Logger logger = LoggerFactory.getLogger(AppDeploymentResource.class);
+    private String deploymentId;
+    private String appModuleId;
+    private String hostId;
+    private String executablePath;
+    private String appDes;
+    private String envModuleLoadCMD;
+    private ComputeHostResource hostResource;
+    private AppModuleResource moduleResource;
 
-    public String getResoureId() {
-        return resoureId;
+    public String getDeploymentId() {
+        return deploymentId;
     }
 
-    public void setResoureId(String resoureId) {
-        this.resoureId = resoureId;
+    public void setDeploymentId(String deploymentId) {
+        this.deploymentId = deploymentId;
     }
 
-    public String getHostName() {
-        return hostName;
+    public String getAppModuleId() {
+        return appModuleId;
     }
 
-    public void setHostName(String hostName) {
-        this.hostName = hostName;
+    public void setAppModuleId(String appModuleId) {
+        this.appModuleId = appModuleId;
     }
 
-    public String getDescription() {
-        return description;
+    public String getHostId() {
+        return hostId;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setHostId(String hostId) {
+        this.hostId = hostId;
     }
 
-    public String getScratchLocation() {
-        return scratchLocation;
+    public String getExecutablePath() {
+        return executablePath;
     }
 
-    public void setScratchLocation(String scratchLocation) {
-        this.scratchLocation = scratchLocation;
+    public void setExecutablePath(String executablePath) {
+        this.executablePath = executablePath;
     }
 
-    public String getPreferredJobSubmissionProtocol() {
-        return preferredJobSubmissionProtocol;
+    public String getAppDes() {
+        return appDes;
     }
 
-    public void setPreferredJobSubmissionProtocol(String preferredJobSubmissionProtocol) {
-        this.preferredJobSubmissionProtocol = preferredJobSubmissionProtocol;
+    public void setAppDes(String appDes) {
+        this.appDes = appDes;
+    }
+
+    public String getEnvModuleLoadCMD() {
+        return envModuleLoadCMD;
+    }
+
+    public void setEnvModuleLoadCMD(String envModuleLoadCMD) {
+        this.envModuleLoadCMD = envModuleLoadCMD;
+    }
+
+    public ComputeHostResource getHostResource() {
+        return hostResource;
+    }
+
+    public void setHostResource(ComputeHostResource hostResource) {
+        this.hostResource = hostResource;
+    }
+
+    public AppModuleResource getModuleResource() {
+        return moduleResource;
+    }
+
+    public void setModuleResource(AppModuleResource moduleResource) {
+        this.moduleResource = moduleResource;
     }
 
     @Override
@@ -89,8 +118,8 @@ public class ComputeHostResource extends AbstractResource {
         try {
             em = AppCatalogJPAUtils.getEntityManager();
             em.getTransaction().begin();
-            AppCatalogQueryGenerator generator= new AppCatalogQueryGenerator(COMPUTE_RESOURCE);
-            generator.setParameter(ComputeResourceConstants.RESOURCE_ID, identifier);
+            AppCatalogQueryGenerator generator= new AppCatalogQueryGenerator(APPLICATION_DEPLOYMENT);
+            generator.setParameter(ApplicationDeploymentConstants.DEPLOYMENT_ID, identifier);
             Query q = generator.deleteQuery(em);
             q.executeUpdate();
             em.getTransaction().commit();
@@ -114,15 +143,15 @@ public class ComputeHostResource extends AbstractResource {
         try {
             em = AppCatalogJPAUtils.getEntityManager();
             em.getTransaction().begin();
-            AppCatalogQueryGenerator generator = new AppCatalogQueryGenerator(COMPUTE_RESOURCE);
-            generator.setParameter(ComputeResourceConstants.RESOURCE_ID, identifier);
+            AppCatalogQueryGenerator generator = new AppCatalogQueryGenerator(APPLICATION_DEPLOYMENT);
+            generator.setParameter(ApplicationDeploymentConstants.DEPLOYMENT_ID, identifier);
             Query q = generator.selectQuery(em);
-            ComputeResource computeResource = (ComputeResource) q.getSingleResult();
-            ComputeHostResource computeHostResource =
-                    (ComputeHostResource) AppCatalogJPAUtils.getResource(AppCatalogResourceType.COMPUTE_RESOURCE, computeResource);
+            ApplicationDeployment deployment = (ApplicationDeployment) q.getSingleResult();
+            AppDeploymentResource deploymentResource =
+                    (AppDeploymentResource) AppCatalogJPAUtils.getResource(AppCatalogResourceType.APPLICATION_DEPLOYMENT, deployment);
             em.getTransaction().commit();
             em.close();
-            return computeHostResource;
+            return deploymentResource;
         } catch (ApplicationSettingsException e) {
             logger.error(e.getMessage(), e);
             throw new AppCatalogException(e);
@@ -138,43 +167,43 @@ public class ComputeHostResource extends AbstractResource {
 
     @Override
     public List<Resource> get(String fieldName, Object value) throws AppCatalogException {
-        List<Resource> computeHostResources = new ArrayList<Resource>();
+        List<Resource> appDeployments = new ArrayList<Resource>();
         EntityManager em = null;
         try {
             em = AppCatalogJPAUtils.getEntityManager();
             em.getTransaction().begin();
             Query q;
-            AppCatalogQueryGenerator generator = new AppCatalogQueryGenerator(COMPUTE_RESOURCE);
+            AppCatalogQueryGenerator generator = new AppCatalogQueryGenerator(APPLICATION_DEPLOYMENT);
             List results;
-            if (fieldName.equals(ComputeResourceConstants.HOST_NAME)) {
-                generator.setParameter(ComputeResourceConstants.HOST_NAME, value);
+            if (fieldName.equals(ApplicationDeploymentConstants.APP_MODULE_ID)) {
+                generator.setParameter(ApplicationDeploymentConstants.APP_MODULE_ID, value);
                 q = generator.selectQuery(em);
                 results = q.getResultList();
                 if (results.size() != 0) {
                     for (Object result : results) {
-                        ComputeResource computeResource = (ComputeResource) result;
-                        ComputeHostResource computeHostResource =
-                                (ComputeHostResource) AppCatalogJPAUtils.getResource(AppCatalogResourceType.COMPUTE_RESOURCE, computeResource);
-                        computeHostResources.add(computeHostResource);
+                        ApplicationDeployment deployment = (ApplicationDeployment) result;
+                        AppDeploymentResource deploymentResource =
+                                (AppDeploymentResource) AppCatalogJPAUtils.getResource(AppCatalogResourceType.APPLICATION_DEPLOYMENT, deployment);
+                        appDeployments.add(deploymentResource);
                     }
                 }
-            } else if (fieldName.equals(ComputeResourceConstants.PREFERED_SUBMISSION_PROTOCOL)) {
-                generator.setParameter(ComputeResourceConstants.PREFERED_SUBMISSION_PROTOCOL, value);
+            } else if (fieldName.equals(ApplicationDeploymentConstants.COMPUTE_HOST_ID)) {
+                generator.setParameter(ApplicationDeploymentConstants.COMPUTE_HOST_ID, value);
                 q = generator.selectQuery(em);
                 results = q.getResultList();
                 if (results.size() != 0) {
                     for (Object result : results) {
-                        ComputeResource computeResource = (ComputeResource) result;
-                        ComputeHostResource projectResource =
-                                (ComputeHostResource) AppCatalogJPAUtils.getResource(AppCatalogResourceType.COMPUTE_RESOURCE, computeResource);
-                        computeHostResources.add(projectResource);
+                        ApplicationDeployment deployment = (ApplicationDeployment) result;
+                        AppDeploymentResource deploymentResource =
+                                (AppDeploymentResource) AppCatalogJPAUtils.getResource(AppCatalogResourceType.APPLICATION_DEPLOYMENT, deployment);
+                        appDeployments.add(deploymentResource);
                     }
                 }
-            } else {
+            }else {
                 em.getTransaction().commit();
                 em.close();
-                logger.error("Unsupported field name for compute resource.", new IllegalArgumentException());
-                throw new IllegalArgumentException("Unsupported field name for compute resource.");
+                logger.error("Unsupported field name for app deployment resource.", new IllegalArgumentException());
+                throw new IllegalArgumentException("Unsupported field name for app deployment resource.");
             }
             em.getTransaction().commit();
             em.close();
@@ -189,44 +218,44 @@ public class ComputeHostResource extends AbstractResource {
                 em.close();
             }
         }
-        return computeHostResources;
+        return appDeployments;
     }
 
     @Override
     public List<String> getIds(String fieldName, Object value) throws AppCatalogException {
-        List<String> computeHostResourceIDs = new ArrayList<String>();
+        List<String> appDeployments = new ArrayList<String>();
         EntityManager em = null;
         try {
             em = AppCatalogJPAUtils.getEntityManager();
             em.getTransaction().begin();
             Query q;
-            AppCatalogQueryGenerator generator = new AppCatalogQueryGenerator(COMPUTE_RESOURCE);
+            AppCatalogQueryGenerator generator = new AppCatalogQueryGenerator(APPLICATION_DEPLOYMENT);
             List results;
-            if (fieldName.equals(ComputeResourceConstants.HOST_NAME)) {
-                generator.setParameter(ComputeResourceConstants.HOST_NAME, value);
+            if (fieldName.equals(ApplicationDeploymentConstants.APP_MODULE_ID)) {
+                generator.setParameter(ApplicationDeploymentConstants.APP_MODULE_ID, value);
                 q = generator.selectQuery(em);
                 results = q.getResultList();
                 if (results.size() != 0) {
                     for (Object result : results) {
-                        ComputeResource computeResource = (ComputeResource) result;
-                        computeHostResourceIDs.add(computeResource.getResourceID());
+                        ApplicationDeployment deployment = (ApplicationDeployment) result;
+                        appDeployments.add(deployment.getDeployementID());
                     }
                 }
-            } else if (fieldName.equals(ComputeResourceConstants.PREFERED_SUBMISSION_PROTOCOL)) {
-                generator.setParameter(ComputeResourceConstants.PREFERED_SUBMISSION_PROTOCOL, value);
+            } else if (fieldName.equals(ApplicationDeploymentConstants.COMPUTE_HOST_ID)) {
+                generator.setParameter(ApplicationDeploymentConstants.COMPUTE_HOST_ID, value);
                 q = generator.selectQuery(em);
                 results = q.getResultList();
                 if (results.size() != 0) {
                     for (Object result : results) {
-                        ComputeResource computeResource = (ComputeResource) result;
-                        computeHostResourceIDs.add(computeResource.getResourceID());
+                        ApplicationDeployment deployment = (ApplicationDeployment) result;
+                        appDeployments.add(deployment.getDeployementID());
                     }
                 }
-            } else {
+            }else {
                 em.getTransaction().commit();
                 em.close();
-                logger.error("Unsupported field name for compute resource.", new IllegalArgumentException());
-                throw new IllegalArgumentException("Unsupported field name for compute resource.");
+                logger.error("Unsupported field name for app deployment resource.", new IllegalArgumentException());
+                throw new IllegalArgumentException("Unsupported field name for app deployment resource.");
             }
             em.getTransaction().commit();
             em.close();
@@ -241,7 +270,7 @@ public class ComputeHostResource extends AbstractResource {
                 em.close();
             }
         }
-        return computeHostResourceIDs;
+        return appDeployments;
     }
 
     @Override
@@ -249,25 +278,30 @@ public class ComputeHostResource extends AbstractResource {
         EntityManager em = null;
         try {
             em = AppCatalogJPAUtils.getEntityManager();
-            ComputeResource existingComputeResource = em.find(ComputeResource.class, resoureId);
+            ApplicationDeployment existingDeployment = em.find(ApplicationDeployment.class, deploymentId);
             em.close();
 
             em = AppCatalogJPAUtils.getEntityManager();
             em.getTransaction().begin();
-            if (existingComputeResource !=  null){
-                existingComputeResource.setHostName(hostName);
-                existingComputeResource.setDescription(description);
-                existingComputeResource.setPreferredJobSubProtocol(preferredJobSubmissionProtocol);
-                existingComputeResource.setScratchLocation(scratchLocation);
-                em.merge(existingComputeResource);
+            ApplicationModule applicationModule = em.find(ApplicationModule.class, appModuleId);
+            ComputeResource computeHost = em.find(ComputeResource.class, hostId);
+            if (existingDeployment !=  null){
+                existingDeployment.setApplicationDesc(appDes);
+                existingDeployment.setAppModuleID(appModuleId);
+                existingDeployment.setApplicationModule(applicationModule);
+                existingDeployment.setComputeResource(computeHost);
+                existingDeployment.setExecutablePath(executablePath);
+                existingDeployment.setEnvModuleLoaString(envModuleLoadCMD);
+                em.merge(existingDeployment);
             }else {
-                ComputeResource computeResource = new ComputeResource();
-                computeResource.setResourceID(resoureId);
-                computeResource.setHostName(hostName);
-                computeResource.setDescription(description);
-                computeResource.setPreferredJobSubProtocol(preferredJobSubmissionProtocol);
-                computeResource.setScratchLocation(scratchLocation);
-                em.persist(computeResource);
+                ApplicationDeployment deployment  = new ApplicationDeployment();
+                deployment.setApplicationDesc(appDes);
+                deployment.setAppModuleID(appModuleId);
+                deployment.setApplicationModule(applicationModule);
+                deployment.setComputeResource(computeHost);
+                deployment.setExecutablePath(executablePath);
+                deployment.setEnvModuleLoaString(envModuleLoadCMD);
+                em.persist(deployment);
             }
             em.getTransaction().commit();
             em.close();
@@ -282,6 +316,7 @@ public class ComputeHostResource extends AbstractResource {
                 em.close();
             }
         }
+
     }
 
     @Override
@@ -289,9 +324,9 @@ public class ComputeHostResource extends AbstractResource {
         EntityManager em = null;
         try {
             em = AppCatalogJPAUtils.getEntityManager();
-            ComputeResource computeResource = em.find(ComputeResource.class, identifier);
+            ApplicationDeployment deployment = em.find(ApplicationDeployment.class, identifier);
             em.close();
-            return computeResource != null;
+            return deployment != null;
         } catch (ApplicationSettingsException e) {
             logger.error(e.getMessage(), e);
             throw new AppCatalogException(e);
--- modules/app-catalog/app-catalog-data/src/main/java/org/apache/aiaravata/application/catalog/data/util/AppCatalogJPAUtils.java
+++ modules/app-catalog/app-catalog-data/src/main/java/org/apache/aiaravata/application/catalog/data/util/AppCatalogJPAUtils.java
@@ -21,8 +21,8 @@
 
 package org.apache.aiaravata.application.catalog.data.util;
 
-import org.apache.aiaravata.application.catalog.data.resources.ComputeHostResource;
-import org.apache.aiaravata.application.catalog.data.resources.Resource;
+import org.apache.aiaravata.application.catalog.data.model.*;
+import org.apache.aiaravata.application.catalog.data.resources.*;
 import org.apache.airavata.common.exception.ApplicationSettingsException;
 import org.apache.airavata.common.utils.ServerSettings;
 import org.slf4j.Logger;
@@ -87,11 +87,53 @@ public class AppCatalogJPAUtils {
     public static Resource getResource(AppCatalogResourceType type, Object o) {
         switch (type){
             case COMPUTE_RESOURCE:
-                if (o instanceof ComputeHostResource){
-                    return createComputeResource((ComputeHostResource)o);
+                if (o instanceof ComputeResource){
+                    return createComputeResource((ComputeResource) o);
                 }else {
                     logger.error("Object should be a Compute Resource.", new IllegalArgumentException());
                     throw new IllegalArgumentException("Object should be a Compute Resource.");
+                }
+            case HOST_ALIAS:
+                if (o instanceof HostAlias){
+                    return createHostAlias((HostAlias) o);
+                }else {
+                    logger.error("Object should be a Host Alias.", new IllegalArgumentException());
+                    throw new IllegalArgumentException("Object should be a Host Alias.");
+                }
+            case HOST_IPADDRESS:
+                if (o instanceof HostIPAddress){
+                    return createHostIPAddress((HostIPAddress) o);
+                }else {
+                    logger.error("Object should be a Host IPAdress.", new IllegalArgumentException());
+                    throw new IllegalArgumentException("Object should be a Host IPAdress.");
+                }
+            case APPLICATION_MODULE:
+                if (o instanceof ApplicationModule){
+                    return createApplicationModule((ApplicationModule) o);
+                }else {
+                    logger.error("Object should be a Application Module.", new IllegalArgumentException());
+                    throw new IllegalArgumentException("Object should be a Application Module.");
+                }
+            case APPLICATION_DEPLOYMENT:
+                if (o instanceof ApplicationDeployment){
+                    return createApplicationDeployment((ApplicationDeployment) o);
+                }else {
+                    logger.error("Object should be a Application Deployment.", new IllegalArgumentException());
+                    throw new IllegalArgumentException("Object should be a Application Deployment.");
+                }
+            case LIBRARY_PREPAND_PATH:
+                if (o instanceof LibraryPrepandPath){
+                    return createLibraryPrepPathResource((LibraryPrepandPath) o);
+                }else {
+                    logger.error("Object should be a Library Prepand path.", new IllegalArgumentException());
+                    throw new IllegalArgumentException("Object should be a Library Prepand path.");
+                }
+            case LIBRARY_APEND_PATH:
+                if (o instanceof LibraryApendPath){
+                    return createLibraryApendPathResource((LibraryApendPath) o);
+                }else {
+                    logger.error("Object should be a Library Apend path.", new IllegalArgumentException());
+                    throw new IllegalArgumentException("Object should be a Library Apend.");
                 }
             default:
                 logger.error("Illegal data type..", new IllegalArgumentException());
@@ -99,13 +141,67 @@ public class AppCatalogJPAUtils {
         }
     }
 
-    private static Resource createComputeResource(ComputeHostResource o) {
+    private static Resource createComputeResource(ComputeResource o) {
         ComputeHostResource hostResource = new ComputeHostResource();
-        hostResource.setResoureId(o.getResoureId());
+        hostResource.setResoureId(o.getResourceID());
         hostResource.setHostName(o.getHostName());
         hostResource.setDescription(o.getDescription());
         hostResource.setScratchLocation(o.getScratchLocation());
-        hostResource.setPreferredJobSubmissionProtocol(o.getPreferredJobSubmissionProtocol());
+        hostResource.setPreferredJobSubmissionProtocol(o.getPreferredJobSubProtocol());
         return hostResource;
+    }
+
+    private static Resource createHostAlias(HostAlias o) {
+        HostAliasResource aliasResource = new HostAliasResource();
+        aliasResource.setResourceID(o.getResourceID());
+        aliasResource.setAlias(o.getAlias());
+        return aliasResource;
+    }
+
+    private static Resource createHostIPAddress(HostIPAddress o) {
+        HostAliasResource aliasResource = new HostAliasResource();
+        aliasResource.setResourceID(o.getResourceID());
+        aliasResource.setAlias(o.getIpaddress());
+        return aliasResource;
+    }
+
+    private static Resource createApplicationModule(ApplicationModule o) {
+        AppModuleResource moduleResource = new AppModuleResource();
+        moduleResource.setModuleId(o.getModuleID());
+        moduleResource.setModuleDesc(o.getModuleDesc());
+        moduleResource.setModuleName(o.getModuleName());
+        moduleResource.setModuleVersion(o.getModuleVersion());
+        return moduleResource;
+    }
+
+    private static Resource createApplicationDeployment(ApplicationDeployment o) {
+        AppDeploymentResource resource = new AppDeploymentResource();
+        resource.setDeploymentId(o.getDeployementID());
+        resource.setAppDes(o.getApplicationDesc());
+        resource.setAppModuleId(o.getAppModuleID());
+        resource.setEnvModuleLoadCMD(o.getEnvModuleLoaString());
+        resource.setHostId(o.getHostID());
+        resource.setExecutablePath(o.getExecutablePath());
+        resource.setModuleResource((AppModuleResource)createApplicationModule(o.getApplicationModule()));
+        resource.setHostResource((ComputeHostResource)createComputeResource(o.getComputeResource()));
+        return resource;
+    }
+
+    private static Resource createLibraryPrepPathResource(LibraryPrepandPath o) {
+        LibraryPrepandPathResource resource = new LibraryPrepandPathResource();
+        resource.setDeploymentId(o.getDeploymentID());
+        resource.setName(o.getName());
+        resource.setValue(o.getValue());
+        resource.setAppDeploymentResource((AppDeploymentResource)createApplicationDeployment(o.getApplicationDeployment()));
+        return resource;
+    }
+
+    private static Resource createLibraryApendPathResource(LibraryApendPath o) {
+        LibraryApendPathResource resource = new LibraryApendPathResource();
+        resource.setDeploymentId(o.getDeploymentID());
+        resource.setName(o.getName());
+        resource.setValue(o.getValue());
+        resource.setAppDeploymentResource((AppDeploymentResource)createApplicationDeployment(o.getApplicationDeployment()));
+        return resource;
     }
 }
