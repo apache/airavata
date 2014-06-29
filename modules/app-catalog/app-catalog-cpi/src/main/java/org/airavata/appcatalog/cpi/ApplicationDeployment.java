373d1dd9b1b168cfc307616ad9f3c02e4c7828d4
Chathuri Wimalasena <kamalasini@gmail.com>
2014-06-29 12:43:12 -0400

    adding app catalog CPI - AIRAVATA-1228

--- modules/app-catalog/app-catalog-cpi/src/main/java/org/airavata/appcatalog/cpi/ApplicationDeployment.java
+++ modules/app-catalog/app-catalog-cpi/src/main/java/org/airavata/appcatalog/cpi/ApplicationDeployment.java
@@ -1,4 +1,42 @@
 package org.airavata.appcatalog.cpi;
 
+import org.apache.airavata.model.appcatalog.appdeployment.ApplicationDeploymentDescription;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ApplicationDeployment {
+    /**
+     * Add application deployment
+     * @param deploymentDescription application deployment
+     * @return unique id for application deployment
+     */
+    String addApplicationDeployment (ApplicationDeploymentDescription deploymentDescription);
+
+    /**
+     * This method will retrive application deployement
+     * @param deploymentId unique deployment id
+     * @return application deployment
+     */
+    String getApplicationDeployement (String deploymentId);
+
+    /**
+     * This method will return a list of application deployments according to given search criteria
+     * @param filters map should be provided as the field name and it's value
+     * @return list of application deployments
+     */
+    List<ApplicationDeploymentDescription> getApplicationDeployements (Map<String, String> filters);
+
+    /**
+     * Check whether application deployment exists
+     * @param deploymentId unique deployment id
+     * @return true or false
+     */
+    boolean isAppDeploymentExists (String deploymentId);
+
+    /**
+     * Remove application deployment
+     * @param deploymentId unique deployment id
+     */
+    void removeAppDeployment (String deploymentId);
 }
