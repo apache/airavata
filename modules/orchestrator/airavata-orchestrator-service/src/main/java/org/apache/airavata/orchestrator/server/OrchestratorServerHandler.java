afe4141a3e7cedc71049695186ac061161fb49bb
Suresh Marru <smarru@apache.org>
2014-07-07 14:14:10 -0400

    Using the correct setter in orchestrator handler

--- modules/orchestrator/airavata-orchestrator-service/src/main/java/org/apache/airavata/orchestrator/server/OrchestratorServerHandler.java
+++ modules/orchestrator/airavata-orchestrator-service/src/main/java/org/apache/airavata/orchestrator/server/OrchestratorServerHandler.java
@@ -429,7 +429,7 @@ public class OrchestratorServerHandler implements OrchestratorService.Iface,
 								+ applicationId);
 			}
 			ApplicationDeploymentDescription applicationDeploymentDescription = getAppDeployment(taskData, applicationId);
-			taskData.setApplicationDescriptorId(applicationDeploymentDescription.getAppDeploymentId());
+            taskData.setApplicationDeploymentId(applicationDeploymentDescription.getAppDeploymentId());
 			registry.update(RegistryModelType.TASK_DETAIL, taskData,taskData.getTaskID());
 			List<Object> workflowNodeDetailList = registry.get(RegistryModelType.WORKFLOW_NODE_DETAIL,
 							org.apache.airavata.registry.cpi.utils.Constants.FieldConstants.WorkflowNodeConstants.TASK_LIST, taskData);
