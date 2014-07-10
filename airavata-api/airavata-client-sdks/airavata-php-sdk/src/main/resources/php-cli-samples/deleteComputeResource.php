52a112b1ef35740252aab0666bce363853eb80a7
Suresh Marru <smarru@apache.org>
2014-07-10 17:11:28 -0400

    more php samples

---
+++ airavata-api/airavata-client-sdks/airavata-php-sdk/src/main/resources/php-cli-samples/deleteComputeResource.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Bundle all thrift and Airavata stubs into a include file. This is simple but not so elegant way.
+ *  Contributions welcome to improve writing PHP Client Samples.
+ *
+ */
+include 'getAiravataClient.php';
+global $airavataclient;
+global $transport;
+
+use Airavata\API\Error\AiravataClientException;
+use Airavata\API\Error\AiravataSystemException;
+use Airavata\API\Error\InvalidRequestException;
+use Thrift\Exception\TTransportException;
+
+use Airavata\Model\AppCatalog\AppDeployment\ApplicationModule;
+
+try {
+
+    if (count($argv) != 2) {
+        exit("\n Incorrect Arguments \n. Usage: deleteComputeResource.php <compute resource id>. \n");
+    } else {
+
+        $computeResourceId = $argv[1];
+
+        $success = $airavataclient->deleteComputeResource($computeResourceId);
+
+        if ($success) {
+            echo "Application Interface $computeResourceId successfully deleted";
+        } else {
+            echo "\n Failed to delete application interface $computeResourceId \n";
+        }
+    }
+} catch (InvalidRequestException $ire) {
+    print 'InvalidRequestException: ' . $ire->getMessage() . "\n";
+} catch (AiravataClientException $ace) {
+    print 'Airavata System Exception: ' . $ace->getMessage() . "\n";
+} catch (AiravataSystemException $ase) {
+    print 'Airavata System Exception: ' . $ase->getMessage() . "\n";
+} catch (TTransportException $tte) {
+    echo 'TTransportException!<br><br>' . $tte->getMessage();
+} catch (\Exception $e) {
+    echo 'Exception!<br><br>' . $e->getMessage();
+}
+
+$transport->close();
+
+?>
+
