bc0666893e7a6558d5cc628b845650de1b9c3afd
Nadeem Anjum <nadeem.cs.iit@gmail.com>
2014-08-03 02:57:37 +0530

    Added java cli client samples

---
+++ airavata-api/airavata-client-sdks/java-client-samples/src/main/java/org/apache/airavata/client/cli_samples/CreateProject.java
@@ -0,0 +1,104 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.client.cli_samples;
+
+import org.apache.airavata.model.error.*;
+import org.apache.airavata.model.util.ProjectModelUtil;
+import org.apache.airavata.model.workspace.Project;
+import org.apache.airavata.api.Airavata;
+import org.apache.airavata.api.client.AiravataClientFactory;
+import org.apache.airavata.common.utils.AiravataUtils;
+import org.apache.thrift.TException;
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateProject{	
+	public static String THRIFT_SERVER_HOST = "localhost";
+    public static int THRIFT_SERVER_PORT = 8930;
+    private static Airavata.Client client;
+    
+    public static void readConfigFile(){
+    	Ini ini = null;
+		try {
+			ini = new Ini(new File("src/main/resources/airavata-client-properties.ini"));
+		} catch (InvalidFileFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	THRIFT_SERVER_HOST = ini.get("airavata", "AIRAVATA_SERVER");
+    	THRIFT_SERVER_PORT = Integer.parseInt(ini.get("airavata", "AIRAVATA_PORT"));
+    }
+    
+    public static String createProject(String projName, String owner, String description) {
+    	AiravataUtils.setExecutionAsClient();
+        try {
+			client = AiravataClientFactory.createAiravataClient(THRIFT_SERVER_HOST, THRIFT_SERVER_PORT);
+		} catch (AiravataClientConnectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Project project = ProjectModelUtil.createProject(projName, owner, description);
+        String projId = "";
+        try {
+			projId = client.createProject(project);
+		} catch (InvalidRequestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AiravataClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AiravataSystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return projId;
+    }
+    
+    public static void main(String[] args) {
+    	if(args.length!=3){
+    		System.out.println("Inputs required: projectName Owner Description");
+    		return;
+    	}
+    	readConfigFile();
+    	String projName = args[0];
+    	String owner = args[1];
+    	String description = args[2];
+    	String projId = createProject(projName, owner, description);
+    	if(projId!=""){
+    		System.out.println("Project with id : "+ projId + " created.");
+    	}
+    	else{
+    		System.out.println("Failed to create new project");
+    	}
+    }
+    
+    
+}
