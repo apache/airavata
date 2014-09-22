d907c9f8081138a02912181364ea828014bb4c67
raminder <raminder@apache.org>
2014-09-22 12:17:37 -0400

    Fixed with current flags. With Application catalog intergration we will be able to provide a better fix. AIRAVATA-1449

--- modules/gfac/gfac-ssh/src/main/java/org/apache/airavata/gfac/ssh/handler/SSHOutputHandler.java
+++ modules/gfac/gfac-ssh/src/main/java/org/apache/airavata/gfac/ssh/handler/SSHOutputHandler.java
@@ -202,9 +202,12 @@ public class SSHOutputHandler extends AbstractHandler {
                 }
             }
             if (outputArray == null || outputArray.isEmpty()) {
-                throw new GFacHandlerException(
+            	log.error("Empty Output returned from the Application, Double check the application and ApplicationDescriptor output Parameter Names");
+            	if(jobExecutionContext.getTaskData().getAdvancedOutputDataHandling() == null){
+            		throw new GFacHandlerException(
                         "Empty Output returned from the Application, Double check the application"
                                 + "and ApplicationDescriptor output Parameter Names");
+            	}
             }
             app.setStandardError(localStdErrFile.getAbsolutePath());
             app.setStandardOutput(localStdOutFile.getAbsolutePath());
