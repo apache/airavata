fa52958905e3c849083fb2f2f6824f99a4408678
msmemon <shahbaz.memon@gmail.com>
2014-09-26 17:43:45 +0200

    jglobus version update

--- modules/gfac/gfac-bes/src/main/java/org/apache/airavata/gfac/bes/handlers/SMSByteIOInHandler.java
+++ modules/gfac/gfac-bes/src/main/java/org/apache/airavata/gfac/bes/handlers/SMSByteIOInHandler.java
@@ -1,5 +1,29 @@
 package org.apache.airavata.gfac.bes.handlers;
 
-public class SMSByteIOInHandler {
 
+import org.apache.airavata.gfac.core.context.JobExecutionContext;
+import org.apache.airavata.gfac.core.handler.GFacHandler;
+import org.apache.airavata.gfac.core.handler.GFacHandlerException;
+import org.apache.airavata.gfac.core.provider.GFacProviderException;
+
+/**
+ * Download upload job's input files to the temporary SMS directory.
+ * 
+ * */
+public class SMSByteIOInHandler extends AbstractSMSHandler implements GFacHandler {
+
+
+	@Override
+	public void invoke(JobExecutionContext jobExecutionContext) throws GFacHandlerException	{
+		super.invoke(jobExecutionContext);
+    try{    
+        	if(jobExecutionContext.getInMessageContext().getParameters().size() < 1) return;
+        	dataTransferrer.uploadLocalFiles();
+	} catch (GFacProviderException e) {
+		throw new GFacHandlerException("Cannot upload local data",e);
+	}
+
+ 	}
+	
+	
 }
