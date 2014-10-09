8b978c62ec74950fb1c37ca37d38301c7506033e
Chathuri Wimalasena <kamalasini@gmail.com>
2014-10-09 12:23:45 -0400

    rabbit mq consumer

---
+++ modules/messaging/core/src/main/java/org/apache/airavata/messaging/core/TestClient.java
@@ -0,0 +1,51 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.airavata.messaging.core;
+
+import org.apache.airavata.common.exception.ApplicationSettingsException;
+import org.apache.airavata.common.utils.AiravataUtils;
+import org.apache.airavata.common.utils.ServerSettings;
+import org.apache.airavata.messaging.core.impl.RabbitMQConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestClient {
+    public static final String RABBITMQ_BROKER_URL = "rabbitmq.broker.url";
+    public static final String RABBITMQ_EXCHANGE_NAME = "rabbitmq.exchange.name";
+    private final static Logger logger = LoggerFactory.getLogger(TestClient.class);
+    private final static String experimentId = "echoExperiment_825d89fa-179a-4da0-ae1e-e7fa0e58bdb2";
+
+    public static void main(String[] args) {
+        try {
+            AiravataUtils.setExecutionAsServer();
+            String brokerUrl = ServerSettings.getSetting(RABBITMQ_BROKER_URL);
+            String exchangeName = ServerSettings.getSetting(RABBITMQ_EXCHANGE_NAME);
+            RabbitMQConsumer consumer = new RabbitMQConsumer(brokerUrl, exchangeName);
+            consumer.listen(experimentId);
+        } catch (ApplicationSettingsException e) {
+            logger.error("Error reading airavata server properties", e);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
