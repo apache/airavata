293308cd1a4f0e34327b4f65a83635c8bdfea96c
supunkamburugamuva <supun06@gmail.com>
2014-10-15 15:03:43 -0400

    adding the capability to register multiple consumers for the same channel
    Signed-off-by: Chathuri Wimalasena <kamalasini@gmail.com>

--- modules/messaging/core/src/main/java/org/apache/airavata/messaging/core/MessageHandler.java
+++ modules/messaging/core/src/main/java/org/apache/airavata/messaging/core/MessageHandler.java
@@ -1,7 +1,9 @@
 package org.apache.airavata.messaging.core;
 
-import org.apache.airavata.model.messaging.event.Message;
+import java.util.Map;
 
 public interface MessageHandler {
+    Map<String, String> getProperties();
+
     void onMessage(MessageContext message);
 }
