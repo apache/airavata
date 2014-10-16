7e498ac54b5e135311b60923fad80247f04470f9
Supun <supun06@gmail.com>
2014-10-16 10:06:00 -0400

    adding more changes to messaging and changing xbaya to fit messaging
    Signed-off-by: Chathuri Wimalasena <kamalasini@gmail.com>

--- modules/messaging/core/src/main/java/org/apache/airavata/messaging/core/MessageHandler.java
+++ modules/messaging/core/src/main/java/org/apache/airavata/messaging/core/MessageHandler.java
@@ -3,7 +3,7 @@ package org.apache.airavata.messaging.core;
 import java.util.Map;
 
 public interface MessageHandler {
-    Map<String, String> getProperties();
+    Map<String, Object> getProperties();
 
     void onMessage(MessageContext message);
 }
