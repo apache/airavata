eb722e1dfe8d115e43b084c9a26d7f8576100b41
msmemon <shahbaz.memon@gmail.com>
2014-10-21 17:18:10 +0200

    added unicore client dependency and removed the unicore repository [AIRAVATA-1473]

--- airavata-api/airavata-client-sdks/java-client-samples/src/main/java/org/apache/airavata/client/samples/CreateLaunchExperiment.java
+++ airavata-api/airavata-client-sdks/java-client-samples/src/main/java/org/apache/airavata/client/samples/CreateLaunchExperiment.java
@@ -53,7 +53,7 @@ public class CreateLaunchExperiment {
     private static final String DEFAULT_GATEWAY = "default.registry.gateway";
     private static Airavata.Client airavataClient;
 
-    private static String echoAppId = "Echo_56c6e26c-ca77-45fe-91d1-58fa59676879";
+    private static String echoAppId = "Echo_438a0de6-544b-4df2-8586-38a4c191b9c9";
     private static String wrfAppId = "WRF_5f097c9c-7066-49ec-aed7-4e39607b3adc";
     private static String amberAppId = "Amber_89906be6-5678-49a6-9d04-a0604fbdef2e";
 
@@ -216,7 +216,7 @@ public class CreateLaunchExperiment {
             output.setValue("");
             exOut.add(output);
             
-            echoAppId = "Echo_bdfe6dc3-0e80-41af-adb6-f89b1b05c3e8";
+            
             
             Experiment simpleExperiment = 
                     ExperimentModelUtil.createSimpleExperiment("default", "admin", "echoExperiment", "SimpleEcho2", echoAppId, exInputs);
