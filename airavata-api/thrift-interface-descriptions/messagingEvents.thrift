18f338c5c025ce124be57556cbbcfc333722bbaf
Chathuri Wimalasena <kamalasini@gmail.com>
2015-01-22 10:02:50 -0500

    Revert "adding RabbitMQDatacat Support Beans, to be tested"
    This reverts commit 714c2048da87e70544ebdeabab739f8158b134ce.

--- airavata-api/thrift-interface-descriptions/messagingEvents.thrift
+++ airavata-api/thrift-interface-descriptions/messagingEvents.thrift
@@ -38,9 +38,7 @@ enum MessageType {
     EXPERIMENT,
     TASK,
     WORKFLOWNODE,
-    JOB,
-    EXPERIMENT_OUTPUT,
-    OUTPUT_PARSED
+    JOB
 }
 
 struct ExperimentStatusChangeEvent {
@@ -120,18 +118,7 @@ struct Message {
     5: optional MessageLevel messageLevel;
 }
 
-struct ExperimentOutputCreatedEvent {
-    1: required string experimentId;
-    2: required string filename;
-    3: required string filepath;
 
-}
-
-struct ExperimentOutputParsedEvent {
-    1: required string experimentId;
-    2: required string documentID;
-    3: required string status;
-}
 
 
 
