a8cf35fc7acc8809f2a8aa0183327cfca3345aff
Lahiru Gunathilake <glahiru@gmail.com>
2015-03-27 11:31:53 -0400

    making worker queue configurable to durable and non-durable

--- modules/messaging/core/src/main/java/org/apache/airavata/messaging/core/MessagingConstants.java
+++ modules/messaging/core/src/main/java/org/apache/airavata/messaging/core/MessagingConstants.java
@@ -29,4 +29,5 @@ public abstract class MessagingConstants {
     public static final String RABBIT_ROUTING_KEY = "routingKey";
     public static final String RABBIT_QUEUE= "queue";
     public static final String RABBIT_CONSUMER_TAG = "consumerTag";
+    public static final String DURABLE_QUEUE="durable.queue";
 }
