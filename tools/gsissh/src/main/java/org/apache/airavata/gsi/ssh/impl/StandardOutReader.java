4de74c6017ad2b6c3554a4956a4fd9db216ad2d3
shamrath <shameerainfo@gmail.com>
2015-05-10 16:39:57 -0400

    Try to read input stream first time and then read until jsh channel is closed.

--- tools/gsissh/src/main/java/org/apache/airavata/gsi/ssh/impl/StandardOutReader.java
+++ tools/gsissh/src/main/java/org/apache/airavata/gsi/ssh/impl/StandardOutReader.java
@@ -42,13 +42,13 @@ public class StandardOutReader implements CommandOutput {
             StringBuffer pbsOutput = new StringBuffer("");
             InputStream inputStream =  channel.getInputStream();
             byte[] tmp = new byte[1024];
-            while (!channel.isClosed()) {
+            do {
                 while (inputStream.available() > 0) {
                     int i = inputStream.read(tmp, 0, 1024);
                     if (i < 0) break;
                     pbsOutput.append(new String(tmp, 0, i));
                 }
-            }
+            } while (!channel.isClosed()) ;
             String output = pbsOutput.toString();
             this.setStdOutputString(output);
         } catch (IOException e) {
