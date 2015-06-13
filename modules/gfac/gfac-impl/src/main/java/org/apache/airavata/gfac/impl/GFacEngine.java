102fb044e5e6d4c318e69cfebdf033ad4c902da1
Shameera Rathanyaka <shameerainfo@gmail.com>
2015-06-13 19:23:17 -0400

    added yam config file and parsers.

--- modules/gfac/gfac-impl/src/main/java/org/apache/airavata/gfac/impl/GFacEngine.java
+++ modules/gfac/gfac-impl/src/main/java/org/apache/airavata/gfac/impl/GFacEngine.java
@@ -22,32 +22,104 @@
 package org.apache.airavata.gfac.impl;
 
 import org.apache.airavata.gfac.core.GFacException;
+import org.apache.airavata.gfac.core.config.DataTransferTaskConfig;
+import org.apache.airavata.gfac.core.config.GFacYamlConfigruation;
+import org.apache.airavata.gfac.core.config.JobSubmitterTaskConfig;
+import org.apache.airavata.gfac.core.config.ResourceConfig;
 import org.apache.airavata.gfac.core.context.ProcessContext;
+import org.apache.airavata.gfac.core.task.Task;
+import org.apache.airavata.model.appcatalog.computeresource.DataMovementProtocol;
+import org.apache.airavata.model.appcatalog.computeresource.JobSubmissionProtocol;
+import org.apache.airavata.model.appcatalog.computeresource.ResourceJobManagerType;
+import org.apache.airavata.model.application.io.DataType;
+import org.apache.airavata.model.application.io.InputDataObjectType;
+import org.apache.airavata.model.process.ProcessModel;
+import org.apache.airavata.model.task.TaskModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class GFacEngine {
+	private static GFacEngine engine;
+	Map<JobSubmissionProtocol, Task> jobSubmissionTask;
+	Map<DataMovementProtocol, Task> dataMovementTask;
+	Map<ResourceJobManagerType, ResourceConfig> resources;
 
-	public static void createTaskChain(ProcessContext processContext) throws GFacException {
+
+	private GFacEngine() throws GFacException {
+		GFacYamlConfigruation config = new GFacYamlConfigruation();
+		for (JobSubmitterTaskConfig jobSubmitterTaskConfig : config.getJobSbumitters()) {
+			jobSubmissionTask.put(jobSubmitterTaskConfig.getSubmissionProtocol(), null);
+		}
+
+		for (DataTransferTaskConfig dataTransferTaskConfig : config.getFileTransferTasks()) {
+			dataMovementTask.put(dataTransferTaskConfig.getTransferProtocol(), null);
+		}
+
+		for (ResourceConfig resourceConfig : config.getResourceConfiguration()) {
+			resources.put(resourceConfig.getJobManagerType(), resourceConfig);
+		}
+	}
+
+	public static GFacEngine getInstance() throws GFacException {
+		if (engine == null) {
+			synchronized (GFacEngine.class) {
+				if (engine == null) {
+					engine = new GFacEngine();
+				}
+			}
+		}
+		return engine;
+	}
+
+	public ProcessContext populateProcessContext(ProcessContext processContext) {
+		processContext.setProcessModel(new ProcessModel());
+		return processContext;
+	}
+
+	public void createTaskChain(ProcessContext processContext) throws GFacException {
+		List<InputDataObjectType> processInputs = processContext.getProcessModel().getProcessInputs();
+		List<TaskModel> taskChain = new ArrayList<>();
+		if (processInputs != null) {
+			for (InputDataObjectType processInput : processInputs) {
+				DataType type = processInput.getType();
+				switch (type) {
+					case STDERR:
+						//
+						break;
+					case STDOUT:
+						//
+						break;
+					case URI:
+						// add URI type Task
+						break;
+					default:
+						// nothing to do
+						break;
+				}
+			}
+		}
+	}
+
+	public void executeProcess(ProcessContext processContext) throws GFacException {
+
 
 	}
 
-	public static void executeProcess(ProcessContext processContext) throws GFacException {
-
-
-	}
-
-	public static void recoverProcess(ProcessContext processContext) throws GFacException {
+	public void recoverProcess(ProcessContext processContext) throws GFacException {
 
 	}
 
-	public static void runProcessOutflow(ProcessContext processContext) throws GFacException {
+	public void runProcessOutflow(ProcessContext processContext) throws GFacException {
 
 	}
 
-	public static void recoverProcessOutflow(ProcessContext processContext) throws GFacException {
+	public void recoverProcessOutflow(ProcessContext processContext) throws GFacException {
 
 	}
 
-	public static void cancelProcess() throws GFacException {
+	public void cancelProcess() throws GFacException {
 
 	}
 }
