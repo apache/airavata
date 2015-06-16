369342dd894b03aa5c1be2007052527c2d9c3d0b
Shameera Rathanyaka <shameerainfo@gmail.com>
2015-06-16 14:30:00 -0400

    Added HPCRemoteCluster implementation logic

--- modules/gfac/gfac-core/src/main/java/org/apache/airavata/gfac/core/cluster/RemoteCluster.java
+++ modules/gfac/gfac-core/src/main/java/org/apache/airavata/gfac/core/cluster/RemoteCluster.java
@@ -47,19 +47,19 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	/**
 	 * This will copy the localFile to remoteFile location in configured cluster
 	 *
-	 * @param sourceFile      remote file location, this can be a directory too
-	 * @param destinationFile local file path of the file which needs to copy to remote location
+	 * @param remoteFile      remote file location, this can be a directory too
+	 * @param localFile local file path of the file which needs to copy to remote location
 	 * @throws SSHApiException throws exception during error
 	 */
-	public void scpTo(String sourceFile, String destinationFile) throws SSHApiException;
+	public void scpTo(String remoteFile, String localFile) throws SSHApiException;
 
 	/**
 	 * This will copy a remote file in path rFile to local file lFile
 	 *
-	 * @param sourceFile      remote file path, this has to be a full qualified path
-	 * @param destinationFile This is the local file to copy, this can be a directory too
+	 * @param remoteFile      remote file path, this has to be a full qualified path
+	 * @param localFile This is the local file to copy, this can be a directory too
 	 */
-	public void scpFrom(String sourceFile, String destinationFile) throws SSHApiException;
+	public void scpFrom(String remoteFile, String localFile) throws SSHApiException;
 
 	/**
 	 * This wil copy source remote file to target remote file.
