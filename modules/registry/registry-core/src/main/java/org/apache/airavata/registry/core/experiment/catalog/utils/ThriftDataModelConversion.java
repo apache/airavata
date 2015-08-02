053bb9f223b60e4cedcb5cca49e61811dda758d7
Supun Nakandala <scnakandala@apache.org>
2015-08-02 23:41:18 +0530

    fixing gatewayId not set for ExperimentSummaryModel

--- modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/experiment/catalog/utils/ThriftDataModelConversion.java
+++ modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/experiment/catalog/utils/ThriftDataModelConversion.java
@@ -101,9 +101,7 @@ public class ThriftDataModelConversion {
             experimentSummary.setUserName(experimentSummaryResource.getUserName());
             experimentSummary.setName(experimentSummaryResource.getExperimentName());
             experimentSummary.setDescription(experimentSummaryResource.getDescription());
-            ExperimentState experimentState = ExperimentState.valueOf(experimentSummaryResource.getState());
-//            FIXME : Experiment Summary resource is incomplete at this point
-//            experimentSummary.setExperimentStatus(experimentSummaryResource.getState());
+            experimentSummary.setExperimentStatus(experimentSummaryResource.getState());
             return experimentSummary;
         }
         return null;
