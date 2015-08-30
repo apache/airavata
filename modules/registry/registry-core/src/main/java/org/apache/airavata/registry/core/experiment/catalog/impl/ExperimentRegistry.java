555b0adbfc9035d291eeb7d559a86038746724b8
Supun Nakandala <scnakandala@apache.org>
2015-08-30 19:46:09 +0530

    fixing getExperimentStatistics issue in registry

--- modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/experiment/catalog/impl/ExperimentRegistry.java
+++ modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/experiment/catalog/impl/ExperimentRegistry.java
@@ -1300,6 +1300,8 @@ public class ExperimentRegistry {
             );
 
             experimentStatistics.setAllExperimentCount(experimentStatisticsResource.getAllExperimentCount());
+            experimentStatistics.setCreatedExperimentCount(experimentStatisticsResource.getCreatedExperimentCount());
+            experimentStatistics.setRunningExperimentCount(experimentStatisticsResource.getRunningExperimentCount());
             experimentStatistics.setCompletedExperimentCount(experimentStatisticsResource.getCompletedExperimentCount());
             experimentStatistics.setFailedExperimentCount(experimentStatisticsResource.getFailedExperimentCount());
             experimentStatistics.setCancelledExperimentCount(experimentStatisticsResource.getCancelledExperimentCount());
@@ -1309,6 +1311,18 @@ public class ExperimentRegistry {
                 experimentSummaries.add(ThriftDataModelConversion.getExperimentSummary(ex));
             }
             experimentStatistics.setAllExperiments(experimentSummaries);
+
+            experimentSummaries = new ArrayList();
+            for (ExperimentSummaryResource ex : experimentStatisticsResource.getCreatedExperiments()) {
+                experimentSummaries.add(ThriftDataModelConversion.getExperimentSummary(ex));
+            }
+            experimentStatistics.setCreatedExperiments(experimentSummaries);
+
+            experimentSummaries = new ArrayList();
+            for (ExperimentSummaryResource ex : experimentStatisticsResource.getRunningExperiments()) {
+                experimentSummaries.add(ThriftDataModelConversion.getExperimentSummary(ex));
+            }
+            experimentStatistics.setRunningExperiments(experimentSummaries);
 
             experimentSummaries = new ArrayList();
             for (ExperimentSummaryResource ex : experimentStatisticsResource.getCompletedExperiments()) {
