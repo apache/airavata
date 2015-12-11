dfc1e27a96e6c0e0988a434dc3f1ed518d74ab8f
Chathuri Wimalasena <chathuri@apache.org>
2015-12-11 14:14:24 -0500

    adding data movements when retrieving data storage resources

--- modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/app/catalog/util/AppCatalogThriftConversion.java
+++ modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/app/catalog/util/AppCatalogThriftConversion.java
@@ -118,6 +118,11 @@ public class AppCatalogThriftConversion {
         description.setHostName(resource.getHostName());
         description.setStorageResourceDescription(resource.getResourceDescription());
         description.setEnabled(resource.isEnabled());
+        DataMovementInterfaceResource dmiResource = new DataMovementInterfaceResource();
+        List<AppCatalogResource> dmiList = dmiResource.get(AppCatAbstractResource.StorageInterfaceConstants.STORAGE_RESOURCE_ID, resource.getStorageResourceId());
+        if (dmiList != null && !dmiList.isEmpty()){
+            description.setDataMovementInterfaces(getDataMovementInterfaces(dmiList));
+        }
         return description;
     }
 
