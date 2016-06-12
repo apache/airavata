e17db1f2feec117ff09de80c7289bfd708358a6f
Bikramjeet Singh <linfestus@Bikramjeets-MacBook-Pro.local>
2016-06-13 00:32:41 +0530

    Added Xbaya UI

---
+++ modules/xbaya/src/main/java/org/airavata/xbaya/App.java
@@ -0,0 +1,12 @@
+package org.airavata.xbaya;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App {
+
+
+}
