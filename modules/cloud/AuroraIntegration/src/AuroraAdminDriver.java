3c024643bdc52d6ab3516511894524bd7bef58e3
ajain13 <ajain13@binghamton.edu>
2016-06-26 12:04:56 -0400

    Cleaned up code, updated AuroraJobSchedulerI interface, changed filenames to make them for understandable

--- modules/cloud/AuroraIntegration/src/AuroraAdminDriver.java
+++ modules/cloud/AuroraIntegration/src/AuroraAdminDriver.java
@@ -11,14 +11,14 @@ public class AuroraAdminDriver{
 
 	    // TODO: add code to call a method to validate the args
 
-	    // TODO: program to an interface. So, the code should be 
+	    // TODO: program to an interface. So, the code should be
 	    // AuroraJobSchedulerI auroraJS = new AuroraJobSchedulerImpl();
-	    
-		AuroraJobScheduler auroraJS = new AuroraJobScheduler();
+
+		AuroraJobSchedulerI auroraJS = new AuroraJobSchedulerImpl();
 		// why is htis Map final? : Does not have to be final, all the implementations of argument readers i saw had final for Map
-		
+
 		// documentation: what is the purpose of the for-loop : to read all the arguments for example to create a job following are the arguments: -o create -r 1024 -n batik -c 2.0 -d 1000 -i gouravr/dacapo:tag9
-  
+
 		Map<String, List<String>> params = new HashMap<>();
 		List<String> options = null;
 		for (int i = 0; i < args.length; i++) {
@@ -36,12 +36,12 @@ public class AuroraAdminDriver{
         			options.add(a);
     			}
     			else {
-			    // TOOD: need an example of correct parameter usage
-        			System.err.println("Illegal parameter usage\nOptions:\n1) -o\tcreate, kill, restart, update, update-info, update-pause\n2) -n\tname of the job\n 3) -r\tamount of RAM\n 4) -c\tCPU count\n 5) -d\tdisk space\n 6) -k\tname of the task to be killed\n 7) -i\texecutable/image\n ");
+
+        			System.err.println("Illegal parameter \n[USAGE]\nOptions:\n1) -o\tcreate, kill, restart, update, update-info, update-pause\n2) -n\tname of the job\n 3) -r\tamount of RAM\n 4) -c\tCPU count\n 5) -d\tdisk space\n 6) -k\tname of the task to be killed\n 7) -i\texecutable/image\n ");
         			return;
     			}
 		}
-		// These Strings should be Enums
+
 		String RamSize,JobName,CpuCount,DiskSize,Option,Image;
 		Option = params.get("o").get(0);
 		switch(Option)
--- modules/cloud/AuroraIntegration/src/AuroraJobSchedulerImpl.java
+++ modules/cloud/AuroraIntegration/src/AuroraJobSchedulerImpl.java
@@ -1,11 +1,3 @@
-
-// TODO: add documentation on the purpose of this class
-
-//TODO: rename this class to AuroraJobSchedulerImpl
-
-// TODO: need javadoc style documentation for each method
-
-//TODO: import each type individually instead of "*"
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,28 +6,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import exception.AuroraException;
-import utilities.Utility;
+import utilities.AuroraUtilImpl;
+import utilities.AuroraUtilI;
 
-public class AuroraJobScheduler implements AuroraJobSchedulerI {
-	Utility util = new Utility();
+public class AuroraJobSchedulerImpl implements AuroraJobSchedulerI {
+	AuroraUtilI util = new AuroraUtilImpl();
 	public void auroraJobCommand(String info, String command) throws AuroraException{
 		try{
 			String line;
 			Process auroraJob = Runtime.getRuntime().exec("aurora task run example/benchmarks/devel/"+info+" "+command);
 			auroraJob.waitFor();
 			BufferedReader stdout = new BufferedReader(new InputStreamReader(auroraJob.getInputStream()));
-			// THE NEXT 5 lines are now put into a utility function called printLog present in the utilities package
-			/*line = stdout.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = stdout.readLine();
-			}*/
+
 			util.printLog(stdout);
 		}
-		// NO NEED TO CATCH THIS EXCEPTION AS ITS BEING CAUGHT BY THE utility function now. Is it required to catch it still ?
-		/*catch (IOException ex) {
-			throw new AuroraException("IO Exception occured while passing the command.\n"+ex.toString());
-		}*/
+
 		catch (Exception ex) {
 			throw new AuroraException("Exception occured while passing the command.\n"+ex.toString());
 		}
@@ -51,7 +36,7 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 		}
 
 		catch (Exception ex) {
-		    // should get the Exception in a string, add your own meaningful message, and then throw it again
+
 			throw new AuroraException("Exception occured while listing the update.\n"+ex.toString());
 		}
 	}
@@ -66,7 +51,7 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 		}
 
 		catch (Exception ex) {
-		    // should get the Exception in a string, add your own meaningful message, and then throw it again
+
 			throw new AuroraException("Exception occured while aborting the update.\n"+ex.toString());
 		}
 	}
@@ -81,7 +66,7 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 		}
 
 		catch (Exception ex) {
-		    // should get the Exception in a string, add your own meaningful message, and then throw it again
+
 			throw new AuroraException("Exception occured while resuming the update.\n"+ex.toString());
 		}
 	}
@@ -96,7 +81,7 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 		}
 
 		catch (Exception ex) {
-		    // should get the Exception in a string, add your own meaningful message, and then throw it again
+
 			throw new AuroraException("Exception occured while pausing the update.\n"+ex.toString());
 		}
 	}
@@ -112,7 +97,7 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 		}
 
 		catch (Exception ex) {
-		    // should get the Exception in a string, add your own meaningful message, and then throw it again
+
 			throw new AuroraException("Exception occured while retrieving the update info."+ex.toString());
 		}
 	}
@@ -129,7 +114,7 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 		}
 
 		catch (Exception ex) {
-		    // TODO: should get the Exception in a string, add your own meaningful message, and then throw it again
+
 			throw new AuroraException("Exception occured while updating the job.\n"+ex.toString());
    		}
 	}
@@ -180,10 +165,6 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 	public void configCreate(String name, String ram, String cpu, String disk, String image) throws AuroraException{
 	try {
 		String config = "import hashlib\n"+name+"= Process(name = '"+name+"', cmdline = 'java -jar /dacapo-9.12-bach.jar "+name+" -s small')\n"+name+"_task = SequentialTask(processes = [ "+name+"], resources = Resources(cpu = "+cpu+", ram = "+ram+"*MB, disk="+disk+"*MB))\njobs = [ Job(cluster = 'example', environment = 'devel', role = 'benchmarks', name = '"+name+"', task = "+name+"_task, instances =1 , container = Container(docker = Docker(image = '"+image+"')))]\n";
-                //String line2 = name+"= Process(name = '"+name+"', cmdline = 'java -jar /dacapo-9.12-bach.jar "+name+" -s small')\n";
-                //String line3 = name+"_task = SequentialTask(processes = [ "+name+"], resources = Resources(cpu = "+cpu+", ram = "+ram+"*MB, disk="+disk+"*MB))\n";
-                //String line4 = "jobs = [ Job(cluster = 'example', environment = 'devel', role = 'benchmarks', name = '"+name+"', task = "+name+"_task, instances =1 , container = Container(docker = Docker(image = '"+image+"')))]\n";
-
 		File file = new File(name+".aurora");
 
 		if (!file.exists()) {
@@ -193,9 +174,6 @@ public class AuroraJobScheduler implements AuroraJobSchedulerI {
 		FileWriter fw = new FileWriter(file.getAbsoluteFile());
 		BufferedWriter bw = new BufferedWriter(fw);
 		bw.write(config);
-                //bw.write(line2);
-                //bw.write(line3);
-                //bw.write(line4);
 		bw.close();
 
 		}catch (IOException ex) {
