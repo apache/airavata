9dd54367afe724241bccfb818bc5dba90b97c255
ajain13 <ajain13@binghamton.edu>
2016-06-26 11:34:07 -0400

    Made design changes

---
+++ modules/cloud/AuroraIntegration/src/utilities/UtilityI.java
@@ -0,0 +1,7 @@
+package utilities;
+import java.io.BufferedReader;
+import exception.AuroraException;
+
+public interface UtilityI{
+  public void printLog(BufferedReader stdout) throws AuroraException;
+}
