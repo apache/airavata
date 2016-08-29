3ce49b9a6530bbe0f1c2249668473b67b1bb29ae
scnakandala <supun.nakandala@gmail.com>
2016-08-29 10:12:34 -0400

    updating the registry refactoring code

--- modules/registry-refactoring/src/test/java/org/apache/airavata/registry/core/repositories/RepositoryTest.java
+++ modules/registry-refactoring/src/test/java/org/apache/airavata/registry/core/repositories/RepositoryTest.java
@@ -20,13 +20,23 @@
 */
 package org.apache.airavata.registry.core.repositories;
 
+import org.apache.airavata.model.experiment.ExperimentModel;
+import org.apache.airavata.model.experiment.UserConfigurationDataModel;
+import org.apache.airavata.model.user.UserProfile;
 import org.apache.airavata.model.workspace.Gateway;
 import org.apache.airavata.model.workspace.GatewayApprovalStatus;
 import org.apache.airavata.model.workspace.Notification;
+import org.apache.airavata.model.workspace.Project;
+import org.apache.airavata.registry.core.entities.expcatalog.ExperimentEntity;
 import org.apache.airavata.registry.core.entities.workspacecatalog.GatewayEntity;
 import org.apache.airavata.registry.core.entities.workspacecatalog.NotificationEntity;
+import org.apache.airavata.registry.core.entities.workspacecatalog.ProjectEntity;
+import org.apache.airavata.registry.core.entities.workspacecatalog.UserProfileEntity;
+import org.apache.airavata.registry.core.repositories.expcatalog.ExperimentRepository;
 import org.apache.airavata.registry.core.repositories.workspacecatalog.GatewayRepository;
 import org.apache.airavata.registry.core.repositories.workspacecatalog.NotificationRepository;
+import org.apache.airavata.registry.core.repositories.workspacecatalog.ProjectRepository;
+import org.apache.airavata.registry.core.repositories.workspacecatalog.UserProfileRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -56,5 +66,35 @@ public class RepositoryTest {
         notificationRepository.create(notification);
 
         notificationRepository.get(notification.getNotificationId());
+
+        UserProfile userProfile = new UserProfile();
+        userProfile.setAiravataInternalUserId(UUID.randomUUID().toString());
+        userProfile.setGatewayId(gateway.getGatewayId());
+        UserProfileRepository userProfileRepository = new UserProfileRepository(UserProfile.class, UserProfileEntity.class);
+        userProfileRepository.create(userProfile);
+
+
+        Project project = new Project();
+        project.setProjectID(UUID.randomUUID().toString());
+        project.setOwner(userProfile.getAiravataInternalUserId());
+        project.setGatewayId(gateway.getGatewayId());
+        project.setName("Project Name");
+
+        ProjectRepository projectRepository = new ProjectRepository(Project.class, ProjectEntity.class);
+        projectRepository.create(project);
+
+        ExperimentModel experiment = new ExperimentModel();
+        experiment.setExperimentId(UUID.randomUUID().toString());
+        experiment.setUserName(userProfile.getAiravataInternalUserId());
+        experiment.setProjectId(project.getProjectID());
+        experiment.setGatewayId(gateway.getGatewayId());
+        experiment.setExperimentName("Dummy Experiment");
+
+        UserConfigurationDataModel userConfigurationData = new UserConfigurationDataModel();
+        userConfigurationData.setExperimentDataDir("some/path");
+        experiment.setUserConfigurationData(userConfigurationData);
+
+        ExperimentRepository experimentRepository = new ExperimentRepository(ExperimentModel.class, ExperimentEntity.class);
+        experimentRepository.create(experiment);
     }
 }
