13303d511810af186e2d41b15f5f74f4c0d70f3e
CoreOS Admin <core@ip-10-7-156-74.ec2.internal>
2016-08-31 07:21:16 +0000

    Pushing zookeeper, kafka, logstash changes

--- deploy/images/kafka/start-kafka.sh
+++ deploy/images/kafka/start-kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if test -z "${BROKER_ID}"; then
-  BROKER_ID=$(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}' | head -n1  | tr -d '.')
+  BROKER_ID=$(ip a | grep 'eth0' | awk '/inet /{print substr($2,4)}'| sed 's/\///g' | head -n1 | tr -d .)
 fi
 
 mkdir -p /opt/kafka/etc
@@ -10,7 +10,7 @@ broker.id=${BROKER_ID}
 zookeeper.connect=${ZOOKEEPER}
 log.dirs=${LOG_DIRS}
 num.partitions=2
-default.replication.factor=2
+default.replication.factor=1
 advertised.host.name=${ADVERTISED_HOST_NAME}
 EOF
 
