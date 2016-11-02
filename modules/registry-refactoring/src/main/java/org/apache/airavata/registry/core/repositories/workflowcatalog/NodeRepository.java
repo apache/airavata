1d96b11ee5f127ce10a418485fe00c6a57224829
scnakandala <supun.nakandala@gmail.com>
2016-11-02 14:39:24 -0400

    adding license block and breaking the tests to different classes

--- modules/registry-refactoring/src/main/java/org/apache/airavata/registry/core/repositories/workflowcatalog/NodeRepository.java
+++ modules/registry-refactoring/src/main/java/org/apache/airavata/registry/core/repositories/workflowcatalog/NodeRepository.java
@@ -1,3 +1,23 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
 package org.apache.airavata.registry.core.repositories.workflowcatalog;
 
 import org.apache.airavata.model.NodeModel;
@@ -7,9 +27,6 @@ import org.apache.airavata.registry.core.repositories.AbstractRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created by abhij on 9/28/2016.
- */
 public class NodeRepository extends AbstractRepository<NodeModel, NodeEntity, NodePK> {
 
 
