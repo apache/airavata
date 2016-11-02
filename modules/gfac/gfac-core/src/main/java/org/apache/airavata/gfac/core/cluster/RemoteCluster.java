a816eab169d88543d678677c660edbd58bdb5ae8
Shameera Rathnayaka <shameerainfo@gmail.com>
2016-11-02 17:29:28 -0400

    Revert scp thrird party refacotring

--- modules/gfac/gfac-core/src/main/java/org/apache/airavata/gfac/core/cluster/RemoteCluster.java
+++ modules/gfac/gfac-core/src/main/java/org/apache/airavata/gfac/core/cluster/RemoteCluster.java
@@ -43,7 +43,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @return jobId after successful job submission
 	 * @throws SSHApiException throws exception during error
 	 */
-	JobSubmissionOutput submitBatchJob(String jobScriptFilePath, String workingDirectory) throws SSHApiException;
+	public JobSubmissionOutput submitBatchJob(String jobScriptFilePath, String workingDirectory) throws SSHApiException;
 
 	/**
 	 * This will copy the localFile to remoteFile location in configured cluster
@@ -52,7 +52,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @param remoteFile remote file location, this can be a directory too
 	 * @throws SSHApiException throws exception during error
 	 */
-	void copyTo(String localFile, String remoteFile) throws SSHApiException;
+	public void copyTo(String localFile, String remoteFile) throws SSHApiException;
 
 	/**
 	 * This will copy a remote file in path rFile to local file lFile
@@ -60,16 +60,18 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @param remoteFile      remote file path, this has to be a full qualified path
 	 * @param localFile This is the local file to copy, this can be a directory too
 	 */
-	void copyFrom(String remoteFile, String localFile) throws SSHApiException;
+	public void copyFrom(String remoteFile, String localFile) throws SSHApiException;
 
 	/**
 	 * This wil copy source remote file to target remote file.
 	 *
 	 * @param sourceFile remote file path, this has to be a full qualified path
 	 * @param destinationFile This is the local file to copy, this can be a directory too
+     * @param session jcraft session of other coner of thirdparty file transfer.
+     * @param inOrOut direction to file transfer , to the remote cluster(DIRECTION.IN) or from the remote cluster(DIRECTION.OUT)
 	 *
 	 */
-	void thirdPartyTransfer(String sourceFile, String destinationFile, SessionConsumer<Session> sessionConsumer) throws SSHApiException;
+	public void scpThirdParty(String sourceFile, String destinationFile ,Session session , DIRECTION inOrOut, boolean ignoreEmptyFile) throws SSHApiException;
 
 	/**
 	 * This will create directories in computing resources
@@ -77,7 +79,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @param directoryPath the full qualified path for the directory user wants to create
 	 * @throws SSHApiException throws during error
 	 */
-	void makeDirectory(String directoryPath) throws SSHApiException;
+	public void makeDirectory(String directoryPath) throws SSHApiException;
 
 	/**
 	 * This will delete the given job from the queue
@@ -86,7 +88,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @return return the description of the deleted job
 	 * @throws SSHApiException throws exception during error
 	 */
-	JobStatus cancelJob(String jobID) throws SSHApiException;
+	public JobStatus cancelJob(String jobID) throws SSHApiException;
 
 	/**
 	 * This will get the job status of the the job associated with this jobId
@@ -95,7 +97,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @return job status of the given jobID
 	 * @throws SSHApiException throws exception during error
 	 */
-	JobStatus getJobStatus(String jobID) throws SSHApiException;
+	public JobStatus getJobStatus(String jobID) throws SSHApiException;
 
 	/**
 	 * This will get the job status of the the job associated with this jobId
@@ -104,7 +106,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @return jobId of the given jobName
 	 * @throws SSHApiException throws exception during error
 	 */
-	String getJobIdByJobName(String jobName, String userName) throws SSHApiException;
+	public String getJobIdByJobName(String jobName, String userName) throws SSHApiException;
 
 	/**
 	 * This method can be used to poll the jobstatuses based on the given
@@ -115,7 +117,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @param userName userName of the jobs which required to get the status
 	 * @param jobIDs   precises set of jobIDs
 	 */
-	void getJobStatuses(String userName, Map<String, JobStatus> jobIDs) throws SSHApiException;
+	public void getJobStatuses(String userName, Map<String, JobStatus> jobIDs) throws SSHApiException;
 
 	/**
 	 * This will list directories in computing resources
@@ -123,7 +125,7 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @param directoryPath the full qualified path for the directory user wants to create
 	 * @throws SSHApiException throws during error
 	 */
-	List<String> listDirectory(String directoryPath) throws SSHApiException;
+	public List<String> listDirectory(String directoryPath) throws SSHApiException;
 
 	/**
 	 * This method can use to execute custom command on remote compute resource.
@@ -131,27 +133,26 @@ public interface RemoteCluster { // FIXME: replace SSHApiException with suitable
 	 * @return <code>true</code> if command successfully executed, <code>false</code> otherwise.
 	 * @throws SSHApiException
 	 */
-	boolean execute(CommandInfo commandInfo) throws SSHApiException;
+	public boolean execute(CommandInfo commandInfo) throws SSHApiException;
 
 	/**
 	 * This method can be used to get created ssh session
 	 * to reuse the created session.
 	 */
-	Session getSession() throws SSHApiException;
+	public Session getSession() throws SSHApiException;
 
 	/**
 	 * This method can be used to close the connections initialized
 	 * to handle graceful shutdown of the system
 	 */
-	void disconnect() throws SSHApiException;
+	public void disconnect() throws SSHApiException;
 
 	/**
 	 * This gives the server Info
 	 */
-	ServerInfo getServerInfo();
+	public ServerInfo getServerInfo();
 
-    AuthenticationInfo getAuthentication();
-
+    public AuthenticationInfo getAuthentication();
     enum DIRECTION {
         TO,
         FROM
