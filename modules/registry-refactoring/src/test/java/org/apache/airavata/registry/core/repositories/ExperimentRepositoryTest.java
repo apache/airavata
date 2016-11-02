d4886e5e5fd306942d6ac75b9dc7db8edd7766ba
scnakandala <supun.nakandala@gmail.com>
2016-11-02 14:39:24 -0400

    adding license block and breaking the tests to different classes

--- modules/registry-refactoring/src/test/java/org/apache/airavata/registry/core/repositories/ExperimentRepositoryTest.java
+++ modules/registry-refactoring/src/test/java/org/apache/airavata/registry/core/repositories/ExperimentRepositoryTest.java
@@ -20,50 +20,60 @@
 */
 package org.apache.airavata.registry.core.repositories;
 
-import org.apache.airavata.model.data.replica.DataProductModel;
+import org.apache.airavata.model.experiment.ExperimentModel;
 import org.apache.airavata.model.user.UserProfile;
 import org.apache.airavata.model.workspace.Gateway;
 import org.apache.airavata.model.workspace.GatewayApprovalStatus;
-import org.apache.airavata.registry.core.entities.replicacatalog.DataProductEntity;
+import org.apache.airavata.model.workspace.Project;
+import org.apache.airavata.registry.core.entities.expcatalog.ExperimentEntity;
 import org.apache.airavata.registry.core.entities.workspacecatalog.GatewayEntity;
+import org.apache.airavata.registry.core.entities.workspacecatalog.ProjectEntity;
 import org.apache.airavata.registry.core.entities.workspacecatalog.UserProfileEntity;
-import org.apache.airavata.registry.core.repositories.replicacatalog.DataProductRepository;
+import org.apache.airavata.registry.core.repositories.expcatalog.ExperimentRepository;
 import org.apache.airavata.registry.core.repositories.workspacecatalog.GatewayRepository;
+import org.apache.airavata.registry.core.repositories.workspacecatalog.ProjectRepository;
 import org.apache.airavata.registry.core.repositories.workspacecatalog.UserProfileRepository;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ReplicaCatalogRepositoryTest {
+public class ExperimentRepositoryTest {
+    private final static Logger logger = LoggerFactory.getLogger(ExperimentRepositoryTest.class);
 
     private GatewayRepository gatewayRepository;
     private UserProfileRepository userProfileRepository;
+    private ProjectRepository projectRepository;
+    private ExperimentRepository experimentRepository;
     private String gatewayId;
     private String userId;
-    private String dataProductUri;
+    private String projectId;
+    private String experimentId;
 
     private final String GATEWAY_DOMAIN = "test1.com";
-    private final String DATA_PRODUCT_DESCRIPTION = "testDesc";
+    private final String EXPERIMENT_NAME = "sample experiment";
+    private final String EXPERIMENT_DESCRIPTION = "sample description";
 
 
     @Before
-    public void setupRepository()   {
+    public void setupRepository() {
 
         gatewayRepository = new GatewayRepository(Gateway.class, GatewayEntity.class);
         userProfileRepository = new UserProfileRepository(UserProfile.class, UserProfileEntity.class);
-
+        projectRepository = new ProjectRepository(Project.class, ProjectEntity.class);
+        experimentRepository = new ExperimentRepository(ExperimentModel.class, ExperimentEntity.class);
 
         gatewayId = "test.com" + System.currentTimeMillis();
         userId = "testuser" + System.currentTimeMillis();
-        dataProductUri = "uri" + System.currentTimeMillis();
-
+        projectId = "project" + System.currentTimeMillis();
+        experimentId = "exp" + System.currentTimeMillis();
     }
+
     @Test
-    public void dataProductRepositoryTest() {
+    public void experimentRepositoryTest() {
 
-        DataProductRepository dataProductRepository = new DataProductRepository(DataProductModel.class, DataProductEntity.class);
-
-        /*
+		/*
          * Creating Gateway required for UserProfile & Project creation
 		 */
         Gateway gateway = new Gateway();
@@ -83,41 +93,56 @@ public class ReplicaCatalogRepositoryTest {
         Assert.assertTrue(!userProfile.getAiravataInternalUserId().isEmpty());
 
         /*
-         * DataProduct Instance creation
+         * Project Instance creation
          */
-        DataProductModel dataProduct = new DataProductModel();
-        dataProduct.setProductUri(dataProductUri);
-        dataProduct.setGatewayId(gatewayId);
-        dataProduct.setOwnerName(gatewayId);
-        dataProduct.setProductName("Product1234");
+        Project project = new Project();
+        project.setGatewayId(gatewayId);
+        project.setOwner(userId);
+        project.setProjectID(projectId);
+        project.setGatewayIdIsSet(true);
+        project = projectRepository.create(project);
+        Assert.assertTrue(!project.getProjectID().isEmpty());
+
+        /*
+         * Experiment Instance Creation
+         */
+
+        ExperimentModel experiment = new ExperimentModel();
+        experiment.setExperimentId(experimentId);
+        experiment.setExperimentName(EXPERIMENT_NAME);
+        experiment.setGatewayId(gatewayId);
+        experiment.setUserName(userId);
+        experiment.setProjectId(projectId);
+
+        /*
+         * Experiment Repository Insert Operation Test
+		 */
+        experiment = experimentRepository.create(experiment);
+        Assert.assertTrue(!experiment.getExperimentId().isEmpty());
 
 
         /*
-         * Data Product Repository Insert Operation Test
+         * Experiment Repository Update Operation Test
 		 */
-        dataProduct = dataProductRepository.create(dataProduct);
-        Assert.assertTrue(!dataProduct.getProductUri().isEmpty());
-
-
-
-        /*
-         * DataProduct Repository Update Operation Test
-		 */
-        dataProduct.setProductDescription(DATA_PRODUCT_DESCRIPTION);
-        dataProductRepository.update(dataProduct);
-        dataProduct = dataProductRepository.get(dataProduct.getProductUri());
-        Assert.assertEquals(dataProduct.getProductDescription(), DATA_PRODUCT_DESCRIPTION);
+        experiment.setDescription(EXPERIMENT_DESCRIPTION);
+        experimentRepository.update(experiment);
+        experiment = experimentRepository.get(experimentId);
+        Assert.assertEquals(experiment.getDescription(), EXPERIMENT_DESCRIPTION);
 
 		/*
-         * Data Product Repository Select Operation Test
+         * Workspace Project Repository Select Operation Test
 		 */
-        dataProduct = dataProductRepository.get(dataProductUri);
-        Assert.assertNotNull(dataProduct);
+        experiment = experimentRepository.get(experimentId);
+        Assert.assertNotNull(experiment);
 
 		/*
-         * Workspace Project Repository Delete Operation
+         * Experiment Repository Delete Operation
 		 */
-        boolean deleteResult = dataProductRepository.delete(dataProductUri);
+
+        boolean deleteResult = experimentRepository.delete(experimentId);
+        Assert.assertTrue(deleteResult);
+
+        deleteResult = projectRepository.delete(projectId);
         Assert.assertTrue(deleteResult);
 
         deleteResult = userProfileRepository.delete(userId);
@@ -126,5 +151,7 @@ public class ReplicaCatalogRepositoryTest {
         deleteResult = gatewayRepository.delete(gatewayId);
         Assert.assertTrue(deleteResult);
 
+
     }
+
 }
