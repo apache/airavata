8fac7256b9f60e0b845ce8785642bbc5c6720777
scnakandala <supun.nakandala@gmail.com>
2017-02-23 21:01:38 -0500

    disabling the cache back

--- modules/sharing-registry/sharing-registry-server/src/main/java/org/apache/airavata/sharing/registry/db/utils/JPAUtils.java
+++ modules/sharing-registry/sharing-registry-server/src/main/java/org/apache/airavata/sharing/registry/db/utils/JPAUtils.java
@@ -83,10 +83,10 @@ public class JPAUtils {
             properties.put("openjpa.DynamicEnhancementAgent", "true");
             properties.put("openjpa.RuntimeUnenhancedClasses", "unsupported");
 
-            properties.put("openjpa.DataCache", "" + readServerProperties(JPA_CACHE_ENABLED)
-                    + "(CacheSize=" + Integer.valueOf(readServerProperties(JPA_CACHE_SIZE)) + ", SoftReferenceSize=0)");
-            properties.put("openjpa.QueryCache", "" + readServerProperties(JPA_CACHE_ENABLED)
-                    + "(CacheSize=" + Integer.valueOf(readServerProperties(JPA_CACHE_SIZE)) + ", SoftReferenceSize=0)");
+//            properties.put("openjpa.DataCache", "" + readServerProperties(JPA_CACHE_ENABLED)
+//                    + "(CacheSize=" + Integer.valueOf(readServerProperties(JPA_CACHE_SIZE)) + ", SoftReferenceSize=0)");
+//            properties.put("openjpa.QueryCache", "" + readServerProperties(JPA_CACHE_ENABLED)
+//                    + "(CacheSize=" + Integer.valueOf(readServerProperties(JPA_CACHE_SIZE)) + ", SoftReferenceSize=0)");
 
             properties.put("openjpa.RemoteCommitProvider", "sjvm");
             properties.put("openjpa.Log", "DefaultLevel=INFO, Runtime=INFO, Tool=INFO, SQL=INFO");
