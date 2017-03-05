c29022f4b26fad7b66581e8c425bcab624ecfc96
Gourav Shenoy <shenoy.200@gmail.com>
2017-03-05 18:16:01 -0500

    Add ProfileService Server and Client

--- thrift-interface-descriptions/service-cpis/profile-service/profile-user/profile-user-cpi.thrift
+++ thrift-interface-descriptions/service-cpis/profile-service/profile-user/profile-user-cpi.thrift
@@ -30,6 +30,7 @@ include "profile_user_cpi_errors.thrift"
 namespace java org.apache.airavata.service.profile.user.cpi
 
 const string USER_PROFILE_CPI_VERSION = "0.17"
+const string USER_PROFILE_CPI_NAME = "UserProfileService"
 
 service UserProfileService {
 
