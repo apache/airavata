ccc92aa3701461a39438ea32f64a07b1b19f72b1
Gourav Shenoy <shenoy.200@gmail.com>
2017-03-21 16:08:08 -0400

    Add Zookeeper utilities

--- modules/db-event-manager/src/main/java/org/apache/airavata/db/event/manager/utils/Constants.java
+++ modules/db-event-manager/src/main/java/org/apache/airavata/db/event/manager/utils/Constants.java
@@ -1,3 +1,23 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
 package org.apache.airavata.db.event.manager.utils;
 
 import java.util.HashMap;
@@ -9,10 +29,5 @@ import java.util.Map;
  */
 public class Constants {
 
-    //FIXME: value can be list of String or list of publisher objects
-    public static final Map<String, List<String>> DB_EVENT_MAP;
-    static{
-        //populate map based on ZK folder structure (cache)
-        DB_EVENT_MAP = new HashMap<>();
-    }
+    public static final String DB_EVENT_MGR_ZK_PATH = "db-event-mgr";
 }
