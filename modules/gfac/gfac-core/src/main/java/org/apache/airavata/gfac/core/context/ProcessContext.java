e769feb8257c5d95bbbed1145819a765216f4f51
Marcus Christie <machristie@apache.org>
2017-03-25 20:29:32 -0400

    AIRAVATA-2339 Attempting to fix data staging with individual account
    In ProcessContext I removed use of isUseUserCRPref() in methods related to
    getting gateway storage information. Whether the current experiment is submitted
    with user's compute resource preferences or not should not affect gateway
    storage information. How to specify user's storage preferences is not yet
    implemented and must be implemented with something other than isUseUserCRPref().
    
    In SCPDataStageTask, I changed the construction of the sshSession for the SCP
    connection to the compute resource to use
    getComputerResourceSSHKeyAuthentication() instead of
    getStorageSSHKeyAuthentication(), which would not be correct, if I understand
    the code correctly.

--- modules/gfac/gfac-core/src/main/java/org/apache/airavata/gfac/core/context/ProcessContext.java
+++ modules/gfac/gfac-core/src/main/java/org/apache/airavata/gfac/core/context/ProcessContext.java
@@ -464,19 +464,10 @@ public class ProcessContext {
 	}
 
 	public String getStorageResourceCredentialToken(){
-		if (isUseUserCRPref()) {
-			if (userStoragePreference != null &&
-					isValid(userStoragePreference.getResourceSpecificCredentialStoreToken())) {
-				return userStoragePreference.getResourceSpecificCredentialStoreToken();
-			} else {
-				return userResourceProfile.getCredentialStoreToken();
-			}
+		if (isValid(gatewayStorageResourcePreference.getResourceSpecificCredentialStoreToken())) {
+			return gatewayStorageResourcePreference.getResourceSpecificCredentialStoreToken();
 		} else {
-			if (isValid(gatewayStorageResourcePreference.getResourceSpecificCredentialStoreToken())) {
-				return gatewayStorageResourcePreference.getResourceSpecificCredentialStoreToken();
-			} else {
-				return gatewayResourceProfile.getCredentialStoreToken();
-			}
+			return gatewayResourceProfile.getCredentialStoreToken();
 		}
 	}
 
@@ -616,33 +607,15 @@ public class ProcessContext {
 	}
 
 	public String getStorageResourceLoginUserName(){
-		if (isUseUserCRPref() &&
-				userStoragePreference != null &&
-				isValid(userStoragePreference.getLoginUserName())) {
-			return userStoragePreference.getLoginUserName();
-		} else {
-			return gatewayStorageResourcePreference.getLoginUserName();
-		}
+		return gatewayStorageResourcePreference.getLoginUserName();
 	}
 
 	public String getStorageFileSystemRootLocation(){
-		if (isUseUserCRPref() &&
-				userStoragePreference != null &&
-				isValid(userStoragePreference.getFileSystemRootLocation())) {
-			return userStoragePreference.getFileSystemRootLocation();
-		} else {
-			return gatewayStorageResourcePreference.getFileSystemRootLocation();
-		}
+		return gatewayStorageResourcePreference.getFileSystemRootLocation();
 	}
 
 	public String getStorageResourceId() {
-		if (isUseUserCRPref() &&
-				userStoragePreference != null &&
-				isValid(userStoragePreference.getStorageResourceId())) {
-			return userStoragePreference.getStorageResourceId();
-		} else {
-			return gatewayStorageResourcePreference.getStorageResourceId();
-		}
+		return gatewayStorageResourcePreference.getStorageResourceId();
 	}
 
 	private ComputationalResourceSchedulingModel getProcessCRSchedule() {
