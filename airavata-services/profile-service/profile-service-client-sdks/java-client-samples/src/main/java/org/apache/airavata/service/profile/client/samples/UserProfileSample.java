e395b1a909b03017c25fffa37b219cc7cdb542df
Gourav Shenoy <shenoy.200@gmail.com>
2017-03-27 13:48:00 -0400

    Add UserProfile sample in client-sdks

---
+++ airavata-services/profile-service/profile-service-client-sdks/java-client-samples/src/main/java/org/apache/airavata/service/profile/client/samples/UserProfileSample.java
@@ -0,0 +1,70 @@
+package org.apache.airavata.service.profile.client.samples;
+
+import org.apache.airavata.model.user.*;
+import org.apache.airavata.service.profile.client.ProfileServiceClientFactory;
+import org.apache.airavata.service.profile.client.util.ProfileServiceClientUtil;
+import org.apache.airavata.service.profile.user.cpi.UserProfileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by goshenoy on 3/23/17.
+ */
+public class UserProfileSample {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserProfileSample.class);
+    private static UserProfileService.Client userProfileClient;
+
+    public static void main(String[] args) throws Exception {
+        try {
+            String profileServiceServerHost = ProfileServiceClientUtil.getProfileServiceServerHost();
+            int profileServiceServerPort = ProfileServiceClientUtil.getProfileServiceServerPort();
+
+            userProfileClient = ProfileServiceClientFactory.createUserProfileServiceClient(profileServiceServerHost, profileServiceServerPort);
+            userProfileClient.addUserProfile(getUserProfile());
+
+        } catch (Exception ex) {
+            logger.error("UserProfile client-sample Exception: " + ex, ex);
+        }
+    }
+
+    private static UserProfile getUserProfile() {
+        UserProfile userProfile = new UserProfile();
+        userProfile.setUserModelVersion("model-001");
+        userProfile.setAiravataInternalUserId("test-user-internal-001");
+        userProfile.setUserId("test-user-001");
+        userProfile.setGatewayId("test-gateway-001");
+
+        List<String> emails = new ArrayList<>();
+        emails.add("test-user-001@domain1.com");
+        emails.add("test-user-001@domain2.com");
+        userProfile.setEmails(emails);
+
+        userProfile.setUserName("test-username-001");
+        userProfile.setValidUntil(new Date().toString());
+        userProfile.setState(Status.ACTIVE);
+        userProfile.setNsfDemographics(getNSFDemographics());
+
+        return userProfile;
+    }
+
+    private static NSFDemographics getNSFDemographics() {
+        NSFDemographics nsfDemographics = new NSFDemographics();
+        nsfDemographics.setGender("male");
+        nsfDemographics.setUsCitizenship(USCitizenship.US_CITIZEN);
+
+        List<ethnicity> ethnicities = new ArrayList<>();
+        ethnicities.add(ethnicity.NOT_HISPANIC_LATINO);
+        List<race> races = new ArrayList<>();
+        races.add(race.AMERICAN_INDIAN_OR_ALASKAN_NATIVE);
+
+        nsfDemographics.setEthnicities(ethnicities);
+        nsfDemographics.setRaces(races);
+        return nsfDemographics;
+    }
+
+}
