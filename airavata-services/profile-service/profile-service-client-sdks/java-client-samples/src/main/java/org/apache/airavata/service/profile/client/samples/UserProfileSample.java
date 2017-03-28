e1a31bbc4dd1993c49102f4371477e605b890c37
Gourav Shenoy <shenoy.200@gmail.com>
2017-03-28 12:19:33 -0400

    Add: jpa enhancer to pom, Add: adduserprofile sample to java-client-sdk

--- airavata-services/profile-service/profile-service-client-sdks/java-client-samples/src/main/java/org/apache/airavata/service/profile/client/samples/UserProfileSample.java
+++ airavata-services/profile-service/profile-service-client-sdks/java-client-samples/src/main/java/org/apache/airavata/service/profile/client/samples/UserProfileSample.java
@@ -1,15 +1,14 @@
 package org.apache.airavata.service.profile.client.samples;
 
 import org.apache.airavata.model.user.*;
+import org.apache.airavata.model.workspace.User;
 import org.apache.airavata.service.profile.client.ProfileServiceClientFactory;
 import org.apache.airavata.service.profile.client.util.ProfileServiceClientUtil;
 import org.apache.airavata.service.profile.user.cpi.UserProfileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by goshenoy on 3/23/17.
@@ -38,32 +37,24 @@ public class UserProfileSample {
         userProfile.setAiravataInternalUserId("test-user-internal-001");
         userProfile.setUserId("test-user-001");
         userProfile.setGatewayId("test-gateway-001");
-
         userProfile.addToEmails("test-user-001@domain1.com");
         userProfile.addToEmails("test-user-002@domain1.com");
-
         userProfile.setUserName("test-username-001");
-        userProfile.setCreationTime(Long.toString(new Date().getTime()));
-        userProfile.setLastAccessTime(Long.toString(new Date().getTime()));
-        userProfile.setValidUntil(Long.toString(new Date().getTime()));
+        userProfile.setCreationTime(new Date().getTime());
+        userProfile.setLastAccessTime(new Date().getTime());
+        userProfile.setValidUntil(new Date().getTime());
         userProfile.setState(Status.ACTIVE);
         userProfile.setNsfDemographics(getNSFDemographics());
-
         return userProfile;
     }
 
     private static NSFDemographics getNSFDemographics() {
         NSFDemographics nsfDemographics = new NSFDemographics();
+        nsfDemographics.setAiravataInternalUserId("test-user-internal-001");
         nsfDemographics.setGender("male");
         nsfDemographics.setUsCitizenship(USCitizenship.US_CITIZEN);
-
-        List<ethnicity> ethnicities = new ArrayList<>();
-        ethnicities.add(ethnicity.NOT_HISPANIC_LATINO);
-        List<race> races = new ArrayList<>();
-        races.add(race.AMERICAN_INDIAN_OR_ALASKAN_NATIVE);
-
-        nsfDemographics.setEthnicities(ethnicities);
-        nsfDemographics.setRaces(races);
+        nsfDemographics.addToEthnicities(ethnicity.NOT_HISPANIC_LATINO);
+        nsfDemographics.addToRaces(race.AMERICAN_INDIAN_OR_ALASKAN_NATIVE);
         return nsfDemographics;
     }
 
