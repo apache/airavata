b65c3d56eb57546136dfb6ffc40c03d977fc2795
Gourav Shenoy <shenoy.200@gmail.com>
2017-03-29 12:55:39 -0400

    Update ProfileServiceUtils and UserProfileServiceHandler

--- airavata-services/profile-service/profile-service-client-sdks/java-client-samples/src/main/java/org/apache/airavata/service/profile/client/samples/UserProfileSample.java
+++ airavata-services/profile-service/profile-service-client-sdks/java-client-samples/src/main/java/org/apache/airavata/service/profile/client/samples/UserProfileSample.java
@@ -9,6 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by goshenoy on 3/23/17.
@@ -18,7 +21,7 @@ public class UserProfileSample {
     private static final Logger logger = LoggerFactory.getLogger(UserProfileSample.class);
     private static UserProfileService.Client userProfileClient;
     private static String testUserId = null;
-    private static String testGatewayId = "test-gateway-001";
+    private static String testGatewayId = "test-client-gateway";
 
     public static void main(String[] args) throws Exception {
         try {
@@ -29,13 +32,42 @@ public class UserProfileSample {
 
             // test add-user-profile
             testUserId = userProfileClient.addUserProfile(getUserProfile());     // (1) run this only once
+            assert (testUserId != null) : "User creation failed. Null userId returned!";
+            System.out.println("User created with userId: " + testUserId);
 
             // test find-user-profile
             UserProfile userProfile = userProfileClient.getUserProfileById(testUserId, testGatewayId);
             assert (userProfile != null) : "Could not find user with userId: " + testUserId + ", and gatewayID: " + testGatewayId;
             System.out.println("UserProfile: " + userProfile);
 
+            // test update-user-profile : update name
+            userProfile = getUserProfile();
+            String newUserName = userProfile.getUserName().replaceAll("username", "username-updated");
+            userProfile.setUserName(newUserName);
+            boolean updateSuccess = userProfileClient.updateUserProfile(userProfile);
+            assert (updateSuccess) : "User update with new userName: [" + newUserName + "], Failed!";
+            System.out.println("User update with new userName: [" + newUserName + "], Successful!");
 
+            // test get-all-userprofiles
+            List<UserProfile> userProfileList = userProfileClient.getAllUserProfilesInGateway(testGatewayId, 0, 5);
+            assert (userProfileList != null && !userProfileList.isEmpty()) : "Failed to retrieve users for gateway!";
+            for (UserProfile userProfile1 : userProfileList) {
+                System.out.println("User found with userId: " + userProfile1.getUserId());
+            }
+
+            // test find-user-profile-by-name
+            userProfile = userProfileClient.getUserProfileByName(newUserName, testGatewayId);
+            assert (userProfile != null) : "Could not find user with userName: " + newUserName;
+            System.out.println("UserProfile: " + userProfile);
+
+            // test delete-user-profile
+            boolean deleteSuccess = userProfileClient.deleteUserProfile(testUserId);
+            assert (deleteSuccess) : "Delete user failed for userId: " + testUserId;
+            System.out.println("Successfully deleted user with userId: " + testUserId);
+
+            // test-check-user-exist
+            boolean userExists = userProfileClient.doesUserExist(newUserName, testGatewayId);
+            assert (!userExists) : "User should not exist, but it does.";
 
 
         } catch (Exception ex) {
@@ -44,25 +76,30 @@ public class UserProfileSample {
     }
 
     private static UserProfile getUserProfile() {
+        // get random value for userId
+        int userIdValue = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+
+        // construct userProfile object
         UserProfile userProfile = new UserProfile();
-        userProfile.setUserModelVersion("model-001");
-        userProfile.setAiravataInternalUserId("test-user-internal-001");
-        userProfile.setUserId("test-user-001");
+        userProfile.setUserModelVersion("model-" + userIdValue);
+        userProfile.setAiravataInternalUserId("test-user-internal-" + userIdValue);
+        userProfile.setUserId("test-user-" + userIdValue);
         userProfile.setGatewayId(testGatewayId);
-        userProfile.addToEmails("test-user-001@domain1.com");
-        userProfile.addToEmails("test-user-002@domain1.com");
-        userProfile.setUserName("test-username-001");
+        userProfile.addToEmails("test-user-" + userIdValue + "@domain1.com");
+        userProfile.addToEmails("test-user-" + userIdValue + "@domain2.com");
+        userProfile.setUserName("test-username-" + userIdValue);
         userProfile.setCreationTime(new Date().getTime());
         userProfile.setLastAccessTime(new Date().getTime());
         userProfile.setValidUntil(new Date().getTime());
         userProfile.setState(Status.ACTIVE);
-        userProfile.setNsfDemographics(getNSFDemographics());
+        userProfile.setNsfDemographics(getNSFDemographics(userIdValue));
         return userProfile;
     }
 
-    private static NSFDemographics getNSFDemographics() {
+    private static NSFDemographics getNSFDemographics(int userIdValue) {
+        // construct nsfdemographics object
         NSFDemographics nsfDemographics = new NSFDemographics();
-        nsfDemographics.setAiravataInternalUserId("test-user-internal-001");
+        nsfDemographics.setAiravataInternalUserId("test-user-internal-" + userIdValue);
         nsfDemographics.setGender("male");
         nsfDemographics.setUsCitizenship(USCitizenship.US_CITIZEN);
         nsfDemographics.addToEthnicities(ethnicity.NOT_HISPANIC_LATINO);
