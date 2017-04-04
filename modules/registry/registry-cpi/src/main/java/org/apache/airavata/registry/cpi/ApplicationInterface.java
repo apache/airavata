25489fdf92914af65410025436e2c162d67c47ae
Suresh Marru <smarru@apache.org>
2017-04-04 07:59:57 -0400

    Updating license headers using license-maven-plugin

--- modules/registry/registry-cpi/src/main/java/org/apache/airavata/registry/cpi/ApplicationInterface.java
+++ modules/registry/registry-cpi/src/main/java/org/apache/airavata/registry/cpi/ApplicationInterface.java
@@ -1,4 +1,5 @@
 /**
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,7 +17,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.airavata.registry.cpi;
 
 import org.apache.airavata.model.appcatalog.appdeployment.ApplicationModule;
