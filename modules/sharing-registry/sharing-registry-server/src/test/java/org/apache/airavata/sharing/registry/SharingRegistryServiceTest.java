7ba98fa399de6370d87537386bcfa7bbdaf183c6
Gourav Shenoy <shenoy.200@gmail.com>
2017-04-04 15:33:40 -0400

    Manually refactor Supuns changes to getUpdatedObject from develop branch

--- modules/sharing-registry/sharing-registry-server/src/test/java/org/apache/airavata/sharing/registry/SharingRegistryServiceTest.java
+++ modules/sharing-registry/sharing-registry-server/src/test/java/org/apache/airavata/sharing/registry/SharingRegistryServiceTest.java
@@ -145,6 +145,9 @@ public class SharingRegistryServiceTest {
         userGroup1.setGroupType(GroupType.USER_LEVEL_GROUP);
 
         sharingServiceClient.createGroup(userGroup1);
+        userGroup1.setDescription("updated description");
+        sharingServiceClient.updateGroup(userGroup1);
+        Assert.assertTrue(sharingServiceClient.getGroup(domainId, userGroup1.groupId).description.equals("updated description"));
 
         UserGroup userGroup2 = new UserGroup();
         //required
