9f5e30e808c962cb9e6c7e63b231dadf61085a36
Marcus Christie <machrist@iu.edu>
2017-04-19 14:43:51 -0400

    DRF tutorial 2: @api_view decorator and content type negotiation

--- django_airavata/apps/api/urls.py
+++ django_airavata/apps/api/urls.py
@@ -1,9 +1,12 @@
 
-from django.conf.urls import url
-
 from . import views
+
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^projects/$', views.project_list),
     url(r'^projects/(?P<project_id>[^\s]+)/$', views.project_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- django_airavata/apps/api/views.py
+++ django_airavata/apps/api/views.py
@@ -1,30 +1,30 @@
 
 from . import serializers
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-@csrf_exempt
-def project_list(request):
-    if request.method == 'GET':
+@api_view(['GET'])
+def project_list(request, format=None):
+    gateway_id = settings.GATEWAY_ID
+    username = request.user.username
 
-        gateway_id = settings.GATEWAY_ID
-        username = request.user.username
+    projects = request.airavata_client.getUserProjects(request.authz_token, gateway_id, username, -1, 0)
+    serializer = serializers.ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
 
-        projects = request.airavata_client.getUserProjects(request.authz_token, gateway_id, username, -1, 0)
-        serializer = serializers.ProjectSerializer(projects, many=True)
-        return JsonResponse(serializer.data, safe=False)
+@api_view(['GET'])
+def project_detail(request, project_id, format=None):
+    gateway_id = settings.GATEWAY_ID
+    username = request.user.username
 
-@csrf_exempt
-def project_detail(request, project_id):
-    if request.method == 'GET':
-
-        gateway_id = settings.GATEWAY_ID
-        username = request.user.username
-
-        project = request.airavata_client.getProject(request.authz_token, project_id)
-        serializer = serializers.ProjectSerializer(project)
-        return JsonResponse(serializer.data)
+    project = request.airavata_client.getProject(request.authz_token, project_id)
+    serializer = serializers.ProjectSerializer(project)
+    return Response(serializer.data)
