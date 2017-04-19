f7d88b2375f4f4eda37a094a4aa362e5ea223b81
Marcus Christie <machrist@iu.edu>
2017-04-19 14:56:58 -0400

    DRF tutorial: refactored as class based views

--- django_airavata/apps/api/urls.py
+++ django_airavata/apps/api/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^projects/$', views.project_list),
-    url(r'^projects/(?P<project_id>[^\s]+)/$', views.project_detail),
+    url(r'^projects/$', views.ProjectList.as_view()),
+    url(r'^projects/(?P<project_id>[^\s]+)/$', views.ProjectDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- django_airavata/apps/api/views.py
+++ django_airavata/apps/api/views.py
@@ -2,7 +2,7 @@
 from . import serializers
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from django.conf import settings
@@ -11,20 +11,22 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-@api_view(['GET'])
-def project_list(request, format=None):
-    gateway_id = settings.GATEWAY_ID
-    username = request.user.username
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        gateway_id = settings.GATEWAY_ID
+        username = request.user.username
 
-    projects = request.airavata_client.getUserProjects(request.authz_token, gateway_id, username, -1, 0)
-    serializer = serializers.ProjectSerializer(projects, many=True)
-    return Response(serializer.data)
+        projects = request.airavata_client.getUserProjects(request.authz_token, gateway_id, username, -1, 0)
+        serializer = serializers.ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+    # TODO: add project creation
 
-@api_view(['GET'])
-def project_detail(request, project_id, format=None):
-    gateway_id = settings.GATEWAY_ID
-    username = request.user.username
+class ProjectDetail(APIView):
+    def get(self, request, project_id, format=None):
+        gateway_id = settings.GATEWAY_ID
+        username = request.user.username
 
-    project = request.airavata_client.getProject(request.authz_token, project_id)
-    serializer = serializers.ProjectSerializer(project)
-    return Response(serializer.data)
+        project = request.airavata_client.getProject(request.authz_token, project_id)
+        serializer = serializers.ProjectSerializer(project)
+        return Response(serializer.data)
+    # TODO: add project update (PUT)
