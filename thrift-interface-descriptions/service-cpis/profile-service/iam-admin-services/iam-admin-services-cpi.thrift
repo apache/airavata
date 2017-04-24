3165748b7bc4b01ce896fa9281f6ae3e5d970e87
Anuj Bhandar <bhandar.anuj@gmail.com>
2017-04-23 23:29:58 -0400

    adding boiler plate code for iam admin services

---
+++ thrift-interface-descriptions/service-cpis/profile-service/iam-admin-services/iam-admin-services-cpi.thrift
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+/*
+ * Component Programming Interface definition for Apache Airavata Tenant profile Service.
+ *
+*/
+
+include "../../../airavata-apis/airavata_errors.thrift"
+include "../../../airavata-apis/security_model.thrift"
+include "../../../data-models/experiment-catalog-models/workspace_model.thrift"
+include "iam_admin_services_cpi_errors.thrift"
+
+namespace java org.apache.airavata.service.profile.iam.admin.services.cpi
+
+const string IAM_ADMIN_SERVICES_CPI_VERSION = "0.17"
+const string IAM_ADMIN_SERVICES_CPI_NAME = "IamAdminServices"
+
+service IamAdminServices {
+
+    string getAPIVersion (1: required security_model.AuthzToken authzToken)
+                       throws (1: iam_admin_services_cpi_errors.IamAdminServicesException Idse,
+                               2: airavata_errors.AuthorizationException ae)
+
+    string setUpGateway (1: required security_model.AuthzToken authzToken,
+                       2: required workspace_model.Gateway gateway)
+                    throws (1: iam_admin_services_cpi_errors.IamAdminServicesException Idse,
+                            2: airavata_errors.AuthorizationException ae)
+
+}
--- thrift-interface-descriptions/service-cpis/profile-service/iam-admin-services/iam_admin_services_cpi_errors.thrift
+++ thrift-interface-descriptions/service-cpis/profile-service/iam-admin-services/iam_admin_services_cpi_errors.thrift
@@ -18,17 +18,15 @@
  *
  */
 
- include "profile-user/profile-user-cpi.thrift"
- include "profile-tenant/profile-tenant-cpi.thrift"
- include "iam-admin-services/iam-admin-services-cpi.thrift"
+/*
+* This file describes the definitions of the Error Messages that can occur
+*  when invoking Apache Airavata Services through the API. In addition Thrift provides
+*  built in funcationality to raise TApplicationException for all internal server errors.
+*/
 
- namespace java org.apache.airavata.service.profile
- namespace php Airavata.Service.Profile
- namespace cpp apache.airavata.service.profile
- namespace py apache.airavata.service.profile
+namespace java org.apache.airavata.service.profile.iam.admin.services.cpi.exception
+namespace php Airavata.Service.Iam.Admin.Services.CPI.Error
 
- /*
-  * This file describes the definitions of the Airavata Execution Data Structures. Each of the
-  *   language specific Airavata Client SDK's will translate this neutral data model into an
-  *   appropriate form for passing to the Airavata Server Execution API Calls.
- */
+exception IamAdminServicesException {
+  1: required string message
+}
