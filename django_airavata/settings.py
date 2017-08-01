44cfb930b09a993baf7cee424de51ca5ed1397a8
Marcus Christie <machrist@iu.edu>
2017-08-01 12:17:52 -0400

    Added a simple Bootstrap-based base template

--- django_airavata/settings.py
+++ django_airavata/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'django_airavata.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "django_airavata", "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "django_airavata", "static")]
 
 AUTHENTICATION_BACKENDS = [
     'django_airavata.apps.auth.backends.KeycloakBackend'
