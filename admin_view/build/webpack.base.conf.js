45cbc1398d92924ded5a21d5749d2784c8a0f569
eldhomathulla <eldhomathulla@gmail.com>
2017-09-05 14:48:03 -0400

    Changing the webpack configuration to expose the modules as a library

--- admin_view/build/webpack.base.conf.js
+++ admin_view/build/webpack.base.conf.js
@@ -16,7 +16,9 @@ module.exports = {
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.dev.assetsPublicPath,
+    libraryTarget: 'var',
+    library: 'AiravataApp'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
--- admin_view/build/webpack.prod.conf.js
+++ admin_view/build/webpack.prod.conf.js
@@ -37,7 +37,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
