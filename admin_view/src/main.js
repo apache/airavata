f7b5b9d5dc722293c2ce575dcc3392bbdce79a20
eldhomathulla <eldhomathulla@gmail.com>
2017-09-05 14:49:54 -0400

    Adding vue router and file relocation changes

--- admin_view/src/main.js
+++ admin_view/src/main.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import ExperimentsDashboard from './dashboards/ExperimentDashboard.vue'
+import AdminDashboard from './dashboards/AdminDashboard.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
 
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
 
-})
+
+export function initializeApacheAiravataDashboard(dashboardName) {
+  return new Vue({
+    el: '#app',
+    template: '<' + dashboardName + '/>',
+    components: {ExperimentsDashboard, AdminDashboard}
+
+  })
+};
+
 
