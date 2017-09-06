8aa2deaaf4174d89e77c8d62072573e04a182eec
eldhomathulla <eldhomathulla@gmail.com>
2017-09-06 08:59:56 -0400

    adding deployment component tab

---
+++ admin_view/src/components/admin/ApplicationDeployments.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main_section">
+    <h4>Application Deployments</h4>
+  </div>
+</template>
+<style>
+  .heading{
+    font-size:1.0em;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+</style>
+
