cd8aa62e0276de55ac63149d88085d7df4822a0c
eldhomathulla <eldhomathulla@gmail.com>
2017-09-06 09:02:19 -0400

    adding the new application tab routes

--- admin_view/src/router.js
+++ admin_view/src/router.js
@@ -1,9 +1,32 @@
 import AdminDashboardHome from './dashboards/AdminDashboardHome.vue'
 import NewApplication from './components/admin/NewApplication.vue'
+import ExperimentsDashboard from './dashboards/ExperimentDashboard.vue'
+import ApplicationDetails from'./components/admin/ApplicationDetails.vue'
+import ApplicationInterface from './components/admin/ApplicationInterface.vue'
+import ApplicationDeployments from  './components/admin/ApplicationDeployments.vue'
 import VueRouter from 'vue-router'
+
+
 const routes=[
-  { path: '/admin/new/application', component: NewApplication,name:'newapp' },
-  { path: '/', component: AdminDashboardHome }
+  { path: '/new/application', component: NewApplication,name:'newapp',
+  children:[
+    {
+      path:'details',
+      component:ApplicationDetails,
+      name:'details'
+    },{
+      path:'interface',
+      component:ApplicationInterface,
+      name:'interface'
+    },{
+      path:'deployments',
+      component:ApplicationDeployments,
+      name:'deployments'
+
+    }
+  ]},
+  { path: '/', component: AdminDashboardHome },
+  {path:'/experiments',component:ExperimentsDashboard}
 ];
 export default new VueRouter({
   routes:routes
