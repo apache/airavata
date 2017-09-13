c10a85de05c9a06b7d58455832f73dd1fbc50207
Marcus Christie <machrist@iu.edu>
2017-09-12 20:55:43 -0400

    Simple Vue component build setup

---
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/views/ProjectList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul id="project-list">
+        <li v-for="project in projects">
+            {{ project.name }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'project-list',
+  data () {
+    return {
+      projects: [
+              {"name": "Project 1"},
+              {"name": "Project 2"},
+              {"name": "Project 3"},
+              {"name": "Project 4"},
+          ]
+      }
+  }
+}
+</script>
+
+<style>
+</style>
