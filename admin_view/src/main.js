b6d460e0a8379d1cf1618551972f34ba753f9dd8
eldhomathulla <eldhomathulla@gmail.com>
2017-09-21 09:04:18 -0400

    adding vue store

--- admin_view/src/main.js
+++ admin_view/src/main.js
@@ -3,20 +3,25 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 
-import ExperimentsDashboard from './components/dashboards/ExperimentDashboard.vue'
-import AdminDashboard from './components/dashboards/AdminDashboard.vue'
-import router from './router'
+import ExperimentsDashboard from './components/dashboards/ExperimentDashboard.vue';
+import AdminDashboard from './components/dashboards/AdminDashboard.vue';
+
+import router from './router';
+import storeParams from './store';
+
 Vue.config.productionTip = false;
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 Vue.use(Vuex);
 
+var store=new Vuex.Store(storeParams);
 
 export function initializeApacheAiravataDashboard(dashboardName) {
   return new Vue({
     el: '#app',
     router,
+    store,
     template: '<' + dashboardName + '/>',
     components: {ExperimentsDashboard, AdminDashboard}
 
