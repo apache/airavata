78af36d17ae796e938679e1595b32efbb7edb17a
eldhomathulla <eldhomathulla@gmail.com>
2017-09-26 21:09:45 -0400

    added functionality for application input fields

--- admin_view/src/main.js
+++ admin_view/src/main.js
@@ -1,21 +1,18 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
-import Vuex from 'vuex';
 
 import ExperimentsDashboard from './components/dashboards/ExperimentDashboard.vue';
 import AdminDashboard from './components/dashboards/AdminDashboard.vue';
 
 import router from './router';
-import storeParams from './store';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
-Vue.use(Vuex);
 
-var store=new Vuex.Store(storeParams);
 
 export function initializeApacheAiravataDashboard(dashboardName) {
   return new Vue({
--- admin_view/src/store.js
+++ admin_view/src/store.js
@@ -1,30 +1,97 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+
+Vue.use(Vuex);
+
+
 
 const store={
   modules:{
     appInterfaceTab:{
+      namespaced: true,
       state:{
-        inputFields:[],
-        counter:0
+        inputFields:{},
+        counter:0,
+        initialized:false
       },
       mutations:{
-        createAppInterfaceInputField:function (state) {
-          state.inputFields.push(
-            {
-              input_id:state.counter++,
-              name:'',
-              value:'',
-              type:'',
-              appArg:'',
-              dataStaged:false,
-              requiredId:false,
-              requiredOnCmdId:false
+        createAppInterfaceInputFieldObject(state,id){
+          Vue.set(state.inputFields,id,{
+            input_id: id,
+            name: 'nm',
+            value: '',
+            type: '',
+            appArg: '',
+            dataStaged: false,
+            requiredId: false,
+            requiredOnCmdId: false
+          });
+          console.log('Creating App Input Field INS: ',state.inputFields);
+        },
+        updateAppInterfaceInputField:function (state,param) {
+          var id=param.id
+          var updateValue=param.update
+          var inputFields=Object.assign({},state.inputFields)
+          console.log(inputFields)
+          var inpField=inputFields[id]
+          var keys=Object.keys(updateValue)
+          console.log('keys',keys)
+          for(var prop in updateValue){
+            console.log('props',prop)
+            if(inpField.hasOwnProperty(prop)){
+              inpField[prop]=updateValue[prop];
+              console.log('type',typeof inputFields[id][prop])
             }
-          );
+          }
+          state.inputFields=inputFields
+
+        },
+        setInitialize:function (state, intitalized) {
+          state.initialized=intitalized
+        },
+        removeAppInterfaceInputField:function (state, id) {
+          var inputFields=state.inputFields;
+          delete inputFields[id];
+          state.inputFields=Object.assign({},inputFields)
+        },
+        deleteAllInputFields:function (state) {
+          state.inputFields={}
         }
       },
       getters:{
-        getAppInterface:state=>{
-          return state.data;
+        getAppInputField:state=>id=>{
+          return state.inputFields[id];
+        },
+        isInitialized:state=>{
+          return state.initialized;
+        },
+        getAppInputFields:state=>{
+          return state.inputFields;
+        },
+        getAppInputFieldsId:state=>{
+          var ids=Object.getOwnPropertyNames(state.inputFields)
+          ids.splice(ids.indexOf('__ob__'),1)
+          return ids;
+        }
+      },
+      actions:{
+        createAppInterfaceInputField:function (context,id=null) {
+          if(id == null || !context.state.inputFields.hasOwnProperty(id)){
+            id=(context.state.counter++).toString();
+            context.commit('createAppInterfaceInputFieldObject',id);
+          }
+          return id;
+        },
+        deleteAppInterfaceInputField:function (context, id) {
+          console.log('deleting',id)
+          context.commit('removeAppInterfaceInputField',id);
+        },
+        updateFieldValues:function (context, param) {
+          context.commit('updateAppInterfaceInputField',param)
+        },
+        initialized:function (context, initialize) {
+          context.commit('setInitialize',initialize)
         }
       }
     },
@@ -61,7 +128,5 @@ const store={
   }
 };
 
-export default {
-  'store':store
-}
+export default new Vuex.Store(store);
 
