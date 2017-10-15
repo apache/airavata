fa0e3c25a4b93b514d0f9f637de13aaf8bbb2657
eldhomathulla@gmail.com <esF5R9@o>
2017-10-15 12:57:41 -0400

    adding function to map names in the store to names used for api fields

--- admin_view/src/utils.js
+++ admin_view/src/utils.js
@@ -1,8 +1,26 @@
 
 
-export function addIndex(list){
-  for(var i=0;i<list.length;i++){
+export default {
+  addIndex:function (list){
+    for(var i=0;i<list.length;i++){
       list[i].index=i;
+    }
+    return list;
+  },
+  post:function (url,body) {
+    return fetch(url,{
+      method:'post',
+      body:JSON.stringify()
+    }).then((response)=>{
+      if(response.ok()){
+        return Promise.resolve(response.json())
+      }else{
+        return Promise.reject(new Error(response.statusText))
+      }
+    })
+  },
+  mapper:function (data,map) {
+
   }
-  return list;
 }
+
