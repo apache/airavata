975df2d3d99b4f5feef7e6c38d007e5b5ce8ac56
nikchett <nikchett@iu.edu>
2017-10-23 12:54:39 -0400

    Changing mailing structure

--- modules/allocation-notification-manager/Notification-Manager/Notification-Sender/src/main/java/org/apache/airavata/allocation/manager/notification/sender/MailNotification.java
+++ modules/allocation-notification-manager/Notification-Manager/Notification-Sender/src/main/java/org/apache/airavata/allocation/manager/notification/sender/MailNotification.java
@@ -13,52 +13,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class MailNotification {
-	public void sendMail(String requestId, String adminId) {
-		// currently sends to only one sender, but will change to send to multiple users
-		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-		// Get a Properties object
-		Properties login = new Properties();
-		try {
-			FileReader in = new FileReader("login.properties");
-		    login.load(in);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		final String username = login.getProperty("username");
-		final String password = login.getProperty("password");
-		Properties props = System.getProperties();
-		props.setProperty("mail.smtp.host", "smtp.gmail.com");
-		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-		props.setProperty("mail.smtp.socketFactory.fallback", "false");
-		props.setProperty("mail.smtp.port", "465");
-		props.setProperty("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.debug", "true");
-		props.put("mail.store.protocol", "pop3");
-		props.put("mail.transport.protocol", "smtp");
-	//	final String username = "demosga123@gmail.com";//
-	//	final String password = "sgauser123";
-		try {
-			Session session = Session.getDefaultInstance(props, new Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(username, password);
-				}
-			});
-
-			// -- Create a new message --
-			Message msg = new MimeMessage(session);
-
-			// -- Set the FROM and TO fields --
-			msg.setFrom(new InternetAddress("xxxx@gmail.com"));
-			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(adminId, false));
-			msg.setSubject("Request Made");
-			msg.setText("There is request made with id" + requestId);
-			msg.setSentDate(new Date());
-			Transport.send(msg);
-			System.out.println("Message sent.");
-		} catch (MessagingException e) {
-			System.out.println("Erreur d'envoi, cause: " + e);
-		}
+	public void sendMail(String requestId, String userMail) {
+		System.out.println("Notification has been sent to"+userMail);
 
 	}
 }
