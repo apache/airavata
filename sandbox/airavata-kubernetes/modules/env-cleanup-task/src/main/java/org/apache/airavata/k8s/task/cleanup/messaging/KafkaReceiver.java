e96bced36aad77da5573948ced313bec86fe8b91
dimuthu.upeksha2@gmail.com <Dimu@1234>
2017-10-27 08:56:03 +0530

    Adding task microservices for each task type - v2

---
+++ sandbox/airavata-kubernetes/modules/env-cleanup-task/src/main/java/org/apache/airavata/k8s/task/cleanup/messaging/KafkaReceiver.java
@@ -0,0 +1,24 @@
+package org.apache.airavata.k8s.task.cleanup.messaging;
+
+import org.apache.airavata.k8s.task.cleanup.service.TaskExecutionService;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import javax.annotation.Resource;
+
+/**
+ * TODO: Class level comments please
+ *
+ * @author dimuthu
+ * @since 1.0.0-SNAPSHOT
+ */
+public class KafkaReceiver {
+
+    @Resource
+    private TaskExecutionService taskExecutionService;
+
+    @KafkaListener(topics = "${task.read.topic.name}")
+    public void receiveTasks(String payload) {
+        System.out.println("received task=" + payload);
+        taskExecutionService.executeTask(Long.parseLong(payload));
+    }
+}
