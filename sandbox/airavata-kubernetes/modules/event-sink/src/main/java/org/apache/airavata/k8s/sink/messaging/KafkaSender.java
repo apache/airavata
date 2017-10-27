14a62f9b65403cd6e340ef24c83ded9189a0af0a
dimuthu.upeksha2@gmail.com <Dimu@1234>
2017-10-27 19:45:05 +0530

    Implementing the event sink to perisit task states to the database

---
+++ sandbox/airavata-kubernetes/modules/event-sink/src/main/java/org/apache/airavata/k8s/sink/messaging/KafkaSender.java
@@ -0,0 +1,24 @@
+package org.apache.airavata.k8s.sink.messaging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+/**
+ * TODO: Class level comments please
+ *
+ * @author dimuthu
+ * @since 1.0.0-SNAPSHOT
+ */
+public class KafkaSender {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String payload) {
+        kafkaTemplate.send(topic, payload);
+    }
+
+    public void send(String topic, String key, String payload) {
+        kafkaTemplate.send(topic, key, payload);
+    }
+}
---
+++ sandbox/airavata-kubernetes/modules/event-sink/src/main/java/org/apache/airavata/k8s/sink/service/EventPersistingService.java
@@ -0,0 +1,35 @@
+package org.apache.airavata.k8s.sink.service;
+
+import org.apache.airavata.k8s.api.resources.task.TaskStatusResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * TODO: Class level comments please
+ *
+ * @author dimuthu
+ * @since 1.0.0-SNAPSHOT
+ */
+@Service
+public class EventPersistingService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${api.server.url}")
+    private String apiServerUrl;
+
+    public EventPersistingService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public void persistTaskState(long processId, long taskId, int state) {
+        System.out.println("Persisting task state event for process " + processId + ", task " + taskId + ", state " + state);
+        TaskStatusResource statusResource = new TaskStatusResource();
+        statusResource.setTaskId(taskId);
+        statusResource.setTimeOfStateChange(System.currentTimeMillis());
+        statusResource.setState(state);
+        this.restTemplate.postForObject("http://" + this.apiServerUrl + "/task/" + taskId + "/status", statusResource,
+                Long.class);
+    }
+}
