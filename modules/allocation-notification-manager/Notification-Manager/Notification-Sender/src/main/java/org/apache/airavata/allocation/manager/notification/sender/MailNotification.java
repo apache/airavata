b695e3415e7b58a0faa1ac6bf0d9354c445c07e3
nikchett <nikchett@iu.edu>
2017-11-05 01:55:16 -0400

    Added mail

--- modules/allocation-notification-manager/Notification-Manager/Notification-Sender/src/main/java/org/apache/airavata/allocation/manager/notification/sender/MailNotification.java
+++ modules/allocation-notification-manager/Notification-Manager/Notification-Sender/src/main/java/org/apache/airavata/allocation/manager/notification/sender/MailNotification.java
@@ -1,6 +1,7 @@
 package org.apache.airavata.allocation.manager.notification.sender;
 
 import java.util.List;
+import org.apache.commons.mail.*;
 
 public class MailNotification {
 	public void sendMail(String requestId, String status,String senderList ) {
@@ -8,6 +9,17 @@ public class MailNotification {
 		NotificationMessage notificationMessage = new NotificationMessage();
 		 
 		System.out.println(notificationMessage.getNotificationMessageForStatus(status));
+		
+		Email email = new SimpleEmail();
+		email.setHostName("smtp.googlemail.com");
+		email.setSmtpPort(465);
+		email.setAuthenticator(new DefaultAuthenticator("username", "password"));
+		email.setSSLOnConnect(true);
+		email.setFrom("user@gmail.com");
+		email.setSubject("TestMail");
+		email.setMsg("This is a test mail ... :-)");
+		email.addTo("foo@bar.com");
+		email.send();
 
 	}
 }
