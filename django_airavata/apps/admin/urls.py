8013b244f35467887b252f2d521f21697d4df7f4
eldhomathulla <eldhomathulla@gmail.com>
2017-11-28 17:03:36 -0500

    added credential store dashboard components

--- django_airavata/apps/admin/urls.py
+++ django_airavata/apps/admin/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 app_name = 'admin'
 urlpatterns = [
-    url(r'^$', views.experiments, name='admin'),
+    url(r'^$', views.admin_home, name='admin'),
+    url(r'^credential/store$', views.credential_store, name='credential_store'),
 ]
