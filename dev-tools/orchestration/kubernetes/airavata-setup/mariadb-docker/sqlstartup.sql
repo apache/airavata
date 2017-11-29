9eac3dada764dd15fa9a0647e08a711fc34c748b
Kumar Satyam <kr.satyam.ind@gmail.com>
2017-11-28 20:16:17 -0500

    more changes

--- dev-tools/orchestration/kubernetes/airavata-setup/mariadb-docker/sqlstartup.sql
+++ dev-tools/orchestration/kubernetes/airavata-setup/mariadb-docker/sqlstartup.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE IF NOT EXISTS experiment_catalog;
+CREATE DATABASE IF NOT EXISTS app_catalog;
+CREATE DATABASE IF NOT EXISTS replica_catalog;
+CREATE DATABASE IF NOT EXISTS workflow_catalog;
+CREATE DATABASE IF NOT EXISTS sharing_catalog;
+
 grant all on experiment_catalog.* to 'airavata';
+grant all on app_catalog.* to 'airavata';
+grant all on replica_catalog.* to 'airavata';
+grant all on workflow_catalog.* to 'airavata';
+grant all on sharing_catalog.* to 'airavata';
