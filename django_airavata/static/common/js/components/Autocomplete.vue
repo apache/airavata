8ac7ead441d411a9e23ebf0954b10e1b3f18bc25
eldhomathulla <eldhomathulla@gmail.com>
2018-04-05 04:06:40 -0400

    updated autocomplete

--- django_airavata/static/common/js/components/Autocomplete.vue
+++ django_airavata/static/common/js/components/Autocomplete.vue
@@ -1,121 +1,113 @@
 <template>
     <div style="position:relative">
-        <span class="selected-cards" style="position:relative" v-for="item in selected" v-bind:key="item.id">
-          <b-button disabled variant="warning">
-             {{ item.name }}&nbsp;&nbsp;<b-badge variant="light"><a href="#" @click="removeClick(item)">x</a></b-badge>
-          </b-button>&nbsp;&nbsp;
+        <span class="selected-cards" style="position:relative">
+          <b-button variant="warning" v-for="item in selected" v-bind:key="item.id" @click="removeClick(item)">
+             {{ item.name }} <b-badge variant="light"><a href="#">x</a></b-badge>
+          </b-button>
         </span>
         <hr>
-        <input class="form-control" type="text" :value="value" placeholder="Type to get suggestions..."
-               @input="updateValue($event.target.value)"
-               @keydown.enter='enter'
-               @keydown.down='down'
-               @keydown.up='up'
+        <input class="form-control" type="text" :value="searchValue" placeholder="Type to get suggestions..." @input="updateSearchValue($event.target.value)"
+          @keydown.enter = 'enter'
+          @keydown.down = 'down'
+          @keydown.up = 'up'
         >
         <b-list-group style="width: 100%;" v-if="open">
-            <b-list-group-item v-for="(suggestion, index) in filtered.slice(0,5)"
-                               v-bind:class="{'active': isActive(index)}" href="#" @click="suggestionClick(index)"
-                               v-bind:key="suggestion.id">
-                {{ suggestion.name }}
+            <b-list-group-item v-for="(suggestion, index) in filtered.slice(0,5)" v-bind:class="{'active': isActive(index)}" href="#" @click="suggestionClick(index)" v-bind:key="suggestion.id">
+              {{ suggestion.name }}
             </b-list-group-item>
         </b-list-group>
     </div>
 </template>
 
 <script>
-    import BootstrapVue from 'bootstrap-vue'
-    import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-    export default {
-        components: {
-            BootstrapVue
-        },
-        props: {
-            value: {
-                type: String,
-                required: true
-            },
+export default {
 
-            suggestions: {
-                type: Array,
-                required: true
-            }
-        },
+  props: {
+    value: {
+      type: Array,
+      required: false
+    },
 
-        data() {
-            return {
-                open: false,
-                current: 0,
-                selected: [],
-            }
-        },
-
-        computed: {
-            filtered() {
-                return this.suggestions.filter((data) => {
-                    return data.name.indexOf(this.value) >= 0
-                })
-            },
-        },
-        created: function () {
-            if (this.suggestions && this.suggestions[0] instanceof String) {
-                this.suggestions = this.suggestions.map((str, index) => {
-                    return {
-                        name: str,
-                        id: index
-                    }
-                })
-            }
-        },
-        methods: {
-            updateValue(value) {
-                if (this.open === false) {
-                    this.open = true
-                    this.current = 0
-                }
-                if (value === '') {
-                    this.open = false;
-                }
-                this.$emit('input', value)
-            },
-            enter() {
-                // this.$emit('input', this.filtered[this.current].name)
-                this.$emit('input', '');
-                var index = this.suggestions.indexOf(this.filtered[this.current].name);
-                if (this.selected.indexOf(this.filtered[this.current]) == -1) {
-                    this.selected.push(this.filtered[this.current]);
-                }
-                this.$emit('updateSelected', this.selected);
-                this.open = false
-            },
-            up() {
-                if (this.current > 0) {
-                    this.current--
-                }
-            },
-            down() {
-                if (this.current < this.filtered.length - 1) {
-                    this.current++
-                }
-            },
-            isActive(index) {
-                return index === this.current
-            },
-            suggestionClick(index) {
-                // this.$emit('input', this.filtered[index].name)
-                this.$emit('input', '');
-                if (this.selected.indexOf(this.filtered[index]) == -1) {
-                    this.selected.push(this.filtered[index]);
-                }
-                this.$emit('updateSelected', this.selected);
-                this.open = false;
-            },
-            removeClick(data) {
-                var index = this.selected.indexOf(data);
-                this.selected.splice(index, 1);
-                this.$emit('updateSelected', this.selected);
-            }
-        }
+    suggestions: {
+      type: Array,
+      required: true
     }
+  },
+
+  data () {
+    return {
+      open: false,
+      current: 0,
+      localValue: this.value ? this.value.slice() : [],
+      searchValue: '',
+    }
+  },
+
+  computed: {
+    filtered () {
+      return this.suggestions.filter((data) => {
+        return data.name.indexOf(this.searchValue) >= 0
+      })
+    },
+    selected () {
+        return this.suggestions.filter((suggestion) => {
+            return this.localValue.indexOf(suggestion.id) >= 0;
+        });
+    }
+  },
+  methods: {
+    updateSearchValue (value) {
+      if (this.open === false) {
+        this.open = true
+        this.current = 0
+      }
+      if(value===''){
+        this.open = false;
+      }
+      this.searchValue = value;
+    },
+    enter () {
+      var index = this.suggestions.indexOf(this.filtered[this.current].name);
+      if(this.localValue.indexOf(this.filtered[this.current].id)==-1){
+        this.localValue.push(this.filtered[this.current].id);
+      }
+      this.$emit('input',this.localValue);
+      this.searchValue = '';
+      this.open = false
+    },
+    up () {
+      if (this.current > 0) {
+        this.current--
+      }
+    },
+    down () {
+      if (this.current < this.filtered.length - 1) {
+        this.current++
+      }
+    },
+    isActive (index) {
+      return index === this.current
+    },
+    suggestionClick (index) {
+      if(this.localValue.indexOf(this.filtered[index].id)==-1) {
+        this.localValue.push(this.filtered[index].id);
+      }
+      this.$emit('input',this.localValue);
+      this.searchValue = '';
+      this.open = false;
+    },
+    removeClick(data) {
+      var index = this.localValue.indexOf(data.id);
+      this.localValue.splice(index,1);
+      this.$emit('input',this.localValue);
+    }
+  }
+}
 
 </script>
+<style>
+.selected-cards > button + button {
+    margin-left: 10px;
+}
+</style>
