ac9ecc2fab9bba084c97f17f5d02ef487d92226f
stephenpaul2727 <stephenpaul2727@gmail.com>
2018-04-13 12:59:59 -0400

    Added comments to the wagtail hook for draftail editor feature

--- django_airavata/wagtailapps/base/wagtail_hooks.py
+++ django_airavata/wagtailapps/base/wagtail_hooks.py
@@ -5,7 +5,7 @@ from wagtail.core import hooks
 @hooks.register('register_rich_text_features')
 def register_custom_style_feature(features):
 
-    feature_name = 'purple' # .mycustomstyle will have to be defined in the CSS in order to get frontend styles working
+    feature_name = 'purple'
     type_ = feature_name.upper()
     tag = 'span'
     detection = f'{tag}[class="{feature_name}"]'
@@ -13,8 +13,9 @@ def register_custom_style_feature(features):
     control = {
         'type': type_,
         'description': 'Purple Color',
+        'icon': ['M100 100 H 900 V 900 H 100 Z'],   # This should be an svg which will occupy a 1024x1024 viewbox
         'label': 'purple',
-        'style': {'color': 'purple'}, # let's say .mycustomstyle changes text color to purple
+        'style': {'color': 'purple'}, # .purple is the class which is defined in draft-colors.css . It is necessary to get this style working.
     }
 
     features.register_editor_plugin(
@@ -24,8 +25,6 @@ def register_custom_style_feature(features):
     db_conversion = {
         'from_database_format': {detection: InlineStyleElementHandler(type_)},
         'to_database_format': {'style_map': {type_: {'element': tag, 'props': {'class': feature_name}}}},
-        # The following line works too but I suppose it's not the proper way to do it
-        #'to_database_format': {'style_map': {type_: f'{tag} class="{feature_name}"'}},
     }
 
     features.register_converter_rule('contentstate', feature_name, db_conversion)
