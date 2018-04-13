2dc14bf10949212b1d92f16e5c0281082d737ecb
dimuthu <dimuthu.upeksha2@gmail.com>
2018-04-13 13:49:47 -0400

    Adding sshj adaptor and replacing jsch adaptor for SSH communication

--- modules/airavata-helix/agent-impl/ssh-agent/src/main/java/org/apache/airavata/helix/agent/ssh/SshAgentAdaptor.java
+++ modules/airavata-helix/agent-impl/ssh-agent/src/main/java/org/apache/airavata/helix/agent/ssh/SshAgentAdaptor.java
@@ -21,25 +21,14 @@ package org.apache.airavata.helix.agent.ssh;
 
 import com.jcraft.jsch.*;
 import org.apache.airavata.agents.api.*;
-import org.apache.airavata.common.exception.ApplicationSettingsException;
-import org.apache.airavata.common.utils.DBUtil;
-import org.apache.airavata.common.utils.ServerSettings;
-import org.apache.airavata.credential.store.client.CredentialStoreClientFactory;
-import org.apache.airavata.credential.store.cpi.CredentialStoreService;
 import org.apache.airavata.model.credential.store.SSHCredential;
-import org.apache.airavata.credential.store.exception.CredentialStoreException;
-import org.apache.airavata.credential.store.store.impl.CredentialReaderImpl;
 import org.apache.airavata.model.appcatalog.computeresource.*;
-import org.apache.airavata.registry.api.RegistryService;
-import org.apache.airavata.registry.api.client.RegistryServiceClientFactory;
-import org.apache.airavata.registry.api.exception.RegistryServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -92,11 +81,11 @@ public class SshAgentAdaptor implements AgentAdaptor {
     @Override
     public void init(String computeResourceId, String gatewayId, String userId, String token) throws AgentException {
         try {
-            ComputeResourceDescription computeResourceDescription = getRegistryServiceClient().getComputeResource(computeResourceId);
+            ComputeResourceDescription computeResourceDescription = AgentUtils.getRegistryServiceClient().getComputeResource(computeResourceId);
 
             logger.info("Fetching credentials for cred store token " + token);
 
-            SSHCredential sshCredential = getCredentialClient().getSSHCredential(token, gatewayId);
+            SSHCredential sshCredential = AgentUtils.getCredentialClient().getSSHCredential(token, gatewayId);
             if (sshCredential == null) {
                 throw new AgentException("Null credential for token " + token);
             }
@@ -114,27 +103,6 @@ public class SshAgentAdaptor implements AgentAdaptor {
         } catch (Exception e) {
             logger.error("Error while initializing ssh agent for compute resource " + computeResourceId + " to token " + token, e);
             throw new AgentException("Error while initializing ssh agent for compute resource " + computeResourceId + " to token " + token, e);
-        }
-    }
-
-    // TODO this is inefficient. Try to use a connection pool
-    public static RegistryService.Client getRegistryServiceClient() throws AgentException {
-        try {
-            final int serverPort = Integer.parseInt(ServerSettings.getRegistryServerPort());
-            final String serverHost = ServerSettings.getRegistryServerHost();
-            return RegistryServiceClientFactory.createRegistryClient(serverHost, serverPort);
-        } catch (RegistryServiceException | ApplicationSettingsException e) {
-            throw new AgentException("Unable to create registry client...", e);
-        }
-    }
-
-    public static CredentialStoreService.Client getCredentialClient() throws AgentException {
-        try {
-            final int serverPort = Integer.parseInt(ServerSettings.getCredentialStoreServerPort());
-            final String serverHost =ServerSettings.getCredentialStoreServerHost();
-            return CredentialStoreClientFactory.createAiravataCSClient(serverHost, serverPort);
-        } catch (CredentialStoreException | ApplicationSettingsException e) {
-            throw new AgentException("Unable to create credential client...", e);
         }
     }
 
--- modules/airavata-helix/agent-impl/sshj-agent/src/main/java/org/apache/airavata/helix/adaptor/SSHJStorageAdaptor.java
+++ modules/airavata-helix/agent-impl/sshj-agent/src/main/java/org/apache/airavata/helix/adaptor/SSHJStorageAdaptor.java
@@ -17,26 +17,23 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.airavata.helix.agent.storage;
+package org.apache.airavata.helix.adaptor;
 
 import org.apache.airavata.agents.api.AgentException;
 import org.apache.airavata.agents.api.AgentUtils;
 import org.apache.airavata.agents.api.CommandOutput;
 import org.apache.airavata.agents.api.StorageResourceAdaptor;
-import org.apache.airavata.helix.agent.ssh.SshAdaptorParams;
-import org.apache.airavata.helix.agent.ssh.SshAgentAdaptor;
 import org.apache.airavata.model.appcatalog.storageresource.StorageResourceDescription;
 import org.apache.airavata.model.credential.store.SSHCredential;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class StorageResourceAdaptorImpl extends SshAgentAdaptor implements StorageResourceAdaptor  {
+public class SSHJStorageAdaptor extends SSHJAgentAdaptor implements StorageResourceAdaptor {
 
-    private final static Logger logger = LoggerFactory.getLogger(StorageResourceAdaptorImpl.class);
+    private final static Logger logger = LoggerFactory.getLogger(SSHJAgentAdaptor.class);
 
     @Override
     public void init(String storageResourceId, String gatewayId, String loginUser, String token) throws AgentException {
-
         try {
             logger.info("Initializing Storage Resource Adaptor for storage resource : "+ storageResourceId + ", gateway : " +
                     gatewayId +", user " + loginUser + ", token : " + token);
@@ -50,14 +47,8 @@ public class StorageResourceAdaptorImpl extends SshAgentAdaptor implements Stora
             }
             logger.info("Description for token : " + token + " : " + sshCredential.getDescription());
 
-            SshAdaptorParams adaptorParams = new SshAdaptorParams();
-            adaptorParams.setHostName(storageResource.getHostName());
-            adaptorParams.setUserName(loginUser);
-            adaptorParams.setPassphrase(sshCredential.getPassphrase());
-            adaptorParams.setPrivateKey(sshCredential.getPrivateKey().getBytes());
-            adaptorParams.setPublicKey(sshCredential.getPublicKey().getBytes());
-            adaptorParams.setStrictHostKeyChecking(false);
-            init(adaptorParams);
+            createPoolingSSHJClient(loginUser, storageResource.getHostName(), sshCredential.getPublicKey(),
+                    sshCredential.getPrivateKey(), sshCredential.getPassphrase());
 
         } catch (Exception e) {
             logger.error("Error while initializing ssh agent for storage resource " + storageResourceId + " to token " + token, e);
