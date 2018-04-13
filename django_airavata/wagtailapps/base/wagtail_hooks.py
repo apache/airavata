6361083155edb3663d0ca64c372ef37c71bf87ab
stephenpaul2727 <stephenpaul2727@gmail.com>
2018-04-13 12:27:58 -0400

    Modified the Fixture Load file for OSU Added a custom purple color to the draftail editor

--- django_airavata/wagtailapps/base/wagtail_hooks.py
+++ django_airavata/wagtailapps/base/wagtail_hooks.py
@@ -2,36 +2,32 @@ import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
 from wagtail.core import hooks
 
-# 1. Use the register_rich_text_features hook.
 @hooks.register('register_rich_text_features')
-def register_strikethrough_feature(features):
-    """
-    Registering the `strikethrough` feature, which uses the `STRIKETHROUGH` Draft.js inline style type,
-    and is stored as HTML with an `<s>` tag.
-    """
-    feature_name = 'strikethrough'
-    type_ = 'STRIKETHROUGH'
-    tag = 's'
+def register_custom_style_feature(features):
 
-    # 2. Configure how Draftail handles the feature in its toolbar.
+    feature_name = 'purple' # .mycustomstyle will have to be defined in the CSS in order to get frontend styles working
+    type_ = feature_name.upper()
+    tag = 'span'
+    detection = f'{tag}[class="{feature_name}"]'
+
     control = {
         'type': type_,
-        'label': 'S',
-        'description': 'Strikethrough',
-        # This isn’t even required – Draftail has predefined styles for STRIKETHROUGH.
-        # 'style': {'textDecoration': 'line-through'},
+        'description': 'Purple Color',
+        'label': 'purple',
+        'style': {'color': 'purple'}, # let's say .mycustomstyle changes text color to purple
     }
 
-    # 3. Call register_editor_plugin to register the configuration for Draftail.
     features.register_editor_plugin(
         'draftail', feature_name, draftail_features.InlineStyleFeature(control)
     )
 
-    # 4.configure the content transform from the DB to the editor and back.
     db_conversion = {
-        'from_database_format': {tag: InlineStyleElementHandler(type_)},
-        'to_database_format': {'style_map': {type_: tag}},
+        'from_database_format': {detection: InlineStyleElementHandler(type_)},
+        'to_database_format': {'style_map': {type_: {'element': tag, 'props': {'class': feature_name}}}},
+        # The following line works too but I suppose it's not the proper way to do it
+        #'to_database_format': {'style_map': {type_: f'{tag} class="{feature_name}"'}},
     }
 
-    # 5. Call register_converter_rule to register the content transformation conversion.
     features.register_converter_rule('contentstate', feature_name, db_conversion)
+
+    features.default_features.append(feature_name)
