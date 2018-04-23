0177a607777b3eb78930362fd6a123953aa658cf
Marcus Christie <machrist@iu.edu>
2018-04-23 13:28:06 -0400

    AIRAVATA-2611 InputEditorMixin refactor

--- django_airavata/apps/workspace/static/django_airavata_workspace/js/components/experiment/input-editors/FileInputEditor.vue
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/components/experiment/input-editors/FileInputEditor.vue
@@ -9,65 +9,9 @@
 </template>
 
 <script>
-import {models} from 'django-airavata-api'
+import InputEditorMixin from './InputEditorMixin'
 export default {
     name: 'file-input-editor',
-    props: {
-        value: {
-            required: false,
-        },
-        experiment: {
-            type: models.Experiment,
-            required: true,
-        },
-        experimentInput: {
-            type: models.InputDataObjectType,
-            required: true,
-        },
-    },
-    data () {
-        return {
-            data: this.value,
-            inputHasBegun: false,
-        }
-    },
-    computed: {
-        validationResults: function() {
-            return this.experimentInput.validate(this.experiment, this.data);
-        },
-        valid: function() {
-            return Object.keys(this.validationResults).length === 0;
-        },
-        validationFeedback: function() {
-            // Only display validation feedback after the user has provided
-            // input so that missing required value errors are only displayed
-            // after interacting with the input editor
-            return this.inputHasBegun && 'value' in this.validationResults
-                ? this.validationResults['value']
-                : null;
-        },
-        validationState: function() {
-            return this.inputHasBegun && 'value' in this.validationResults
-                ? 'invalid'
-                : null;
-        },
-    },
-    methods: {
-        valueChanged: function() {
-            this.inputHasBegun = true;
-            this.$emit('input', this.data);
-            this.checkValidation();
-        },
-        checkValidation: function() {
-            if (this.valid) {
-                this.$emit('valid');
-            } else {
-                this.$emit('invalid');
-            }
-        }
-    },
-    mounted: function() {
-        this.checkValidation();
-    }
+    mixins: [InputEditorMixin],
 }
 </script>
