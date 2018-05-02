7a182e16b37c71a220682a21fae8f4bcf9bddef4
dimuthu <dimuthu.upeksha2@gmail.com>
2018-05-02 13:29:29 -0400

    Forcefully making the job state as Cancelled once an Experiment cancellation is triggered

--- modules/airavata-helix/helix-spectator/src/main/java/org/apache/airavata/helix/impl/task/AiravataTask.java
+++ modules/airavata-helix/helix-spectator/src/main/java/org/apache/airavata/helix/impl/task/AiravataTask.java
@@ -24,6 +24,7 @@ import org.apache.airavata.common.exception.ApplicationSettingsException;
 import org.apache.airavata.common.utils.AiravataUtils;
 import org.apache.airavata.common.utils.ServerSettings;
 import org.apache.airavata.helix.core.AbstractTask;
+import org.apache.airavata.helix.core.util.MonitoringUtil;
 import org.apache.airavata.helix.task.api.TaskHelper;
 import org.apache.airavata.helix.task.api.annotation.TaskParam;
 import org.apache.airavata.messaging.core.MessageContext;
@@ -178,6 +179,38 @@ public abstract class AiravataTask extends AbstractTask {
             getStatusPublisher().publish(msgCtx);
         } catch (Exception e) {
             logger.error("Failed to publist task status of task " + getTaskId());
+        }
+    }
+
+
+    public void saveAndPublishJobStatus(String jobId, String taskId, String processId, String experimentId, String gateway,
+                                         JobState jobState) throws Exception {
+        try {
+
+            JobStatus jobStatus = new JobStatus();
+            jobStatus.setReason(jobState.name());
+            jobStatus.setTimeOfStateChange(AiravataUtils.getCurrentTimestamp().getTime());
+            jobStatus.setJobState(jobState);
+
+            if (jobStatus.getTimeOfStateChange() == 0 || jobStatus.getTimeOfStateChange() > 0 ) {
+                jobStatus.setTimeOfStateChange(AiravataUtils.getCurrentTimestamp().getTime());
+            } else {
+                jobStatus.setTimeOfStateChange(jobStatus.getTimeOfStateChange());
+            }
+
+            getRegistryServiceClient().addJobStatus(jobStatus, taskId, jobId);
+
+            JobIdentifier identifier = new JobIdentifier(jobId, taskId, processId, experimentId, gateway);
+
+            JobStatusChangeEvent jobStatusChangeEvent = new JobStatusChangeEvent(jobStatus.getJobState(), identifier);
+            MessageContext msgCtx = new MessageContext(jobStatusChangeEvent, MessageType.JOB, AiravataUtils.getId
+                    (MessageType.JOB.name()), gateway);
+            msgCtx.setUpdatedTime(AiravataUtils.getCurrentTimestamp());
+            getStatusPublisher().publish(msgCtx);
+
+            MonitoringUtil.updateStatusOfJob(getCuratorClient(), jobId, jobState);
+        } catch (Exception e) {
+            logger.error("Error persisting job status " + e.getLocalizedMessage(), e);
         }
     }
 
