d2cd58148b8e864fda2fb799927428e8c3a100e4
tilaks26 <sneha.tilak26@gmail.com>
2018-05-02 20:16:46 -0400

    Adding new task repositories

--- modules/registry-refactoring/src/test/java/org/apache/airavata/registry/core/repositories/expcatalog/TaskErrorRepositoryTest.java
+++ modules/registry-refactoring/src/test/java/org/apache/airavata/registry/core/repositories/expcatalog/TaskErrorRepositoryTest.java
@@ -1,25 +1,6 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
 package org.apache.airavata.registry.core.repositories.expcatalog;
 
+import org.apache.airavata.model.commons.ErrorModel;
 import org.apache.airavata.model.experiment.ExperimentModel;
 import org.apache.airavata.model.experiment.ExperimentType;
 import org.apache.airavata.model.process.ProcessModel;
@@ -28,7 +9,6 @@ import org.apache.airavata.model.task.TaskTypes;
 import org.apache.airavata.model.workspace.Gateway;
 import org.apache.airavata.model.workspace.Project;
 import org.apache.airavata.registry.core.repositories.expcatalog.util.Initialize;
-import org.apache.airavata.registry.core.utils.DBConstants;
 import org.apache.airavata.registry.cpi.RegistryException;
 import org.junit.After;
 import org.junit.Before;
@@ -37,11 +17,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
-public class TaskRepositoryTest {
+public class TaskErrorRepositoryTest {
 
     private static Initialize initialize;
     GatewayRepository gatewayRepository;
@@ -49,7 +29,8 @@ public class TaskRepositoryTest {
     ExperimentRepository experimentRepository;
     ProcessRepository processRepository;
     TaskRepository taskRepository;
-    private static final Logger logger = LoggerFactory.getLogger(TaskRepositoryTest.class);
+    TaskErrorRepository taskErrorRepository;
+    private static final Logger logger = LoggerFactory.getLogger(TaskErrorRepositoryTest.class);
 
     @Before
     public void setUp() {
@@ -61,6 +42,7 @@ public class TaskRepositoryTest {
             experimentRepository = new ExperimentRepository();
             processRepository = new ProcessRepository();
             taskRepository = new TaskRepository();
+            taskErrorRepository = new TaskErrorRepository();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
@@ -73,7 +55,7 @@ public class TaskRepositoryTest {
     }
 
     @Test
-    public void TaskRepositoryTest() throws RegistryException {
+    public void TaskErrorRepositoryTest() throws RegistryException {
         Gateway gateway = new Gateway();
         gateway.setGatewayId("gateway");
         gateway.setDomain("SEAGRID");
@@ -105,21 +87,24 @@ public class TaskRepositoryTest {
 
         String taskId = taskRepository.addTask(taskModel, processId);
         assertTrue(taskId != null);
-        assertTrue(processRepository.getProcess(processId).getTasks().size() == 1);
 
-        taskModel.setTaskType(TaskTypes.MONITORING);
-        taskRepository.updateTask(taskModel, taskId);
-        assertEquals(TaskTypes.MONITORING, taskRepository.getTask(taskId).getTaskType());
+        ErrorModel errorModel = new ErrorModel();
+        errorModel.setErrorId("error");
 
-        List<String> taskIdList = taskRepository.getTaskIds(DBConstants.Task.PARENT_PROCESS_ID, processId);
-        assertTrue(taskIdList.size() == 1);
-        assertTrue(taskIdList.get(0).equals(taskId));
+        String taskErrorId = taskErrorRepository.addTaskError(errorModel, taskId);
+        assertTrue(taskErrorId != null);
+        assertTrue(taskRepository.getTask(taskId).getTaskErrors().size() == 1);
+
+        errorModel.setActualErrorMessage("message");
+        taskErrorRepository.updateTaskError(errorModel, taskId);
+
+        List<ErrorModel> retrievedErrorList = taskErrorRepository.getTaskError(taskId);
+        assertTrue(retrievedErrorList.size() == 1);
+        assertEquals("message", retrievedErrorList.get(0).getActualErrorMessage());
 
         experimentRepository.removeExperiment(experimentId);
         processRepository.removeProcess(processId);
         taskRepository.removeTask(taskId);
-        assertFalse(taskRepository.isTaskExist(taskId));
-
         gatewayRepository.removeGateway(gatewayId);
         projectRepository.removeProject(projectId);
     }
