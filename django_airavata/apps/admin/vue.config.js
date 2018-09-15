b3cfd192aa1ac4b8863433fb0a2909bddd6e409b
Marcus Christie <machrist@iu.edu>
2018-09-15 19:16:11 -0400

    eslint config and BundleTracker

--- django_airavata/apps/admin/vue.config.js
+++ django_airavata/apps/admin/vue.config.js
@@ -1,8 +1,18 @@
+const BundleTracker = require('webpack-bundle-tracker')
+
 module.exports = {
     outputDir: './static/django_airavata_admin/dist',
     assetsDir: 'static',
     filenameHashing: false,
     css: {
         extract: true
+    },
+    configureWebpack: {
+      plugins: [
+        new BundleTracker({
+          filename: 'webpack-stats.json',
+          path: './static/django_airavata_admin/dist/'
+        }),
+      ]
     }
 }
