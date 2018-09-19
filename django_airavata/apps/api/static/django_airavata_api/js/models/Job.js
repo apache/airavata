d6daad60bfd85e0b3007a1d338a4c064915f437f
Marcus Christie <machrist@iu.edu>
2018-09-19 14:01:21 -0400

    Bug fix: most recent job status is last one

--- django_airavata/apps/api/static/django_airavata_api/js/models/Job.js
+++ django_airavata/apps/api/static/django_airavata_api/js/models/Job.js
@@ -1,47 +1,51 @@
-import BaseModel from './BaseModel';
-import JobStatus from './JobStatus';
+import BaseModel from "./BaseModel";
+import JobStatus from "./JobStatus";
 
 const FIELDS = [
-     'jobId',
-     'taskId',
-     'processId',
-     'jobDescription',
-     {
-         name: 'creationTime',
-         type: 'date',
-     },
-     {
-         name: 'jobStatuses',
-         type: JobStatus,
-         list: true,
-     },
-     'computeResourceConsumed',
-     'jobName',
-     'workingDir',
-     'stdOut',
-     'stdErr',
-     'exitCode',
+  "jobId",
+  "taskId",
+  "processId",
+  "jobDescription",
+  {
+    name: "creationTime",
+    type: "date"
+  },
+  {
+    name: "jobStatuses",
+    type: JobStatus,
+    list: true
+  },
+  "computeResourceConsumed",
+  "jobName",
+  "workingDir",
+  "stdOut",
+  "stdErr",
+  "exitCode"
 ];
 
 export default class Job extends BaseModel {
-    constructor(data = {}) {
-        super(FIELDS, data);
-    }
+  constructor(data = {}) {
+    super(FIELDS, data);
+  }
 
-    // get the first job status
-    get jobStatus() {
-        return (this.jobStatuses && this.jobStatuses.length > 0) ? this.jobStatuses[0] : null;
+  // get the first job status
+  get latestJobStatus() {
+    if (this.jobStatuses && this.jobStatuses.length > 0) {
+      return this.jobStatuses[this.jobStatuses.length - 1];
+    } else {
+      return null;
     }
+  }
 
-    get jobStatusStateName() {
-        return this.jobStatus ? this.jobStatus.jobState.name : null;
-    }
+  get jobStatusStateName() {
+    return this.latestJobStatus ? this.latestJobStatus.jobState.name : null;
+  }
 
-    get jobStatusTimeOfStateChange() {
-        return this.jobStatus ? this.jobStatus.timeOfStateChange : null;
-    }
+  get jobStatusTimeOfStateChange() {
+    return this.latestJobStatus ? this.latestJobStatus.timeOfStateChange : null;
+  }
 
-    get jobStatusReason() {
-        return this.jobStatus ? this.jobStatus.reason : null;
-    }
+  get jobStatusReason() {
+    return this.latestJobStatus ? this.latestJobStatus.reason : null;
+  }
 }
