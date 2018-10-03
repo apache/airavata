cddacba9e9c220a264eb1b8a7774dc7796e1e446
Marcus Christie <machrist@iu.edu>
2018-10-03 11:23:13 -0400

    Order matters here
    But really we shouldn't create the default project until user replication finishes

--- django_airavata/apps/workspace/signals.py
+++ django_airavata/apps/workspace/signals.py
@@ -16,6 +16,36 @@ log = logging.getLogger(__name__)
 
 
 @receiver(user_logged_in)
+def create_user_profile_for_new_user(sender, request, user, **kwargs):
+    """Create basic User Profile for new user."""
+    # auth middleware hasn't run yet so authz_token attribute is not available
+    # on request, so need to create the authz_token manually
+    authz_token = get_authz_token(request)
+    with get_user_profile_client() as user_profile_client:
+        user_profile_exists = user_profile_client.doesUserExist(
+            authz_token, user.username, settings.GATEWAY_ID)
+        if not user_profile_exists:
+            log.debug("UserProfile doesn't exist for {username}, "
+                      "creating...".format(username=user.username))
+            new_user_profile = UserProfile()
+            new_user_profile.airavataInternalUserId = (user.username + "@" +
+                                                       settings.GATEWAY_ID)
+            new_user_profile.userId = user.username
+            new_user_profile.gatewayId = settings.GATEWAY_ID
+            new_user_profile.emails = [user.email]
+            new_user_profile.firstName = user.first_name
+            new_user_profile.lastName = user.last_name
+            unix_utcnow_ms = int(datetime.datetime.utcnow().timestamp() * 1000)
+            new_user_profile.creationTime = unix_utcnow_ms
+            new_user_profile.lastAccessTime = unix_utcnow_ms
+            new_user_profile.validUntil = -1
+            new_user_profile.State = Status.ACTIVE
+            user_profile_client.addUserProfile(authz_token, new_user_profile)
+            log.info("Created a new UserProfile for {username}".format(
+                username=user.username))
+
+
+@receiver(user_logged_in)
 def create_default_project_if_not_exists(sender, request, user, **kwargs):
     """Create 'Default Project' for new user."""
     # auth middleware hasn't run yet so authz_token attribute is not available
@@ -37,33 +67,3 @@ def create_default_project_if_not_exists(sender, request, user, **kwargs):
                                                owner=default_project.owner))
             airavata_client.createProject(authz_token, settings.GATEWAY_ID,
                                           default_project)
-
-
-@receiver(user_logged_in)
-def create_user_profile_for_new_user(sender, request, user, **kwargs):
-    """Create basic User Profile for new user."""
-    # auth middleware hasn't run yet so authz_token attribute is not available
-    # on request, so need to create the authz_token manually
-    authz_token = get_authz_token(request)
-    with get_user_profile_client() as user_profile_client:
-        user_profile_exists = user_profile_client.doesUserExist(
-            authz_token, user.username, settings.GATEWAY_ID)
-        if not user_profile_exists:
-            log.debug("UserProfile doesn't exist for {username}, "
-                      "creating...".format(username=user.username))
-            new_user_profile = UserProfile()
-            new_user_profile.airavataInternalUserId = (user.username + "@"
-                                                       + settings.GATEWAY_ID)
-            new_user_profile.userId = user.username
-            new_user_profile.gatewayId = settings.GATEWAY_ID
-            new_user_profile.emails = [user.email]
-            new_user_profile.firstName = user.first_name
-            new_user_profile.lastName = user.last_name
-            unix_utcnow_ms = int(datetime.datetime.utcnow().timestamp() * 1000)
-            new_user_profile.creationTime = unix_utcnow_ms
-            new_user_profile.lastAccessTime = unix_utcnow_ms
-            new_user_profile.validUntil = -1
-            new_user_profile.State = Status.ACTIVE
-            user_profile_client.addUserProfile(authz_token, new_user_profile)
-            log.info("Created a new UserProfile for {username}".format(
-                username=user.username))
