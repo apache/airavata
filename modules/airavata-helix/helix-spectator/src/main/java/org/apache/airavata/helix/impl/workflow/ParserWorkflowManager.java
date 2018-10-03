ee072f0ee0f2d14af8ce3078e830cc3c5bfc3ed8
lahiru-j <lahirujayathilake@gmail.com>
2018-10-03 21:15:59 +0530

    implementation of data-parsing based on accepting DAG templates from outside. Removed unnecessary classes

--- modules/airavata-helix/helix-spectator/src/main/java/org/apache/airavata/helix/impl/workflow/ParserWorkflowManager.java
+++ modules/airavata-helix/helix-spectator/src/main/java/org/apache/airavata/helix/impl/workflow/ParserWorkflowManager.java
@@ -25,16 +25,21 @@ import org.apache.airavata.helix.core.AbstractTask;
 import org.apache.airavata.helix.core.OutPort;
 import org.apache.airavata.helix.core.util.MonitoringUtil;
 import org.apache.airavata.helix.impl.task.parsing.*;
+import org.apache.airavata.model.appcatalog.appinterface.ApplicationInterfaceDescription;
+import org.apache.airavata.model.process.ProcessModel;
+import org.apache.airavata.monitor.JobStatusResult;
+import org.apache.airavata.monitor.kafka.JobStatusResultDeserializer;
+import org.apache.airavata.registry.api.RegistryService;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 /**
- * Workflow Manager which will create and launch a Data Parsing DAG
+ * Workflow Manager which will create and launch Data Parsing DAGs
  *
  * @since 1.0.0-SNAPSHOT
  */
@@ -42,80 +47,128 @@ public class ParserWorkflowManager extends WorkflowManager {
 
     private final static Logger logger = LoggerFactory.getLogger(ParserWorkflowManager.class);
 
-    private ParserRequest parserRequest;
-
-    public ParserWorkflowManager(ParserRequest parserRequest) throws ApplicationSettingsException {
+    public ParserWorkflowManager() throws ApplicationSettingsException {
         super(ServerSettings.getSetting("parser.workflow.manager.name"));
-        this.parserRequest = parserRequest;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ParserWorkflowManager manager = new ParserWorkflowManager();
+        manager.init();
+        manager.runConsumer();
     }
 
     private void init() throws Exception {
         super.initComponents();
     }
 
-    private boolean process() {
-        if (parserRequest.getInputFileName().isEmpty() || parserRequest.getOutputFileName().isEmpty() ||
-                parserRequest.getWorkingDir().isEmpty()) {
-
-            logger.error("Parser Request has missing field(s). Input file name, output file name, and " +
-                    "working directory parameters must be non-empty");
-            return false;
-        }
-
-        File inputFile = new File(parserRequest.getWorkingDir().endsWith(File.separator)
-                ? parserRequest.getWorkingDir() + parserRequest.getInputFileName()
-                : parserRequest.getWorkingDir() + File.separator + parserRequest.getInputFileName());
-
-        if (!inputFile.exists()) {
-            logger.error("Input file: " + inputFile.getName() + " does not exists");
-            return false;
-        }
-
+    private boolean process(JobStatusResult jobStatusResult) {
         try {
-            final List<AbstractTask> allTasks = new ArrayList<>();
-            CatalogGraph catalogGraph = new CatalogGraph(parserRequest, ServerSettings.getSetting("parser.catalog.path"));
-            List<CatalogEntry> catalogEntries = catalogGraph.getSPCatalogEntries(parserRequest.outputFileType());
-            String processId = "ID-" + UUID.randomUUID().toString();
+            if (MonitoringUtil.hasMonitoringRegistered(getCuratorClient(), jobStatusResult.getJobId())) {
 
-            // Set the input file name into the firs CatalogEntry
-            catalogEntries.get(0).setInputFileName(parserRequest.getInputFileName());
-            // Set the output file name into the last CatalogEntry
-            catalogEntries.get(catalogEntries.size() - 1).setOutputFileName(parserRequest.getOutputFileName());
+                String processId = Optional.ofNullable(MonitoringUtil.getProcessIdByJobId(getCuratorClient(), jobStatusResult.getJobId()))
+                        .orElseThrow(() -> new Exception("Can not find the process for job id " + jobStatusResult.getJobId()));
 
-            for (int i = 0; i < catalogEntries.size(); i++) {
-                CatalogEntry entry = catalogEntries.get(i);
-                if (i > 0) {
-                    // Set the input file name as the previous catalog entry output file name
-                    entry.setInputFileName(catalogEntries.get(i - 1).getOutputFileName());
+                RegistryService.Client registryClient = getRegistryClientPool().getResource();
+                ProcessModel processModel;
+                ApplicationInterfaceDescription appDescription;
+                try {
+                    processModel = registryClient.getProcess(processId);
+                    appDescription = registryClient.getApplicationInterface(processModel.getApplicationInterfaceId());
+
+                } catch (Exception e) {
+                    logger.error("Failed to fetch process or application description from registry associated with process id " + processId, e);
+                    throw new Exception("Failed to fetch process or application description from registry associated with process id " + processId, e);
+
+                } finally {
+                    getRegistryClientPool().returnResource(registryClient);
                 }
 
-                DataParsingTask task = new DataParsingTask();
-                task.setJsonStrCatalogEntry(CatalogUtil.catalogEntryToJSONString(entry));
-                task.setLocalWorkingDir(parserRequest.getWorkingDir());
-                task.setTaskId("ID-" + entry.getDockerImageName().replaceAll("[^a-zA-Z0-9_.-]", "-"));
+                // All the templates should be run
+                // FIXME is it ApplicationInterfaceId or ApplicationName
+                Set<ParsingTemplate> parsingTemplates = ParserCatalog.getParserTemplatesForApplication(appDescription.getApplicationInterfaceId());
 
-                if (allTasks.size() > 0) {
-                    allTasks.get(allTasks.size() - 1).setNextTask(new OutPort(task.getTaskId(), task));
-                }
-                allTasks.add(task);
-                logger.info("Successfully added the data parsing task: " + task.getTaskId() + " to the task DAG");
-            }
+                for (ParsingTemplate template : parsingTemplates) {
+                    List<AbstractTask> allTasks = new ArrayList<>();
+                    ParserInfo parentParser = null;
 
-            String workflowName = getWorkflowOperator()
-                    .launchWorkflow(processId + "-DataParsing-" + UUID.randomUUID().toString(),
+                    for (ParserDAGElement dagElement : template.getParserDag()) {
+                        ParserInfo childParser = ParserCatalog.getParserById(dagElement.getChildParser());
+
+                        DataParsingTask task = new DataParsingTask();
+                        task.setTaskId("Data-Parsing-Task-" + childParser.getId().replaceAll("[^a-zA-Z0-9_.-]", "-"));
+                        task.setJsonStrParserInfo(ParserUtil.getStrFromObj(childParser));
+                        task.setJsonStrParserDagElement(ParserUtil.getStrFromObj(dagElement));
+                        task.setParsingTemplateId(template.getId());
+                        task.setGatewayId(ServerSettings.getSetting("gateway.id"));
+                        task.setStorageResourceId(ServerSettings.getSetting("storage.resource.id"));
+                        task.setDataMovementProtocol(ServerSettings.getSetting("data.movement.protocol"));
+                        task.setStorageResourceCredToken(ServerSettings.getSetting("storage.cred.token"));
+                        task.setStorageResourceLoginUName(ServerSettings.getSetting("resource.login.name"));
+                        task.setStorageInputFilePath(parentParser != null
+                                ? ParserUtil.getStorageUploadPath(template.getId(), parentParser.getId())
+                                : template.getInitialInputsPath());
+
+                        if (allTasks.size() > 0) {
+                            allTasks.get(allTasks.size() - 1).setNextTask(new OutPort(task.getTaskId(), task));
+                        }
+                        allTasks.add(task);
+                        logger.info("Successfully added the data parsing task: " + task.getTaskId() +
+                                " to the task DAG of the parsing template: " + template.getId());
+
+                        parentParser = childParser;
+                    }
+
+                    String workflowName = getWorkflowOperator().launchWorkflow(processId + "-DataParsing-" + UUID.randomUUID().toString(),
                             new ArrayList<>(allTasks), true, false);
-            try {
-                MonitoringUtil.registerWorkflow(getCuratorClient(), processId, workflowName);
 
-            } catch (Exception e) {
-                logger.error("Failed to save workflow " + workflowName + " of process " + processId +
-                        " in zookeeper registry. " + "This will affect cancellation tasks", e);
+                    try {
+                        MonitoringUtil.registerWorkflow(getCuratorClient(), processId, workflowName);
+
+                    } catch (Exception e) {
+                        logger.error("Failed to save workflow " + workflowName + " of process " + processId +
+                                " in zookeeper registry. " + "This will affect cancellation tasks", e);
+                    }
+                }
+                return true;
+
+            } else {
+                logger.warn("Could not find a monitoring registered for the job id " + jobStatusResult.getJobId());
+                return false;
             }
-            return true;
 
         } catch (Exception e) {
             logger.error("Failed to create the DataParsing task DAG", e);
             return false;
+        }
+    }
+
+    private void runConsumer() throws ApplicationSettingsException {
+        final Properties props = new Properties();
+        final Consumer<String, JobStatusResult> consumer = new KafkaConsumer<>(props);
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, ServerSettings.getSetting("kafka.broker.url"));
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, ServerSettings.getSetting("kafka.broker.consumer.group"));
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JobStatusResultDeserializer.class.getName());
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        consumer.subscribe(Collections.singletonList(ServerSettings.getSetting("kafka.parser.topic")));
+
+        while (true) {
+            final ConsumerRecords<String, JobStatusResult> consumerRecords = consumer.poll(Long.MAX_VALUE);
+
+            for (TopicPartition partition : consumerRecords.partitions()) {
+                List<ConsumerRecord<String, JobStatusResult>> partitionRecords = consumerRecords.records(partition);
+                for (ConsumerRecord<String, JobStatusResult> record : partitionRecords) {
+                    boolean success = process(record.value());
+                    logger.info("Status of processing job: " + record.value().getJobId() + " : " + success);
+                    if (success) {
+                        consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(record.offset() + 1)));
+                    }
+                }
+            }
+
+            consumerRecords.forEach(record -> process(record.value()));
+            consumer.commitAsync();
         }
     }
 }
