ceb4cdb04fa2a0ed1f090fad0d344df655e9f3c2
Marcus Christie <machristie@apache.org>
2018-11-01 15:23:39 -0400

    AIRAVATA-2932 Fix saving job submission protocol

--- modules/registry-refactoring/src/main/java/org/apache/airavata/registry/core/repositories/appcatalog/JobSubmissionInterfaceRepository.java
+++ modules/registry-refactoring/src/main/java/org/apache/airavata/registry/core/repositories/appcatalog/JobSubmissionInterfaceRepository.java
@@ -19,9 +19,7 @@
  */
 package org.apache.airavata.registry.core.repositories.appcatalog;
 
-import org.apache.airavata.model.appcatalog.computeresource.ComputeResourceDescription;
 import org.apache.airavata.model.appcatalog.computeresource.JobSubmissionInterface;
-import org.apache.airavata.registry.core.entities.appcatalog.ComputeResourceEntity;
 import org.apache.airavata.registry.core.entities.appcatalog.JobSubmissionInterfaceEntity;
 import org.apache.airavata.registry.core.entities.appcatalog.JobSubmissionInterfacePK;
 import org.apache.airavata.registry.core.utils.ObjectMapperSingleton;
@@ -36,9 +34,6 @@ public class JobSubmissionInterfaceRepository extends AppCatAbstractRepository<J
     public String addJobSubmission(String computeResourceId, JobSubmissionInterface jobSubmissionInterface) {
         Mapper mapper = ObjectMapperSingleton.getInstance();
         JobSubmissionInterfaceEntity jobSubmissionInterfaceEntity = mapper.map(jobSubmissionInterface, JobSubmissionInterfaceEntity.class);
-        ComputeResourceDescription computeResourceDescription = new ComputeResourceRepository().get(computeResourceId);
-        ComputeResourceEntity computeResourceEntity = mapper.map(computeResourceDescription, ComputeResourceEntity.class);
-        jobSubmissionInterfaceEntity.setComputeResource(computeResourceEntity);
         jobSubmissionInterfaceEntity.setComputeResourceId(computeResourceId);
         execute(entityManager -> entityManager.merge(jobSubmissionInterfaceEntity));
 
