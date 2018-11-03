61e5992544ae08e148d2ad81beb59e20acbdfec2
Marcus Christie <machrist@iu.edu>
2018-11-03 15:13:48 -0400

    Moving need for runtime template compiler by pulling template out to separate comp

---
+++ django_airavata/apps/admin/static/django_airavata_admin/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="vmain">
+    <notifications-display />
+    <Loading />
+    <transition name="fade">
+      <router-view>
+      </router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import ExperimentsDashboard from "./components/dashboards/ExperimentDashboard.vue";
+import CredentialStore from "./components/dashboards/CredentialStoreDashboard.vue";
+import Loading from "./components/Loading.vue";
+import ComputeResourceDashboard from "./components/dashboards/ComputeResourceDashboard";
+import ComputeResourcePreferenceDashboard from "./components/dashboards/ComputeResourcePreferenceDashboard";
+import { components } from 'django-airavata-common-ui'
+
+export default {
+  components: {
+    ExperimentsDashboard,
+    // AdminDashboard,
+    CredentialStore,
+    Loading,
+    ComputeResourceDashboard,
+    ComputeResourcePreferenceDashboard,
+    "notifications-display": components.NotificationsDisplay
+  }
+};
+</script>
+
--- django_airavata/apps/admin/static/django_airavata_admin/src/main.js
+++ django_airavata/apps/admin/static/django_airavata_admin/src/main.js
@@ -2,11 +2,7 @@ import Vue from "vue";
 import VueResource from "vue-resource";
 import VueRouter from "vue-router";
 
-import ExperimentsDashboard from "./components/dashboards/ExperimentDashboard.vue";
-import CredentialStore from "./components/dashboards/CredentialStoreDashboard.vue";
-import Loading from "./components/Loading.vue";
-import ComputeResourceDashboard from "./components/dashboards/ComputeResourceDashboard";
-import ComputeResourcePreferenceDashboard from "./components/dashboards/ComputeResourcePreferenceDashboard";
+import App from "./App.vue";
 import BootstrapVue from "bootstrap-vue";
 // TODO: load the latest bootstrap css globally
 import "bootstrap/dist/css/bootstrap.css";
@@ -24,7 +20,7 @@ faLibrary.add(faGripVertical, faEquals, faClipboard, faPlus);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
-import { components, errors } from "django-airavata-common-ui";
+import { errors } from "django-airavata-common-ui";
 
 import router from "./router";
 import store from "./store/store";
@@ -37,30 +33,8 @@ Vue.use(BootstrapVue);
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
-
-var template = `
-    <div class="vmain"><notifications-display/><Loading/>
-        <transition name="fade">
-            <router-view>
-            </router-view>
-        </transition>
-    </div>`
 new Vue({
-  el: '#app',
+  render: h => h(App),
   router,
-  store,
-  template: template,
-  components: {
-    ExperimentsDashboard,
-    // AdminDashboard,
-    CredentialStore,
-    Loading,
-    ComputeResourceDashboard,
-    ComputeResourcePreferenceDashboard,
-    'notifications-display': components.NotificationsDisplay,
-  }
-});
-Vue.config.devtools = true
-Vue.config.debug = true
-Vue.config.silent = false
-
+  store
+}).$mount("#app");
