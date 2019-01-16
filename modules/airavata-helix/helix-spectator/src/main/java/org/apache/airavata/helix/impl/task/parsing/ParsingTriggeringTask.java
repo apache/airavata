0cc43ea2c3ff2191ca08b6c5bba7659e792539c1
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2019-01-16 10:17:56 -0500

    Fixing the bug of producer becomming null

--- modules/airavata-helix/helix-spectator/src/main/java/org/apache/airavata/helix/impl/task/parsing/ParsingTriggeringTask.java
+++ modules/airavata-helix/helix-spectator/src/main/java/org/apache/airavata/helix/impl/task/parsing/ParsingTriggeringTask.java
@@ -31,7 +31,7 @@ public class ParsingTriggeringTask extends AiravataTask {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ProcessCompletionMessageSerializer.class.getName());
-        this.producer = producer;
+        this.producer = new KafkaProducer<String, ProcessCompletionMessage>(props);;
     }
 
     public void submitMessageToParserEngine(ProcessCompletionMessage completionMessage) throws ExecutionException, InterruptedException, ApplicationSettingsException {
