f55bb83596c6d87151118ca20c66746f779590b3
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2019-01-23 21:11:03 -0500

    Cleaning up monitoring util

--- modules/airavata-helix/task-core/src/main/java/org/apache/airavata/helix/core/util/MonitoringUtil.java
+++ modules/airavata-helix/task-core/src/main/java/org/apache/airavata/helix/core/util/MonitoringUtil.java
@@ -1,26 +1,16 @@
 package org.apache.airavata.helix.core.util;
 
-import org.apache.airavata.model.status.JobState;
-import org.apache.airavata.model.workspace.Gateway;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 public class MonitoringUtil {
 
     private final static Logger logger = LoggerFactory.getLogger(MonitoringUtil.class);
 
     private static final String PATH_PREFIX = "/airavata";
-    private static final String REGISTRY = "/registry/";
-
     private static final String TASK = "/task";
-
-    private static final String JOBS = "/jobs";
-    private static final String WORKFLOWS = "/workflows";
     private static final String RETRY = "/retry";
 
     public static int getTaskRetryCount(CuratorFramework curatorClient, String taskId) throws Exception {
@@ -50,12 +40,5 @@ public class MonitoringUtil {
 
     public static void deleteTaskSpecificNodes(CuratorFramework curatorClient, String takId) throws Exception {
         deleteIfExists(curatorClient, PATH_PREFIX + TASK + "/" + takId + RETRY);
-    }
-
-    public static void deleteProcessSpecificNodes(CuratorFramework curatorClient, String processId) throws Exception {
-
-        deleteIfExists(curatorClient, PATH_PREFIX + REGISTRY + processId + JOBS);
-        deleteIfExists(curatorClient, PATH_PREFIX + REGISTRY + processId + WORKFLOWS);
-        deleteIfExists(curatorClient, PATH_PREFIX + REGISTRY + processId);
     }
 }
