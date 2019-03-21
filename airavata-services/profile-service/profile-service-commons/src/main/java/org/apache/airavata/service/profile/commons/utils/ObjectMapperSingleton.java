8eb16e203df1d429e5f2c5c6762dfbb8e40760aa
Suresh Marru <smarru@apache.org>
2019-03-20 21:55:34 -0400

    adding missing license headers

--- airavata-services/profile-service/profile-service-commons/src/main/java/org/apache/airavata/service/profile/commons/utils/ObjectMapperSingleton.java
+++ airavata-services/profile-service/profile-service-commons/src/main/java/org/apache/airavata/service/profile/commons/utils/ObjectMapperSingleton.java
@@ -16,8 +16,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
-*/
+ */
 package org.apache.airavata.service.profile.commons.utils;
 
 import org.dozer.DozerBeanMapper;
