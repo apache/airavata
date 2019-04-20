5a3e8a7bd87b6a327531994996cfc486586887b8
bd2019us <bd2019us@126.com>
2019-04-20 11:50:41 -0500

    use System.currentTimeMillis()

--- modules/credential-store/credential-store-service/src/main/java/org/apache/airavata/credential/store/store/impl/db/CredentialsDAO.java
+++ modules/credential-store/credential-store-service/src/main/java/org/apache/airavata/credential/store/store/impl/db/CredentialsDAO.java
@@ -101,9 +101,8 @@ public class CredentialsDAO extends ParentDAO {
             preparedStatement.setBinaryStream(3, isCert);
 
             preparedStatement.setString(4, credential.getPortalUserName());
-
-            java.util.Date date = new java.util.Date();
-            Timestamp timestamp = new Timestamp(date.getTime());
+            
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
             preparedStatement.setTimestamp(5, timestamp);
 
