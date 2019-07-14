a9a7dc4a1c3928b02aee6d51b48b58276d624b73
Marcus Christie <machristie@apache.org>
2019-07-14 16:12:16 -0400

    AIRAVATA-3123 Create storage home dir on login

--- django_airavata/apps/api/apps.py
+++ django_airavata/apps/api/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     name = 'django_airavata.apps.api'
     label = 'django_airavata_api'
+
+    def ready(self):
+        from . import signals # noqa
---
+++ django_airavata/apps/api/signals.py
@@ -0,0 +1,19 @@
+"""Signal receivers for the api app."""
+
+import logging
+
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+
+from . import data_products_helper
+
+log = logging.getLogger(__name__)
+
+
+@receiver(user_logged_in)
+def create_user_storage_dir(sender, request, user, **kwargs):
+    """Create user's home direct in gateway storage."""
+    path = ""
+    if not data_products_helper.dir_exists(request, path):
+        data_products_helper.create_user_dir(request, path)
+        log.info("Created home directory for user {}".format(user.username))
