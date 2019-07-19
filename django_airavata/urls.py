b641e0202e986fd88909e47cb53fbe288f3b09e5
Marcus Christie <machristie@apache.org>
2019-07-19 15:52:13 -0400

    Removing unused import

--- django_airavata/urls.py
+++ django_airavata/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.http import HttpResponse
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
