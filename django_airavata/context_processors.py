6c251d71a8fd26ea68fe2b18aa911e53ed2b9157
Marcus Christie <machristie@apache.org>
2019-08-26 17:16:04 -0400

    Delay enhancing app configs until needed

--- django_airavata/context_processors.py
+++ django_airavata/context_processors.py
@@ -9,7 +9,10 @@ from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse
 
-from django_airavata.app_config import AiravataAppConfig
+from django_airavata.app_config import (
+    AiravataAppConfig,
+    enhance_custom_app_config
+)
 from django_airavata.apps.api.models import User_Notifications
 
 logger = logging.getLogger(__name__)
@@ -98,7 +101,7 @@ def airavata_app_registry(request):
 def custom_app_registry(request):
     """Put custom Django apps into the context."""
     custom_apps = settings.CUSTOM_DJANGO_APPS.copy()
-    custom_apps = [app for app in custom_apps
+    custom_apps = [enhance_custom_app_config(app) for app in custom_apps
                    if (getattr(app, 'enabled', None) is None or
                        app.enabled(request)
                        )]
