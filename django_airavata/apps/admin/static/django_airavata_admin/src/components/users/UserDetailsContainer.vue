4384aaf382f3cb82833fe5485594134db62ebb6e
Marcus Christie <machristie@apache.org>
2019-10-22 17:05:21 -0400

    AIRAVATA-3226 Allow "activating" user who is enabled in Keycloak but for some reason doesn't have an Airavata User Profile

--- django_airavata/apps/admin/static/django_airavata_admin/src/components/users/UserDetailsContainer.vue
+++ django_airavata/apps/admin/static/django_airavata_admin/src/components/users/UserDetailsContainer.vue
@@ -7,6 +7,11 @@
       :airavata-internal-user-id="iamUserProfile.airavataInternalUserId"
       @input="groupsUpdated"
     />
+    <activate-user-panel
+      v-if="iamUserProfile.enabled && iamUserProfile.emailVerified && !iamUserProfile.airavataUserProfileExists"
+      :username="iamUserProfile.userId"
+      @activate-user="$emit('enable-user', $event)"
+    />
     <enable-user-panel
       v-if="!iamUserProfile.enabled && !iamUserProfile.emailVerified"
       :username="iamUserProfile.userId"
@@ -23,6 +28,7 @@
 <script>
 import { models } from "django-airavata-api";
 import UserGroupMembershipEditor from "./UserGroupMembershipEditor";
+import ActivateUserPanel from "./ActivateUserPanel";
 import EnableUserPanel from "./EnableUserPanel";
 import DeleteUserPanel from "./DeleteUserPanel";
 
@@ -41,7 +47,8 @@ export default {
   components: {
     UserGroupMembershipEditor,
     EnableUserPanel,
-    DeleteUserPanel
+    DeleteUserPanel,
+    ActivateUserPanel
   },
   data() {
     return {
