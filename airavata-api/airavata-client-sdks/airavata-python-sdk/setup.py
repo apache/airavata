8bf1331febf74b6cd9d48b0cf9f4bf83c7faf644
Isuru Ranawaka <irjanith@gmail.com>
2019-12-12 11:54:09 -0500

    change setup.py

--- airavata-api/airavata-client-sdks/airavata-python-sdk/setup.py
+++ airavata-api/airavata-client-sdks/airavata-python-sdk/setup.py
@@ -1,9 +1,17 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 setup(
     name='airavata-python-sdk',
     version='1.0.0',
-    packages=find_packages(include=['clients']),
+    packages=find_packages(),
     url='http://airavata.com',
     license='Apache License 2.0',
     author='Airavata Developers',
