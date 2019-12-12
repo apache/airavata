ee2c24786d4a4fe36eb8fc7b285e86067a28b3cd
Marcus Christie <machristie@apache.org>
2019-12-12 15:12:11 -0500

    AIRAVATA-3243 auth: activate signal receivers on startup

--- django_airavata/apps/auth/apps.py
+++ django_airavata/apps/auth/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthConfig(AppConfig):
     name = 'django_airavata.apps.auth'
     label = 'django_airavata_auth'
+
+    def ready(self):
+        from . import signals  # noqa
