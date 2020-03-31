fa4c6834f10a02aab105586471a20a0c21fb72aa
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-04-01 05:19:12 +0530

    AIRAVATA-3310: Adding Codemirror editor to storage file edit view

--- django_airavata/apps/workspace/static/django_airavata_workspace/js/components/storage/UserStorageEditViewer.vue
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/components/storage/UserStorageEditViewer.vue
@@ -2,10 +2,17 @@
   <div>
     User storage edit {{storagePath}}
     <br/>
-    User storage edit {{userStoragePath}}
+
+    <div style="width: 100%;" id="code">
+    </div>
   </div>
 </template>
 <script>
+
+  import CodeMirror from 'codemirror'
+  import 'codemirror/lib/codemirror.css'
+  import 'codemirror/theme/abcdef.css'
+  import './UserStorageEditViewer.css'
 
   export default {
     name: "user-storage-file-edit-viewer",
@@ -17,9 +24,27 @@
         required: true
       }
     },
+    mounted() {
+      const value = this.getFileContent();
+      CodeMirror(document.getElementById("code"), {
+        theme: "abcdef",
+        mode: "text/plain",
+        lineNumbers: true,
+        lineWrapping: true,
+        scrollbarStyle: "native",
+        extraKeys: {"Ctrl-Space": "autocomplete"},
+        value: value
+      });
+    },
     components: {},
     computed: {},
-    methods: {}
+    methods: {
+      getFileContent() {
+        // TODO
+        return "aaa bbb ccc dd ee"
+      }
+    }
   };
+
 </script>
 
