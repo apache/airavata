53b1b5abda6d714c69c0479245ef0793f15a18db
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-04-01 21:29:01 +0530

    AIRAVATA-3310: Enbale spinner on user storage file edit view

--- django_airavata/apps/workspace/static/django_airavata_workspace/js/components/storage/UserStorageEditViewer.vue
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/components/storage/UserStorageEditViewer.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    User storage edit {{storagePath}}
-    <br/>
-
-    <div style="width: 100%;" id="code">
-    </div>
+  <div style="width: 100%;" id="code">
   </div>
 </template>
+
 <script>
 
   import CodeMirror from 'codemirror'
   import 'codemirror/lib/codemirror.css'
   import 'codemirror/theme/abcdef.css'
   import './UserStorageEditViewer.css'
+  import UserStoragePathBreadcrumb from "./UserStoragePathBreadcrumb";
+  import {utils} from "django-airavata-api";
 
   export default {
     name: "user-storage-file-edit-viewer",
@@ -26,27 +24,28 @@
     },
     data() {
       return {
-        fileContent: ""
+        fileContent: "",
+        editor: null
       };
     },
     mounted() {
+      this.setFileContentEditor();
       this.setFileContent();
     },
-    components: {},
-    computed: {},
     methods: {
       setFileContent() {
         if (this.userStoragePath && this.userStoragePath.files && this.userStoragePath.files.length > 0) {
-          fetch(this.userStoragePath.files[0].downloadURL).then((res) => {
-            res.text().then((text) => {
-              this.fileContent = text;
-              this.setFileContentEditor(this.fileContent);
-            });
+          utils.FetchUtils.get(this.userStoragePath.files[0].downloadURL, "", {
+            ignoreErrors: false,
+            showSpinner: true
+          }, "text").then((res) => {
+            this.fileContent = res;
+            this.editor.getDoc().setValue(this.fileContent);
           });
         }
       },
-      setFileContentEditor(value) {
-        CodeMirror(document.getElementById("code"), {
+      setFileContentEditor(value = "") {
+        this.editor = CodeMirror(document.getElementById("code"), {
           theme: "abcdef",
           mode: "text/plain",
           lineNumbers: true,
