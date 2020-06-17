b632c7dadb00eadb4de3f62f0c17a53861415b34
Dinuka De Silva <L.dinukadesilva@gmail.com>
2020-06-17 21:54:22 +0530

    AIRAVATA-3310: Adding a validation to getStoragePath method
    * Check the validitiy of `$route`

--- django_airavata/apps/workspace/static/django_airavata_workspace/js/components/storage/UserStorageFileSelectionContainer.vue
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/components/storage/UserStorageFileSelectionContainer.vue
@@ -25,6 +25,7 @@
 <script>
 import { services } from "django-airavata-api";
 import UserStoragePathViewer from "./UserStoragePathViewer";
+import {getStoragePath} from "./util";
 
 // Keep track of most recent path so that when user needs to select an
 // additional file they are taken back to the last path
@@ -33,23 +34,8 @@ let mostRecentPath = "~";
 export default {
   name: "user-storage-file-selection-container",
   computed: {
-    /**
-     * @returns {string} user storage path with an ending slash.
-     */
     storagePath() {
-      let _storagePath = /~.*$/.exec(this.$route.fullPath);
-      if (_storagePath && _storagePath.length > 0) {
-        _storagePath = _storagePath[0];
-      } else {
-        _storagePath = this.$route.path;
-      }
-
-      // Validate to have the ending slash.
-      if (!_storagePath.endsWith("/")) {
-        _storagePath += "/";
-      }
-
-      return _storagePath;
+      return getStoragePath(this.$route);
     }
   },
   props: {
