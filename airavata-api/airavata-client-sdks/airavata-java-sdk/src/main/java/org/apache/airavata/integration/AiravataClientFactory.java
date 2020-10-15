f0c4d73fde59263a75dce08aa7da637e819bc9f2
Isuru Ranawaka <irjanith@gmail.com>
2020-10-15 10:41:13 -0400

    Adding logs

--- airavata-api/airavata-client-sdks/airavata-java-sdk/src/main/java/org/apache/airavata/integration/AiravataClientFactory.java
+++ airavata-api/airavata-client-sdks/airavata-java-sdk/src/main/java/org/apache/airavata/integration/AiravataClientFactory.java
@@ -3,37 +3,49 @@ package org.apache.airavata.integration;
 import org.apache.airavata.integration.clients.AiravataAPIClient;
 import org.apache.airavata.integration.clients.IdentityManagementClient;
 import org.apache.airavata.integration.utils.SFTPFileHandler;
+import org.apache.airavata.model.error.AiravataClientException;
+import org.apache.airavata.security.AiravataSecurityException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class AiravataClientFactory {
+    private static Logger LOGGER = LoggerFactory.getLogger(AiravataAPIClient.class);
 
-
-    public static IdentityManagementClient getIdentityManagementClient(String propertiesFilePath) {
+    public static IdentityManagementClient getIdentityManagementClient(String propertiesFilePath) throws IOException, AiravataSecurityException {
         try {
             IdentityManagementClient identityManagementClient = new IdentityManagementClient(propertiesFilePath);
             return identityManagementClient;
         } catch (Exception ex) {
-            ex.printStackTrace();
+            String msg = "Error occurred while creating Identity client, " +
+                    "properties file may not exist " + ex.getMessage();
+            LOGGER.error(msg, ex);
+            throw ex;
         }
-        return null;
     }
 
-    public static SFTPFileHandler getSFTPHandler(String propertiesFilePath) {
+    public static SFTPFileHandler getSFTPHandler(String propertiesFilePath) throws IOException {
         try {
             SFTPFileHandler sftpFileHandler = new SFTPFileHandler(propertiesFilePath);
             return sftpFileHandler;
         } catch (Exception ex) {
-            ex.printStackTrace();
+            String msg = "Error occurred while creating SFTP handler, " +
+                    "properties file may not exist " + ex.getMessage();
+            LOGGER.error(msg, ex);
+            throw ex;
         }
-        return null;
     }
 
-    public static AiravataAPIClient getAiravataAPIClient(String propertiesFilePath) {
+    public static AiravataAPIClient getAiravataAPIClient(String propertiesFilePath) throws IOException, AiravataClientException {
         try {
             AiravataAPIClient airavataAPIClient = new AiravataAPIClient(propertiesFilePath);
             return airavataAPIClient;
         } catch (Exception ex) {
-            ex.printStackTrace();
+            String msg = "Error occurred while retreving Airvata api client, " +
+                    "properties file may not exist " + ex.getMessage();
+            LOGGER.error(msg, ex);
+            throw ex;
         }
-        return null;
     }
 }
