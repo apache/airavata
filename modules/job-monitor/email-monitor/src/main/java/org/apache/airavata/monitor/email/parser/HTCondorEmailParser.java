88933143381befa4023e7e755f9f8bb226ff028f
daniellthibodeau <daniellthibodeau@gmail.com>
2020-11-19 15:55:03 -0600

    Implemented the parser to allow for monitoring via emails.

--- modules/job-monitor/email-monitor/src/main/java/org/apache/airavata/monitor/email/parser/HTCondorEmailParser.java
+++ modules/job-monitor/email-monitor/src/main/java/org/apache/airavata/monitor/email/parser/HTCondorEmailParser.java
@@ -27,23 +27,69 @@ import org.slf4j.LoggerFactory;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HTCondorEmailParser implements EmailParser {
     private static final Logger log = LoggerFactory.getLogger(HTCondorEmailParser.class);
 
+    private static final String JOBID_REGEX = "(\\d+(?:\\.\\d+)?)";  // Regex pattern to match a Job ID from an HTCondor email
+    private static final String CONTENTS_REGEX = "(\\d+)";           // Regex pattern to match a Job Status
+
+    // Regex Patterns
+    private static final Pattern jobIdPattern = Pattern.compile(JOBID_REGEX);
+    private static final Pattern statusPattern = Pattern.compile(CONTENTS_REGEX);
+
+
     public JobStatusResult parseEmail(Message message) throws MessagingException, AiravataException{
-        // TODO
-        return null;
+        // Job Status Results
+        JobStatusResult jobStatusResult = new JobStatusResult();
+
+        try {
+            parseSubject(message.getSubject(), jobStatusResult);
+            parseJobState((String) message.getContent(), jobStatusResult);
+        } catch (IOException e) {
+            throw new AiravataException("[EJM]: There was an error while parsing the content of the HTCondor email -> " + e);
+        }
+
+        return jobStatusResult;
     }
 
-    private void parseSubject(String subject, JobStatusResult jobStatusResult) throws MessagingException {
-        // TODO
+
+    private void parseSubject(String subject, JobStatusResult jobStatusResult) {
+        Matcher matcher = jobIdPattern.matcher(subject);
+
+        if (matcher.find()) {
+            jobStatusResult.setJobId(matcher.group());
+            jobStatusResult.setJobName(matcher.group());
+        } else {
+            log.error("[EJM]: The Job ID was not found in the HTCondor email subject -> " + subject);
+        }
     }
 
-    private JobState getJobState(String state, String subject) {
-        // TODO
-        return null;
+
+    private void parseJobState(String content, JobStatusResult jobStatusResult) {
+        // Split message content into an array of lines
+        String[] messageArray = content.split("\n");
+
+        // Access the line of the email with the status result
+        String statusLine = messageArray[5];
+
+        // Match the job status in the status line
+        Matcher matcher = statusPattern.matcher(statusLine);
+
+        // Determine the state that the job is in
+        if(matcher.find()) {
+           String status = matcher.group();
+
+           if (status == "0") {
+               jobStatusResult.setState(JobState.COMPLETE);
+           }else {
+               jobStatusResult.setState(JobState.FAILED);
+           }
+        }else{
+            log.error("[EJM]: The Job Status was not found in the content of the HTCondor email.");
+        }
     }
 }
