c32774e0db38c8035df6a41ad701cd4e99dfbb66
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-01-22 10:48:43 -0500

    AIRAVATA-3310: Refactoring

--- django_airavata/apps/api/serializers.py
+++ django_airavata/apps/api/serializers.py
@@ -2,9 +2,10 @@ import copy
 import datetime
 import json
 import logging
+import re
+
 from urllib.parse import quote, urlencode
 
-import re
 import requests
 from airavata.model.appcatalog.appdeployment.ttypes import (
     ApplicationDeploymentDescription,
