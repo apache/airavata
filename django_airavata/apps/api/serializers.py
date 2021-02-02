876de738ac98b80157bdcf503ae85ec71256ecc0
Marcus Christie <machristie@apache.org>
2021-02-02 11:54:30 -0500

    AIRAVATA-3362 moved get_rel_experiment_dir to user_storage in SDK

--- django_airavata/apps/api/serializers.py
+++ django_airavata/apps/api/serializers.py
@@ -457,7 +457,11 @@ class ExperimentSerializer(
             ResourcePermissionType.WRITE)
 
     def get_relativeExperimentDataDir(self, experiment):
+        request = self.context['request']
+        if hasattr(user_storage, "get_rel_experiment_dir"):
+            return user_storage.get_rel_experiment_dir(request, experiment.experimentId)
 
+        # TODO: remove this older implementation
         if (experiment.userConfigurationData and
                 experiment.userConfigurationData.experimentDataDir):
             request = self.context['request']
