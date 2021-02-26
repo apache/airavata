c6c66e6bebb0549100fe47208bd3eeb4d957c952
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2021-02-26 12:15:02 -0500

    Revert "Setting custos id to experiment"
    This reverts commit 53ccef40

--- modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/entities/expcatalog/ExperimentEntity.java
+++ modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/entities/expcatalog/ExperimentEntity.java
@@ -73,9 +73,6 @@ public class ExperimentEntity implements Serializable {
     @Column(name = "ENABLE_EMAIL_NOTIFICATION")
     public boolean enableEmailNotification;
 
-    @Column(name = "CUSTOS_ID")
-    public boolean custosId;
-
     @Lob
     @Column(name = "EMAIL_ADDRESSES")
     public String emailAddresses;
