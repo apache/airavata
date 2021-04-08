6bcdebc3cfd492802b6a95b33ee2cbcdfaa0f46d
Marcus Christie <machristie@apache.org>
2021-04-08 15:06:47 -0400

    Common UI components library, for use in custom apps

--- django_airavata/static/common/vue.config.js
+++ django_airavata/static/common/vue.config.js
@@ -7,12 +7,23 @@ module.exports = {
     cms: "./js/cms.js",
     notices: "./js/notices.js",
   },
-  configureWebpack: {
-    plugins: [
-      new BundleTracker({
-        filename: "webpack-stats.json",
-        path: "./dist/",
-      }),
-    ],
+  configureWebpack: (config) => {
+    if (process.env.LIBRARY_MODE === "true") {
+      // Externalize the django-airavata-api library, will be available to
+      // custom apps as global AiravataAPI object
+      config.externals = config.externals || {};
+      config.externals["django-airavata-api"] = {
+        commonjs: "django-airavata-api",
+        commonjs2: "django-airavata-api",
+        root: "AiravataAPI",
+      };
+    } else {
+      config.plugins.push(
+        new BundleTracker({
+          filename: "webpack-stats.json",
+          path: "./dist/",
+        })
+      );
+    }
   },
 };
