87b33a18d097575d2ca39d855334aa5379564d2d
Marcus Christie <machristie@apache.org>
2021-04-15 17:27:51 -0400

    AIRAVATA-3420 Revert download url change
    Keep old behavior since new behavior requires changes to airavata-django-portal and this
    way we can update the SDK first and then the airavata-django-portal

--- airavata_django_portal_sdk/urls.py
+++ airavata_django_portal_sdk/urls.py
@@ -9,7 +9,7 @@ from . import views
 def get_download_url(data_product_uri):
     """(Deprecated) Get URL for downloading data product identified by data_product_uri."""
     warnings.warn("Use user_storage.get_download_url instead.", DeprecationWarning)
-    return (reverse("airavata_django_portal_sdk:download_file") + "?" +
+    return (reverse("django_airavata_api:download_file") + "?" +
             urlencode({"data-product-uri": data_product_uri}))
 
 
