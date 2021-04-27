c8cacc227101c48b35dee1781f369c24f63a1e03
Marcus Christie <machristie@apache.org>
2021-04-27 16:56:59 -0400

    AIRAVATA-3420 Only load MFTUserStorageProvider if dependencies are also available

--- airavata_django_portal_sdk/user_storage/backends/__init__.py
+++ airavata_django_portal_sdk/user_storage/backends/__init__.py
@@ -1,4 +1,9 @@
 from .django_filesystem_provider import DjangoFileSystemProvider
-from .mft_provider import MFTUserStorageProvider
+try:
+    # Try to import MFTUserStorageProvider, but dependencies may not be loaded
+    # so the following might fail
+    from .mft_provider import MFTUserStorageProvider
+except Exception:
+    pass
 
 __all__ = ['DjangoFileSystemProvider', 'MFTUserStorageProvider']
