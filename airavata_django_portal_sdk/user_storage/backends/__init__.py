3c3fb34b18c0e0420621fb67e6437ba7a9be45c7
Marcus Christie <machristie@apache.org>
2021-04-29 18:11:16 -0400

    AIRAVATA-3420 import style fix

--- airavata_django_portal_sdk/user_storage/backends/__init__.py
+++ airavata_django_portal_sdk/user_storage/backends/__init__.py
@@ -1,4 +1,5 @@
 from .django_filesystem_provider import DjangoFileSystemProvider
+
 try:
     # Try to import MFTUserStorageProvider, but dependencies may not be loaded
     # so the following might fail
