e54afd181f296ec1985f98c959c04226adb261ee
Marcus Christie <machristie@apache.org>
2021-05-03 11:15:17 -0400

    AIRAVATA-3453 Integrated jquery-textcomplete plugin
    To get the plugin and the hide/show logic to work
    I needed to switch from Vue slots to native slots
    which are now dynamically generated for exp
    inputs in ExperimentEditor.

--- django_airavata/apps/workspace/static/django_airavata_workspace/js/web-components/ExperimentEditor.vue
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/web-components/ExperimentEditor.vue
@@ -10,16 +10,12 @@
       </slot>
     </div>
     <template v-for="input in experiment.experimentInputs">
-      <div :key="input.name" @input="updateInputValue($event, input)">
-        <slot :name="input.name">
-          {{ input.name }}
-          <input
-            v-if="input.type.name == 'STRING'"
-            :name="`input:${input.name}`"
-            :value="input.value"
-          />
-        </slot>
-        <!-- TODO: add support for other input types -->
+      <div
+        :ref="input.name"
+        :key="input.name"
+        @input="updateInputValue($event, input)"
+      >
+      <!-- programmatically define slots as native slots (not Vue slots), see #mounted() -->
       </div>
     </template>
     <slot name="save-button">
@@ -48,12 +44,32 @@ export default {
       required: false,
     },
   },
-  async created() {
+  async created() {},
+  async mounted() {
     this.applicationModule = await getApplicationModule(this.applicationId);
     this.appInterface = await getApplicationInterfaceForModule(
       this.applicationId
     );
     this.experiment = await this.loadExperiment();
+    // vue-web-component-wrapper clones native slots and turns them into Vue
+    // slots which means they lose any event listeners and they basically aren't
+    // in the DOM any more.  As a workaround, programmatically create native
+    // slots. See also https://github.com/vuejs/vue-web-component-wrapper/issues/38
+    this.$nextTick(() => {
+      for (const input of this.experiment.experimentInputs) {
+        const slot = document.createElement("slot");
+        slot.setAttribute("name", input.name);
+        if (input.type.name === "STRING") {
+          slot.textContent = `${input.name} `;
+          const textInput = document.createElement("input");
+          textInput.setAttribute("type", "text");
+          textInput.setAttribute("value", input.value);
+          slot.appendChild(textInput);
+        }
+        // TODO: add support for other input types
+        this.$refs[input.name][0].append(slot);
+      }
+    });
   },
   data() {
     return {
@@ -105,7 +121,7 @@ export default {
         const defaultProjectId = await getDefaultProjectId();
         experiment.projectId = defaultProjectId;
         experiment.userConfigurationData.computationalResourceScheduling.resourceHostId =
-          "bigred3.uits.iu.edu_2141bf96-c458-4ecd-8759-aa3a08f31956";
+          "js-169-51.jetstream-cloud.org_6672e8fe-8d63-4bbe-8bf8-4ea04092e72f";
         this.$emit("loaded", experiment);
         return experiment;
       }
