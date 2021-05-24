7cb087cb2e660dad9c228f9011085c1a042df157
Marcus Christie <machristie@apache.org>
2021-05-24 17:26:40 -0400

    AIRAVATA-3420 backwards compatible /api/download

--- airavata_django_portal_sdk/user_storage/api.py
+++ airavata_django_portal_sdk/user_storage/api.py
@@ -223,7 +223,9 @@ def open_file(request, data_product=None, data_product_uri=None):
             request,
             "/download",
             params={'data-product-uri': data_product.productUri},
-            base_url="/sdk")
+            # TODO: once /sdk/download is integrated with airavata-django-portal, this can be updated to
+            # base_url="/sdk"
+            base_url="/api")
         file = io.BytesIO(resp.content)
         disposition = resp.headers['Content-Disposition']
         disp_value, disp_params = cgi.parse_header(disposition)
