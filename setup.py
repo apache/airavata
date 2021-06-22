5d0f4e562e4a75149fd393291875491c5f649fcc
Marcus Christie <machristie@apache.org>
2021-06-22 16:11:18 -0400

    AIRAVATA-3420 Fix dependency on DRF

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "django",
-        "django-rest-framework",
+        "djangorestframework",
         # Installed from git repo; see requirements.txt for details
         "airavata-python-sdk",
     ],
