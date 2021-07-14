7724c7eba70f1b75edb9b1f4df9bb40c18062627
Marcus Christie <machristie@apache.org>
2021-07-14 14:01:57 -0400

    Windows: fixes extra whitespace when inserting output view provider in setup.cfg #1

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -51,9 +51,11 @@ def get_django_app_package_name(setup_cfg_file):
 
 def insert_output_view_provider(setup_cfg_lines, index, insert_entry_point_group=False):
     updated_lines = setup_cfg_lines.copy()
-    updated_lines.insert(index+1, "    {{cookiecutter.project_slug}} = {{cookiecutter.custom_django_app_module_name}}.{{cookiecutter.output_views_directory_name}}.{{cookiecutter.project_slug}}:{{cookiecutter.output_view_provider_class_name}}" + os.linesep)
+    # Just use \n for line endings. readlines and writelines already does
+    # platform specific line ending conversion.
+    updated_lines.insert(index+1, "    {{cookiecutter.project_slug}} = {{cookiecutter.custom_django_app_module_name}}.{{cookiecutter.output_views_directory_name}}.{{cookiecutter.project_slug}}:{{cookiecutter.output_view_provider_class_name}}\n")
     if insert_entry_point_group:
-        updated_lines.insert(index+1, "airavata.output_view_providers =" + os.linesep)
+        updated_lines.insert(index+1, "airavata.output_view_providers =\n")
     return updated_lines
 
 setup_cfg_file = find_setup_cfg_file()
