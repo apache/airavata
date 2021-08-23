fb1c4ee2d95ef7778cd468cf081ebeed01e16c94
Marcus Christie <machristie@apache.org>
2021-08-23 15:20:26 -0400

    AIRAVATA-3502 Handle edge case that refresh token fails because user is logged out in another client

--- django_airavata/apps/auth/backends.py
+++ django_airavata/apps/auth/backends.py
@@ -50,10 +50,12 @@ class KeycloakBackend(object):
                 user = self._process_userinfo(request, userinfo)
                 access_token = token
             # user is already logged in and can use refresh token
-            elif request.user and not utils.is_refresh_token_expired(request):
+            elif request.user.is_authenticated and not utils.is_refresh_token_expired(request):
                 logger.debug("Refreshing token...")
                 token, userinfo = \
                     self._get_token_and_userinfo_from_refresh_token(request)
+                if token is None:  # refresh failed
+                    return None
                 self._process_token(request, token)
                 # user is already logged in
                 user = request.user
@@ -63,6 +65,8 @@ class KeycloakBackend(object):
                 token, userinfo = \
                     self._get_token_and_userinfo_from_refresh_token(
                         request, refresh_token=refresh_token)
+                if token is None:  # refresh failed
+                    return None
                 self._process_token(request, token)
                 user = self._process_userinfo(request, userinfo)
                 access_token = token['access_token']
@@ -161,12 +165,18 @@ class KeycloakBackend(object):
         # refresh_token doesn't take client_secret kwarg, so create auth
         # explicitly
         auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-        token = oauth2_session.refresh_token(token_url=token_url,
-                                             refresh_token=refresh_token_,
-                                             auth=auth,
-                                             verify=verify)
-        userinfo = oauth2_session.get(userinfo_url).json()
-        return token, userinfo
+        try:
+            token = oauth2_session.refresh_token(token_url=token_url,
+                                                 refresh_token=refresh_token_,
+                                                 auth=auth,
+                                                 verify=verify)
+            userinfo = oauth2_session.get(userinfo_url).json()
+            return token, userinfo
+        except InvalidGrantError as e:
+            # probably session was terminated by admin or by user logging out in another client
+            logger.warning(f"Failed to refresh token for user {request.user.username} "
+                           f": {e}")
+            return None, None
 
     def _get_userinfo_from_token(self, request, token):
         client_id = settings.KEYCLOAK_CLIENT_ID
