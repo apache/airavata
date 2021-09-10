49dfb23291c7199d6ee5920f3f06370434ab1c1d
Marcus Christie <machristie@apache.org>
2021-09-10 15:33:57 -0400

    AIRAVATA-3477 Wrapped SelectInputEditor as web component

---
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/web-components/input-editors/SelectInputEditor.vue
@@ -0,0 +1,40 @@
+<template>
+  <select-input-editor
+    v-if="experimentInput"
+    :id="id"
+    :value="data"
+    :experiment-input="experimentInput"
+    :read-only="readOnly"
+    :options="options"
+    @input="valueChanged"
+  />
+</template>
+
+<script>
+import SelectInputEditor from "../../components/experiment/input-editors/SelectInputEditor.vue";
+import WebComponentInputEditorMixin from "./WebComponentInputEditorMixin.js";
+
+export default {
+  mixins: [WebComponentInputEditorMixin],
+  props: {
+    // Explicit copy props from mixin, workaround for bug, see
+    // https://github.com/vuejs/vue-web-component-wrapper/issues/30#issuecomment-427350734
+    // for more details
+    ...WebComponentInputEditorMixin.props,
+    options: {
+      type: Array,
+      default: null,
+    },
+  },
+  components: {
+    SelectInputEditor,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../styles";
+:host {
+  display: block;
+}
+</style>
