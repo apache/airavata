b7f21340af2f1b639e1d005af5d8c3033cbe506a
Marcus Christie <machristie@apache.org>
2021-09-15 11:59:40 -0400

    AIRAVATA-3477 Wrapped SliderInputEditor as web component

---
+++ django_airavata/apps/workspace/static/django_airavata_workspace/js/web-components/input-editors/SliderInputEditor.vue
@@ -0,0 +1,64 @@
+<template>
+  <slider-input-editor
+    v-if="experimentInput"
+    :id="id"
+    :value="data"
+    :experiment-input="experimentInput"
+    :read-only="readOnly"
+    :min="min"
+    :max="max"
+    :step="step"
+    :value-format="valueFormat"
+    :display-format="displayFormat"
+    @input="valueChanged"
+  />
+</template>
+
+<script>
+import SliderInputEditor from "../../components/experiment/input-editors/SliderInputEditor.vue";
+import WebComponentInputEditorMixin from "./WebComponentInputEditorMixin.js";
+
+export default {
+  mixins: [WebComponentInputEditorMixin],
+  props: {
+    // Explicit copy props from mixin, workaround for bug, see
+    // https://github.com/vuejs/vue-web-component-wrapper/issues/30#issuecomment-427350734
+    // for more details
+    ...WebComponentInputEditorMixin.props,
+    min: {
+      type: Number,
+    },
+    max: {
+      type: Number,
+    },
+    step: {
+      type: Number,
+    },
+    valueFormat: {
+      type: String,
+      validator(value) {
+        return ["percentage"].indexOf(value) !== -1;
+      },
+    },
+    displayFormat: {
+      type: String,
+      validator(value) {
+        return ["percentage"].indexOf(value) !== -1;
+      },
+    },
+  },
+  components: {
+    SliderInputEditor,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../styles";
+// Need to explicitly import VueSlider's CSS because importing component scss doesn't work
+// https://github.com/vuejs/vue-web-component-wrapper/issues/12
+@import "~vue-slider-component/dist-css/vue-slider-component.css";
+:host {
+  display: block;
+}
+</style>
