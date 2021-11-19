28ccd98b9f459d3b59736bc47e9e94fabdedf987
Marcus Christie <machristie@apache.org>
2021-11-19 10:57:27 -0500

    AIRAVATA-3542 bump version to 1.3.dev3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="airavata-django-portal-sdk",
-    version="1.3.dev2",
+    version="1.3.dev3",
     url="https://github.com/apache/airavata-django-portal-sdk",
     author="Apache Software Foundation",
     author_email="dev@airavata.apache.org",
