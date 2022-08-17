eb903882e2e56505b6c429351bf239ceabf6d5cf
Marcus Christie <machristie@apache.org>
2022-08-17 15:19:17 -0400

    Bump to version 1.5.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="airavata-django-portal-sdk",
-    version="1.4.1",
+    version="1.5.0",
     url="https://github.com/apache/airavata-django-portal-sdk",
     author="Apache Software Foundation",
     author_email="dev@airavata.apache.org",
