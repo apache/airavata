db1ec023060df770470317fd9e09cdde501c6f99
Marcus Christie <machristie@apache.org>
2022-08-23 11:09:14 -0400

    AIRAVATA-3649 Moved tests into module

--- runtests.py
+++ runtests.py
@@ -7,9 +7,9 @@ from django.conf import settings
 from django.test.utils import get_runner
 
 if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'airavata_django_portal_sdk.tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(["tests"])
+    failures = test_runner.run_tests(["airavata_django_portal_sdk.tests"])
     sys.exit(bool(failures))
