a925344e120c3ab1d9c2287b31f755ef0804b038
Marcus Christie <machristie@apache.org>
2023-01-25 11:23:21 -0500

    AIRAVATA-3680 Basic unit test for loading experiment details by job id

--- test_js.sh
+++ test_js.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo -e "Testing JS"
 (cd $SCRIPT_DIR/django_airavata/apps/api && yarn && yarn run test) || exit 1
 (cd $SCRIPT_DIR/django_airavata/apps/workspace && yarn && yarn run test) || exit 1
+(cd $SCRIPT_DIR/django_airavata/apps/admin && yarn && yarn run test) || exit 1
 
 echo -e "All testing finished successfully!"
 
