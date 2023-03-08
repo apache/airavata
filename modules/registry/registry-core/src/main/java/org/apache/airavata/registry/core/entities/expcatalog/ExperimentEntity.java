363468ac5f5b07b64de4bc40bc8f95c509ff2c48
Isuru Ranawaka <irjanith@gmail.com>
2023-03-08 14:56:58 -0500

    Bug fix in Thrift model to ENtity mapping

--- modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/entities/expcatalog/ExperimentEntity.java
+++ modules/registry/registry-core/src/main/java/org/apache/airavata/registry/core/entities/expcatalog/ExperimentEntity.java
@@ -32,7 +32,7 @@ import java.util.List;
  */
 @Entity
 @Table(name = "EXPERIMENT")
-public class ExperimentEntity implements Serializable {
+public class   ExperimentEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
