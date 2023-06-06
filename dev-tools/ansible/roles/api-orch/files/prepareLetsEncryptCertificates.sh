e3592baeabb18e3742caaef6061bf4f4f20df0b9
Marcus Christie <machristie@apache.org>
2023-06-06 12:40:01 -0400

    Ansible: fixes for haproxy configuration

--- dev-tools/ansible/roles/api-orch/files/prepareLetsEncryptCertificates.sh
+++ dev-tools/ansible/roles/api-orch/files/prepareLetsEncryptCertificates.sh
@@ -8,6 +8,7 @@ for CERTIFICATE in `find /etc/letsencrypt/live/* -type d`; do
   CERTIFICATE=`basename $CERTIFICATE`
 
   # Combine certificate and private key to single file
+  mkdir -p /etc/ssl/$CERTIFICATE/
   cat /etc/letsencrypt/live/$CERTIFICATE/fullchain.pem /etc/letsencrypt/live/$CERTIFICATE/privkey.pem > /etc/ssl/$CERTIFICATE/$CERTIFICATE.pem
 
 done
