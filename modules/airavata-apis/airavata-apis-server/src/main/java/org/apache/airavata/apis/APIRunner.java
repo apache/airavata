5fe4af33d2619aa5f9ff8dad714db2eea4165905
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2023-06-28 00:55:55 -0400

    Bundling MFT with main service

--- modules/airavata-apis/airavata-apis-server/src/main/java/org/apache/airavata/apis/APIRunner.java
+++ modules/airavata-apis/airavata-apis-server/src/main/java/org/apache/airavata/apis/APIRunner.java
@@ -4,17 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.apache.airavata",
+@ComponentScan(basePackages = {"org.apache.airavata.apis",
         "org.apache.airavata.mft.secret.server.handler",
-        "org.apache.airavata.mft.resource.server.handler"
+        "org.apache.airavata.mft.resource.server.handler",
+        "org.apache.airavata.mft.api.handler",
+        "org.apache.airavata.mft.controller",
+        "org.apache.airavata.mft.agent"
 })
 @EntityScan(basePackages = {"org.apache.airavata"})
 @EnableJpaAuditing
-@PropertySource(value = "classpath:api.properties")
 public class APIRunner {
 
     public static void main(String[] args) {
