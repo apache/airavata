d0b5e68abb01f56fc7ba4bfcb1d6aac31eed2638
ganning127 <ganningxu@gmail.com>
2024-05-28 13:18:20 +0800

    setup list experiments

---
+++ airavata-local-agent/renderer/pages/list-experiments.jsx
@@ -0,0 +1,16 @@
+import { Button, Container, Img, Text, Flex, Heading, Link, HStack } from "@chakra-ui/react";
+import { NavContainer } from "../components/NavContainer";
+
+const ListExperiments = () =>
+{
+    return (
+        <>
+            <NavContainer activePage="list-experiments">
+                sdfsa
+            </NavContainer>
+
+        </>
+    );
+};
+
+export default ListExperiments;
