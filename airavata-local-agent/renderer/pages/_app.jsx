84a4dae2a225a27f0c20a28331c3e42e66cf2019
ganning127 <ganningxu@gmail.com>
2024-06-15 14:25:12 -0400

    queues, compute, all works now

--- airavata-local-agent/renderer/pages/_app.jsx
+++ airavata-local-agent/renderer/pages/_app.jsx
@@ -2,11 +2,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
-    );
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
