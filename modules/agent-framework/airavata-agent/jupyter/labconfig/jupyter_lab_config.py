cb44d8f75d87ebf8fcbcf75f2b6a17ba7ab49bbc
Dimuthu Wannipurage <dimuthuw@gatech.edu>
2024-07-19 15:14:32 -0400

    Adding container configurations to jupyter and airavata agent

---
+++ modules/agent-framework/airavata-agent/jupyter/labconfig/jupyter_lab_config.py
@@ -0,0 +1,15 @@
+c = get_config()
+
+# Set the IP address Jupyter Lab will listen on
+c.ServerApp.ip = '0.0.0.0'
+
+# Set the port Jupyter Lab will listen on
+c.ServerApp.port = 8888
+
+# Don't open the browser by default
+c.ServerApp.open_browser = False
+
+c.FileContentsManager.use_atomic_writing = False
+
+# Allow root access
+c.ServerApp.allow_root = True
