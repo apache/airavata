407772ae2a82fc67bb5cadd2414fd30ed7e2c61b
ganning127 <ganningxu@gmail.com>
2024-08-22 12:12:43 -0400

    add asf src headers

--- airavata-local-agent/renderer/components/NavContainer.jsx
+++ airavata-local-agent/renderer/components/NavContainer.jsx
@@ -1,6 +1,25 @@
+/*****************************************************************
+*
+*  Licensed to the Apache Software Foundation (ASF) under one  
+*  or more contributor license agreements.  See the NOTICE file
+*  distributed with this work for additional information       
+*  regarding copyright ownership.  The ASF licenses this file  
+*  to you under the Apache License, Version 2.0 (the           
+*  "License"); you may not use this file except in compliance  
+*  with the License.  You may obtain a copy of the License at  
+*                                                              
+*    http://www.apache.org/licenses/LICENSE-2.0                
+*                                                              
+*  Unless required by applicable law or agreed to in writing,  
+*  software distributed under the License is distributed on an 
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY      
+*  KIND, either express or implied.  See the License for the   
+*  specific language governing permissions and limitations     
+*  under the License.                                          
+*                                                              
+*
+*****************************************************************/
 import React from 'react';
-
-
 import { Center, Text, Grid, GridItem, Box, Avatar, Flex, Button, useToast, Badge } from '@chakra-ui/react';
 import { BsPersonFill, BsCalendar2Week } from "react-icons/bs";
 import { LinkIconBox } from '../components/LinkIconBox';
@@ -8,8 +27,7 @@ import { MdDashboard } from "react-icons/md";
 import { NavBar } from './NavBar';
 import { LuScreenShare } from "react-icons/lu";
 
-export function NavContainer({ activePage, children })
-{
+export function NavContainer({ activePage, children }) {
 
     activePage = activePage.toLowerCase();
 
