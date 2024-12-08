8b9cacf758cfe68e1a864589f9f0f7c49975c205
yasith <yasith.j@icloud.com>
2024-12-08 03:06:19 -0600

    improve auth flow, add python code invocation, add plan crud apis and file ul/dl apis, reduce settings.ini deps, fix bugs

--- airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/sftp.py
+++ airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/sftp.py
@@ -20,7 +20,6 @@ from datetime import datetime
 from rich.progress import Progress
 
 import paramiko
-from paramiko import SFTPClient, Transport
 from scp import SCPClient
 
 logger = logging.getLogger(__name__)
@@ -36,61 +35,28 @@ def create_pkey(pkey_path):
 
 class SFTPConnector(object):
 
-  def __init__(self, host, port, username, password=None, pkey=None):
+  def __init__(self, host: str, port: int, username: str, password: str | None = None, pkey: str | None = None):
     self.host = host
     self.port = port
     self.username = username
     self.password = password
-    self.pkey = pkey
 
     ssh = paramiko.SSHClient()
     self.ssh = ssh
-    # self.sftp = paramiko.SFTPClient()
-    # Trust all key policy on remote host
-
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-  def upload_files(self, localpaths: list[Path], remote_path: str):
-    transport = Transport(sock=(self.host, int(self.port)))
-    if self.pkey is not None:
-      transport.connect(username=self.username, pkey=create_pkey(self.pkey))
-    else:
-      transport.connect(username=self.username, password=self.password)
-    try:
-      with Progress() as progress:
-        task = progress.add_task("Uploading...", total=len(localpaths)-1)
-        for file in localpaths:
-          connection = SFTPClient.from_transport(transport)
-          assert connection is not None
-          try:
-            connection.lstat(remote_path)  # Test if remote_path exists
-          except IOError:
-            connection.mkdir(remote_path)
-          remote_fpath = remote_path + "/" + file.name
-          connection.put(file, remote_fpath)
-          progress.update(task, advance=1, description=f"Uploading: {file.name}")
-        progress.update(task, completed=True)
-    finally:
-      transport.close()
-    return remote_path
-
-  def make_experiment_dir(self, project_name: str, exprement_id, remote_base=""):
+  def mkdir(self, project_name: str, exprement_id: str):
     project_name = project_name.replace(" ", "_")
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(" ", "_")
     time = time.replace(":", "_")
     time = time.replace("-", "_")
     exprement_id = exprement_id + "_" + time
-    base_path = remote_base + "/" + project_name
+    base_path = "/" + project_name
     remote_path = base_path + "/" + exprement_id
-    transport = Transport(sock=(self.host, int(self.port)))
-    if self.pkey is not None:
-      transport.connect(username=self.username,
-                        pkey=create_pkey(self.pkey))
-    else:
-      transport.connect(username=self.username, password=self.password)
-
+    transport = paramiko.Transport(sock=(self.host, int(self.port)))
+    transport.connect(username=self.username, password=self.password)
     try:
-      connection = SFTPClient.from_transport(transport)
+      connection = paramiko.SFTPClient.from_transport(transport)
       assert connection is not None
       try:
         connection.lstat(base_path)  # Test if remote_path exists
@@ -102,25 +68,55 @@ class SFTPConnector(object):
         connection.mkdir(remote_path)
     finally:
       transport.close()
-
     return remote_path
 
-  def download_files(self, local_path, remote_path):
-    if self.pkey is not None:
-      self.ssh.connect(self.host, self.port, self.username,
-                       pkey=create_pkey(self.pkey))
-    else:
-      self.ssh.connect(self.host, self.port,
-                       self.username, password=self.password)
-
-    transport = self.ssh.get_transport()
-    assert transport is not None
+  def put(self, local_paths: list[Path], remote_path: str) -> list[str]:
+    transport = paramiko.Transport(sock=(self.host, int(self.port)))
+    transport.connect(username=self.username, password=self.password)
+    remote_paths = []
+    try:
+      with Progress() as progress:
+        task = progress.add_task("Uploading...", total=len(local_paths)-1)
+        for file in local_paths:
+          connection = paramiko.SFTPClient.from_transport(transport)
+          assert connection is not None
+          try:
+            connection.lstat(remote_path)  # Test if remote_path exists
+          except IOError:
+            connection.mkdir(remote_path)
+          remote_fpath = remote_path + "/" + file.name
+          connection.put(file, remote_fpath)
+          remote_paths.append(remote_fpath)
+          progress.update(task, advance=1, description=f"Uploading: {file.name}")
+        progress.update(task, completed=True)
+    finally:
+      transport.close()
+    return remote_paths
+  
+  def ls(self, remote_path: str) -> list[str]:
+    transport = paramiko.Transport(sock=(self.host, int(self.port)))
+    transport.connect(username=self.username, password=self.password)
+    try:
+      connection = paramiko.SFTPClient.from_transport(transport)
+      assert connection is not None
+      files = connection.listdir(remote_path)
+    finally:
+      transport.close()
+    return files
+  
+  def get(self, remote_path: str, local_path: str) -> str:
+    transport = paramiko.Transport(sock=(self.host, int(self.port)))
+    transport.connect(username=self.username, password=self.password)
     with SCPClient(transport) as conn:
-      conn.get(remote_path=remote_path,
-               local_path=local_path, recursive=True)
+      conn.get(remote_path, local_path, recursive=True)
     self.ssh.close()
-
-  @staticmethod
-  def uploading_info(uploaded_file_size, total_file_size):
-    logging.info("uploaded_file_size : {} total_file_size : {}".format(
-        uploaded_file_size, total_file_size))
+    return (Path(local_path) / Path(remote_path).name).as_posix()
+  
+  def cat(self, remote_path: str) -> bytes:
+    transport = paramiko.Transport(sock=(self.host, int(self.port)))
+    transport.connect(username=self.username, password=self.password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    assert sftp is not None
+    with sftp.open(remote_path, "r") as f:
+      content = f.read()
+    return content
