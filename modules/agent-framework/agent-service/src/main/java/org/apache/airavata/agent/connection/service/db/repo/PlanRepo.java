0eeffef158cee73b04fa8370607eb982e11cd5c3
lahiruj <lahirujayathilake@gmail.com>
2024-12-09 03:17:25 -0500

    support airavata experiment plan services

---
+++ modules/agent-framework/agent-service/src/main/java/org/apache/airavata/agent/connection/service/db/repo/PlanRepo.java
@@ -0,0 +1,13 @@
+package org.apache.airavata.agent.connection.service.db.repo;
+
+import org.apache.airavata.agent.connection.service.db.entity.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PlanRepo extends JpaRepository<Plan, String> {
+
+    List<Plan> findByUserId(String userId);
+}
---
+++ modules/agent-framework/agent-service/src/main/java/org/apache/airavata/agent/connection/service/handlers/PlanHandler.java
@@ -0,0 +1,35 @@
+package org.apache.airavata.agent.connection.service.handlers;
+
+import org.apache.airavata.agent.connection.service.db.entity.Plan;
+import org.apache.airavata.agent.connection.service.db.repo.PlanRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlanHandler {
+
+    private final static Logger logger = LoggerFactory.getLogger(PlanHandler.class);
+
+    private final PlanRepo planRepo;
+
+    public PlanHandler(PlanRepo planRepo) {
+        this.planRepo = planRepo;
+    }
+
+    public Plan savePlan(Plan plan) {
+        Plan savedPlan = planRepo.save(plan);
+        logger.info("Created the plan with the id: {}", plan.getId());
+        return savedPlan;
+    }
+
+    public List<Plan> getAllPlansByUserId(String userId) {
+        return planRepo.findByUserId(userId);
+    }
+
+    public Plan getPlanById(String planId) {
+        return planRepo.findById(planId).orElseThrow(() -> new RuntimeException("Plan not found: " + planId));
+    }
+}
