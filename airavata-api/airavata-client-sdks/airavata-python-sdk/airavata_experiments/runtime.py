f083e103693df69bbcee92bd9fbe911fae0c9c4e
yasith <yasith.j@icloud.com>
2024-12-18 01:32:11 -0600

    fix bug in code execution, remove jupyter kernel start from agent add support to cold-start analysis agents reset changes to sftp_file_handling_client update notebooks remove verbose log from remote code execution separate python env creation and code execution steps. always return stdout+err for command/script executions lock agent to python 3.12 and update pyproject.toml update file listing and python execution cmds

--- airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/runtime.py
+++ airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/runtime.py
@@ -165,12 +165,11 @@ class Remote(Runtime):
   def execute_py(self, libraries: list[str], code: str, task: Task) -> None:
     assert task.ref is not None
     assert task.agent_ref is not None
-    print(f"* Packages: {libraries}")
-    print(f"* Code:\n{code}")
+    assert task.pid is not None
 
     from .airavata import AiravataOperator
     av = AiravataOperator(context.access_token)
-    result = av.execute_py(libraries, code, task.agent_ref)
+    result = av.execute_py(libraries, code, task.agent_ref, task.pid, task.runtime.args)
     print(result)
 
   def status(self, task: Task):
@@ -246,11 +245,21 @@ class Remote(Runtime):
 def list_runtimes(
     cluster: str | None = None,
     category: str | None = None,
+    node_count: int | None = None,
+    cpu_count: int | None = None,
+    walltime: int | None = None,
 ) -> list[Runtime]:
   from .airavata import AiravataOperator
   av = AiravataOperator(context.access_token)
   all_runtimes = av.get_available_runtimes()
-  return [*filter(lambda r: (cluster in [None, r.args["cluster"]]) and (category in [None, r.args["category"]]), all_runtimes)]
+  out_runtimes = []
+  for r in all_runtimes:
+    if (cluster in [None, r.args["cluster"]]) and (category in [None, r.args["category"]]):
+      r.args["node_count"] = node_count or r.args["node_count"]
+      r.args["cpu_count"] = cpu_count or r.args["cpu_count"]
+      r.args["walltime"] = walltime or r.args["walltime"]
+      out_runtimes.append(r)
+  return out_runtimes
 
 def is_terminal_state(x):
   return x in ["CANCELED", "COMPLETED", "FAILED"]
--- modules/agent-framework/airavata-agent/agent.go
+++ modules/agent-framework/airavata-agent/agent.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	protos "airavata-agent/protos"
-	"bufio"
 	"bytes"
 	"context"
 	"encoding/json"
@@ -50,57 +49,57 @@ func main() {
 		log.Printf("Connected to the server...")
 	}
 
-	go func() {
-		log.Printf("Starting jupyter kernel")
-		cmd := exec.Command("python", "/opt/jupyter/kernel.py")
-		//cmd := exec.Command("jupyter/venv/bin/python", "jupyter/kernel.py")
-		stdout, err := cmd.StdoutPipe()
+	// go func() {
+	// 	log.Printf("Starting jupyter kernel")
+	// 	cmd := exec.Command("python", "/opt/jupyter/kernel.py")
+	// 	//cmd := exec.Command("jupyter/venv/bin/python", "jupyter/kernel.py")
+	// 	stdout, err := cmd.StdoutPipe()
 
-		if err != nil {
-			fmt.Println("[agent.go] Error creating StdoutPipe:", err)
-			return
-		}
+	// 	if err != nil {
+	// 		fmt.Println("[agent.go] Error creating StdoutPipe:", err)
+	// 		return
+	// 	}
 
-		// Get stderr pipe
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			fmt.Println("[agent.go] Error creating StderrPipe:", err)
-			return
-		}
+	// 	// Get stderr pipe
+	// 	stderr, err := cmd.StderrPipe()
+	// 	if err != nil {
+	// 		fmt.Println("[agent.go] Error creating StderrPipe:", err)
+	// 		return
+	// 	}
 
-		log.Printf("[agent.go] Starting command for execution")
-		// Start the command
-		if err := cmd.Start(); err != nil {
-			fmt.Println("[agent.go] Error starting command:", err)
-			return
-		}
+	// 	log.Printf("[agent.go] Starting command for execution")
+	// 	// Start the command
+	// 	if err := cmd.Start(); err != nil {
+	// 		fmt.Println("[agent.go] Error starting command:", err)
+	// 		return
+	// 	}
 
-		// Create channels to read from stdout and stderr
-		stdoutScanner := bufio.NewScanner(stdout)
-		stderrScanner := bufio.NewScanner(stderr)
+	// 	// Create channels to read from stdout and stderr
+	// 	stdoutScanner := bufio.NewScanner(stdout)
+	// 	stderrScanner := bufio.NewScanner(stderr)
 
-		// Stream stdout
-		go func() {
-			for stdoutScanner.Scan() {
-				fmt.Printf("[agent.go] stdout: %s\n", stdoutScanner.Text())
-			}
-		}()
+	// 	// Stream stdout
+	// 	go func() {
+	// 		for stdoutScanner.Scan() {
+	// 			fmt.Printf("[agent.go] stdout: %s\n", stdoutScanner.Text())
+	// 		}
+	// 	}()
 
-		// Stream stderr
-		go func() {
-			for stderrScanner.Scan() {
-				fmt.Printf("[agent.go] stderr: %s\n", stderrScanner.Text())
-			}
-		}()
+	// 	// Stream stderr
+	// 	go func() {
+	// 		for stderrScanner.Scan() {
+	// 			fmt.Printf("[agent.go] stderr: %s\n", stderrScanner.Text())
+	// 		}
+	// 	}()
 
-		// Wait for the command to finish
-		if err := cmd.Wait(); err != nil {
-			fmt.Println("[agent.go] Error waiting for command:", err)
-			return
-		}
+	// 	// Wait for the command to finish
+	// 	if err := cmd.Wait(); err != nil {
+	// 		fmt.Println("[agent.go] Error waiting for command:", err)
+	// 		return
+	// 	}
 
-		fmt.Println("[agent.go] Command finished")
-	}()
+	// 	fmt.Println("[agent.go] Command finished")
+	// }()
 
 	go func() {
 		for {
@@ -128,45 +127,59 @@ func main() {
 				log.Printf("[agent.go] Working Dir %s", workingDir)
 				log.Printf("[agent.go] Libraries %s", libraries)
 
-				// bash script to
-				// (a) create the virtual environment,
-				// (b) source it, and
-				// (c) run a python code
-				bashScript := `
-        workingDir="%s";
-        cd $workingDir;
-        if [ ! -f "$workingDir/venv/pyenv.cfg" ]; then
-          rm -rf $workingDir/venv;
-          python3 -m venv $workingDir/venv;
-        fi
-        source $workingDir/venv/bin/activate
-        pip install %s > /dev/null
-        python -c "%s"
-        `
-
-				runCmd := fmt.Sprintf(
-					bashScript,
-					workingDir,
-					strings.Join(libraries, " "),
-					strings.ReplaceAll(code, `"`, `\"`),
-				)
-				log.Printf("[agent.go] Running bash script:\n%s", runCmd)
-				cmd := exec.Command("bash", "-c", runCmd)
-
 				go func() {
-					output, err := cmd.Output()
-					if err != nil {
-						fmt.Println("[agent.go] Failed to run python command:", err)
+
+					// setup the venv
+					venvCmd := fmt.Sprintf(`
+					agentId="%s"
+					pkgs="%s"
+
+					if [ ! -f "/tmp/$agentId/venv" ]; then
+						mkdir -p /tmp/$agentId
+						python3 -m venv /tmp/$agentId/venv
+					fi
+
+					source /tmp/$agentId/venv/bin/activate
+					python3 -m pip install $pkgs
+					
+					`, agentId, strings.Join(libraries, " "))
+					log.Println("[agent.go] venv setup:", venvCmd)
+					venvExc := exec.Command("bash", "-c", venvCmd)
+					venvOut, venvErr := venvExc.CombinedOutput()
+					if venvErr != nil {
+						fmt.Println("[agent.go] venv setup: ERR", venvErr)
 						return
 					}
-					stdoutString := string(output)
-					log.Printf("[agent.go] Execution output is %s", stdoutString)
+					venvStdout := string(venvOut)
+					fmt.Println("[agent.go] venv setup:", venvStdout)
+
+					// execute the python code
+					pyCmd := fmt.Sprintf(`
+					workingDir="%s";
+					agentId="%s";
+
+					cd $workingDir;
+					source /tmp/$agentId/venv/bin/activate;
+					python3 <<EOF
+%s
+EOF`, workingDir, agentId, code)
+					log.Println("[agent.go] python code:", pyCmd)
+					pyExc := exec.Command("bash", "-c", pyCmd)
+					pyOut, pyErr := pyExc.CombinedOutput()
+					if pyErr != nil {
+						fmt.Println("[agent.go] python code: ERR", pyErr)
+					}
+
+					// send the result back to the server
+					pyStdout := string(pyOut)
 					if err := stream.Send(&protos.AgentMessage{Message: &protos.AgentMessage_PythonExecutionResponse{
 						PythonExecutionResponse: &protos.PythonExecutionResponse{
 							SessionId:      sessionId,
 							ExecutionId:    executionId,
-							ResponseString: stdoutString}}}); err != nil {
+							ResponseString: pyStdout}}}); err != nil {
 						log.Printf("[agent.go] Failed to send execution result to server: %v", err)
+					} else {
+						log.Printf("[agent.go] Sent execution result to the server: %v", pyStdout)
 					}
 				}()
 
@@ -177,17 +190,16 @@ func main() {
 				log.Printf("[agent.go] Execution id %s", executionId)
 				cmd := exec.Command(execArgs[0], execArgs[1:]...)
 				log.Printf("[agent.go] Completed execution with the id %s", executionId)
-				stdout, err := cmd.Output()
+				output, err := cmd.CombinedOutput() // combined output of stdout and stderr
 				if err != nil {
-					log.Fatalf(err.Error())
-					return
+					log.Printf("[agent.go] command execution failed: %s", err)
 				}
 
-				stdoutString := string(stdout)
-				log.Printf("[agent.go] Execution output is %s", stdoutString)
+				outputString := string(output)
+				log.Printf("[agent.go] Execution output is %s", outputString)
 
 				if err := stream.Send(&protos.AgentMessage{Message: &protos.AgentMessage_CommandExecutionResponse{
-					CommandExecutionResponse: &protos.CommandExecutionResponse{ExecutionId: executionId, ResponseString: stdoutString}}}); err != nil {
+					CommandExecutionResponse: &protos.CommandExecutionResponse{ExecutionId: executionId, ResponseString: outputString}}}); err != nil {
 					log.Printf("[agent.go] Failed to send execution result to server: %v", err)
 				}
 
