03c1a875d21dde705cf97712d04b35394e6404ca
yasith <yasith.j@icloud.com>
2025-02-20 22:11:40 -0600

    pretty-printing of experiment plans

--- airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/__init__.py
+++ airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/__init__.py
@@ -33,7 +33,7 @@ def display_runtimes(runtimes: list[Runtime]):
     record = dict(id=runtime.id, **runtime.args)
     records.append(record)
   
-  return pd.DataFrame(records)
+  return pd.DataFrame(records).set_index("id")
 
 def display_experiments(experiments: list[base.Experiment]):
   """
@@ -48,23 +48,51 @@ def display_experiments(experiments: list[base.Experiment]):
       record[k] = ", ".join(v) if isinstance(v, list) else str(v)
     records.append(record)
   
-  return pd.DataFrame(records)
+  return pd.DataFrame(records).set_index("name")
 
 def display_plans(plans: list[plan.Plan]):
   """
   Display plans in a tabular format
   """
-  import pandas as pd
+
+  from IPython.display import HTML
   
-  records = []
+  html = """
+  <table border='1'>
+      <tr>
+        <th><b>Plan ID</b></th>
+        <th><b>Status</b></th>
+        <th><b>Name</b></th>
+        <th><b>App</b></th>
+        <th><b>Inputs</b></th>
+        <th><b>Cluster</b></th>
+        <th><b>Group</b></th>
+        <th><b>Category</b></th>
+        <th><b>Queue</b></th>
+        <th><b>Nodes</b></th>
+        <th><b>CPUs</b></th>
+        <th><b>GPUs</b></th>
+        <th><b>Time</b></th>
+      </tr>
+  """
+
   for plan in plans:
     for task in plan.tasks:
-      record = dict(plan_id=str(plan.id))
-      for k, v in task.model_dump().items():
-        record[k] = ", ".join(v) if isinstance(v, list) else str(v)
-      records.append(record)
+      html += generate_task_row(plan.id or "N/A", task)
+    html += "<tr></tr>"
   
-  return pd.DataFrame(records)
+  html += "</table>"
+
+  script = """
+  <script type="text/javascript">
+      document.querySelectorAll('.hover-container').forEach(item => {
+          item.addEventListener('mouseover', () => item.querySelector('.hover-content').style.visibility = 'visible');
+          item.addEventListener('mouseout', () => item.querySelector('.hover-content').style.visibility = 'hidden');
+      });
+  </script>
+  """
+
+  return HTML(html + script)
 
 def display(arg):
   
@@ -84,3 +112,46 @@ def display(arg):
       return display_plans([arg])
   
   raise NotImplementedError(f"Cannot display object of type {type(arg)}")
+
+
+def generate_inputs_table(inputs: dict):
+
+  html = """
+  <table border='1'>
+    <tr><th><b>Input</b></th><th><b>Type</b></th><th><b>Value</b></th></tr>
+  """
+  for k, v in inputs.items():
+    html += f"""<tr><th>{k}</th><th>{v.get("type")}</th><th>{v.get("value")}</th></tr>"""
+  html += "</table>"
+  return html
+
+
+def generate_task_row(plan_id: str, task: plan.Task):
+  """
+  Generate a row for the task
+  """
+
+  return f"""
+  <tr>
+      <td>{plan_id[:8]}...</td>
+      <td>{task.status()}</td>
+      <td>{task.name}</td>
+      <td>{task.app_id}</td>
+      <td>
+          <div style="position:relative;" class="hover-container">
+              [Hover]
+              <div class="hover-content" style="visibility:hidden; position:absolute; top:20px; left:0; background:white; border:1px solid black; padding:5px; z-index:10;">
+                {generate_inputs_table(task.inputs)}
+              </div>
+          </div>
+      </td>
+      <td>{task.runtime.args.get("cluster", "N/A")}</td>
+      <td>{task.runtime.args.get("group", "N/A")}</td>
+      <td>{task.runtime.args.get("category", "N/A")}</td>
+      <td>{task.runtime.args.get("queue_name", "N/A")}</td>
+      <td>{task.runtime.args.get("node_count", "N/A")}</td>
+      <td>{task.runtime.args.get("cpu_count", "N/A")}</td>
+      <td>{task.runtime.args.get("gpu_count", "N/A")}</td>
+      <td>{task.runtime.args.get("walltime", "N/A")}</td>
+  </tr>
+  """
