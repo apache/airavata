0059c71fe8f988fce5a2ec15adf427c205b14456
yasith <yasith.j@icloud.com>
2025-02-20 18:53:33 -0600

    choose group/project from API, silent plan launch, add_replica() -> create_task(), display task state, planid and taskid, improve messaging

--- airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/__init__.py
+++ airavata-api/airavata-client-sdks/airavata-python-sdk/airavata_experiments/__init__.py
@@ -19,10 +19,12 @@ from __future__ import annotations
 from . import base, plan
 from .auth import login, logout
 from .runtime import list_runtimes, Runtime
+from typing import Any
 
 __all__ = ["login", "logout", "list_runtimes", "base", "plan"]
 
-def display_runtimes(runtimes: list[Runtime]):
+
+def display_runtimes(runtimes: list[Runtime]) -> None:
   """
   Display runtimes in a tabular format
   """
@@ -33,9 +35,11 @@ def display_runtimes(runtimes: list[Runtime]):
     record = dict(id=runtime.id, **runtime.args)
     records.append(record)
   
-  return pd.DataFrame(records)
+  d = get_display_fn()
+  d(pd.DataFrame(records).set_index("id"))
 
-def display_experiments(experiments: list[base.Experiment]):
+
+def display_experiments(experiments: list[base.Experiment]) -> None:
   """
   Display experiments in a tabular format
   """
@@ -48,23 +52,113 @@ def display_experiments(experiments: list[base.Experiment]):
       record[k] = ", ".join(v) if isinstance(v, list) else str(v)
     records.append(record)
   
-  return pd.DataFrame(records)
+  d = get_display_fn()
+  d(pd.DataFrame(records).set_index("name"))
 
-def display_plans(plans: list[plan.Plan]):
+
+def display_plans(plans: list[plan.Plan]) -> None:
   """
   Display plans in a tabular format
   """
-  import pandas as pd
+
+  from IPython.display import HTML
   
-  records = []
+  html = """
+  <table border='1'>
+      <tr>
+        <th><b>Plan Id</b></th>
+        <th><b>Task Id</b></th>
+        <th><b>State</b></th>
+        <th><b>Name</b></th>
+        <th><b>App</b></th>
+        <th><b>Inputs</b></th>
+        <th><b>Cluster</b></th>
+        <th><b>Group</b></th>
+        <th><b>Category</b></th>
+        <th><b>Queue</b></th>
+        <th><b>Nodes</b></th>
+        <th><b>CPUs</b></th>
+        <th><b>GPUs</b></th>
+        <th><b>Time</b></th>
+      </tr>
+  """
+
   for plan in plans:
     for task in plan.tasks:
-      record = dict(plan_id=str(plan.id))
-      for k, v in task.model_dump().items():
-        record[k] = ", ".join(v) if isinstance(v, list) else str(v)
-      records.append(record)
+      html += generate_task_row(plan.id or "N/A", task)
+    html += "<tr></tr>"
   
-  return pd.DataFrame(records)
+  html += "</table>"
+
+  script = """
+  <script type="text/javascript">
+      document.querySelectorAll('.hover-container').forEach(item => {
+          item.addEventListener('mouseover', () => item.querySelector('.hover-content').style.visibility = 'visible');
+          item.addEventListener('mouseout', () => item.querySelector('.hover-content').style.visibility = 'hidden');
+      });
+  </script>
+  """
+
+  d = get_display_fn()
+  d(HTML(html + script))
+
+
+def generate_task_row(plan_id: str, task: plan.Task):
+  """
+  Generate a row for the task
+  """
+
+  task_id, task_state = task.status() if task.ref else ("N/A", "N/A")
+
+  return f"""
+  <tr>
+      <td>{plan_id[:8]}...</td>
+      <td>{task_id}</td>
+      <td>{task_state}</td>
+      <td>{task.name}</td>
+      <td>{task.app_id}</td>
+      <td>
+          <div style="position:relative;" class="hover-container">
+              [Hover]
+              <div class="hover-content" style="visibility:hidden; position:absolute; top:20px; left:0; background:white; border:1px solid black; padding:5px; z-index:10;">
+                {generate_inputs_table(task.inputs)}
+              </div>
+          </div>
+      </td>
+      <td>{task.runtime.args.get("cluster", "N/A")}</td>
+      <td>{task.runtime.args.get("group", "N/A")}</td>
+      <td>{task.runtime.args.get("category", "N/A")}</td>
+      <td>{task.runtime.args.get("queue_name", "N/A")}</td>
+      <td>{task.runtime.args.get("node_count", "N/A")}</td>
+      <td>{task.runtime.args.get("cpu_count", "N/A")}</td>
+      <td>{task.runtime.args.get("gpu_count", "N/A")}</td>
+      <td>{task.runtime.args.get("walltime", "N/A")}</td>
+  </tr>
+  """
+
+
+def generate_inputs_table(inputs: dict):
+
+  html = """
+  <table border='1'>
+    <tr><th><b>Input</b></th><th><b>Type</b></th><th><b>Value</b></th></tr>
+  """
+  for k, v in inputs.items():
+    html += f"""<tr><th>{k}</th><th>{v.get("type")}</th><th>{v.get("value")}</th></tr>"""
+  html += "</table>"
+  return html
+
+def get_display_fn() -> Any:
+  try:
+    from IPython.core.getipython import get_ipython
+    from IPython.display import display as d
+    if get_ipython() is not None and d is not None:
+      return d
+    else:
+      raise Exception("Not in IPython environment")
+  except:
+    return print
+
 
 def display(arg):
   
