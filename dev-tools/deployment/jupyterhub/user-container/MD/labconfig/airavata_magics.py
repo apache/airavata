1baf653252af8346c383ab18f19c8c7844415974
yasith <yasith.j@icloud.com>
2025-02-25 15:59:00 -0600

    add airavata magics to dev-tools/, make airavata-agent/ build generic

--- dev-tools/deployment/jupyterhub/user-container/MD/labconfig/airavata_magics.py
+++ dev-tools/deployment/jupyterhub/user-container/MD/labconfig/airavata_magics.py
@@ -1,62 +1,76 @@
-from IPython.core.magic import register_cell_magic
-from IPython.core.magic import register_line_magic
-
-from IPython.display import display, Image, HTML
 import base64
-import requests
+import binascii
 import json
-import jwt
 import time
 from pathlib import Path
+from typing import NamedTuple
+import jwt
 import os
+import requests
+from IPython.core.magic import register_cell_magic, register_line_magic
+from IPython.display import HTML, Image, display
 from device_auth import DeviceFlowAuthenticator
 
 
-current_agent_info = None
+AgentInfo = NamedTuple('AgentInfo', [
+    ('agentId', str),
+    ('experimentId', str),
+    ('processId', str),
+    ('cluster', str),
+    ('queue', str),
+    ('cpus', int),
+    ('memory', int),
+    ('walltime', int),
+    ('gateway_id', str),
+    ('group', str),
+])
+api_base_url = "https://api.gateway.cybershuttle.org"
+file_server_url = "http://3.142.234.94:8050"
+current_agent : AgentInfo | None = None
+MSG_NOT_INITIALIZED = r"Remote agent not initialized. Please run %init_remote cluster=<cluster> cpu=<cpu> memory=<memory mb> queue=<queue> walltime=<walltime minutes> group=<group>"
 
-EXPLICIT_TOKEN_FILE = (
-        Path(os.path.expanduser("~")) / "csagent" / "token" / "keys.json"
-)
 
-def read_explicit_token_file():
-    if not EXPLICIT_TOKEN_FILE.exists():
-        return None
-    else:
-        with open(EXPLICIT_TOKEN_FILE, "r") as f:
-            return json.load(f)
-
-def get_access_token():
-
+def get_access_token() -> str | None:
     token_from_env = os.getenv('CS_ACCESS_TOKEN')
     if token_from_env:
         return token_from_env
+    EXPLICIT_TOKEN_FILE = Path("~").expanduser() / "csagent" / "token" / "keys.json"
+    if EXPLICIT_TOKEN_FILE.exists():
+        with open(EXPLICIT_TOKEN_FILE, "r") as f:
+            return json.load(f).get("access_token")
 
-    expl_token_data = read_explicit_token_file()
-    if expl_token_data:
-        return expl_token_data["access_token"]
 
-def get_agent_status():
-    global current_agent_info
-
-    if not current_agent_info:
-        print("No agent was scheduled yet. Please run %init_remote cluster=<cluster> cpu=<cpu> memory=<memory mb> queue=<queue> walltime=<walltime minutes>")
-        return
-
-    url = 'https://api.gateway.cybershuttle.org/api/v1/agent/' + current_agent_info['agentId']
+def get_agent_status() -> dict | None:
+    if not current_agent:
+        return print(MSG_NOT_INITIALIZED)
+    url = f"{api_base_url}/api/v1/agent/{current_agent.agentId}"
     response = requests.get(url)
     if response.status_code == 202:
-        print("Got response for agent")
-        print(response.json())
         return response.json()
-    else:
-        print('Invalid response reveived. Status code:', response.status_code)
-        print('Response:', response.text)
+    return print(f"Got [{response.status_code}] Response: {response.text}")
 
-def submit_agent_job(experiment_name, cluster, queue, cpus, memory, wallTime, access_token, gateway_id='default'):
 
-    global current_agent_info
+def get_process_id(experiment_id: str, headers) -> str:
+    """
+    Get process id by experiment id
+
+    """
+    url = f"{api_base_url}/api/v1/exp/{experiment_id}/process"
+    process_id = ""
+    while not process_id:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            process_id = response.json().get("processId")
+        else:
+            time.sleep(5)
+    return process_id
+
+
+def submit_agent_job(experiment_name, cluster, queue, cpus, memory, walltime, access_token, group, gateway_id='default'):
+    global current_agent
+
     # URL to which the POST request will be sent
-    url = 'https://api.gateway.cybershuttle.org/api/v1/exp/launch'
+    url = api_base_url + '/api/v1/exp/launch'
 
     # Data to be sent in the POST request
     data = {
@@ -65,8 +79,9 @@ def submit_agent_job(experiment_name, cluster, queue, cpus, memory, wallTime, ac
         'cpuCount': cpus,
         'nodeCount': 1,
         'memory': memory,
-        'wallTime': wallTime,
-        'queue': queue
+        'wallTime': walltime,
+        'queue': queue,
+        'group': group,
     }
 
     # Convert the data to JSON format
@@ -92,19 +107,32 @@ def submit_agent_job(experiment_name, cluster, queue, cpus, memory, wallTime, ac
     # Check if the request was successful
     if response.status_code == 200:
         # Parse the JSON response
-        response_data = response.json()
-        print('Response:', response_data)
-        current_agent_info = response_data
+        obj = response.json()
+        current_agent = AgentInfo(
+            agentId=obj['agentId'],
+            experimentId=obj['experimentId'],
+            processId=get_process_id(obj['experimentId'], headers=headers),
+            cluster=cluster,
+            queue=queue,
+            cpus=cpus,
+            memory=memory,
+            walltime=walltime,
+            gateway_id=gateway_id,
+            group=group,
+        )
+        print('Agent Initialized:', current_agent)
     else:
         print('Failed to send POST request. Status code:', response.status_code)
         print('Response:', response.text)
 
+
 def terminate_agent(access_token, gateway_id='default'):
+    global current_agent
+    if not current_agent:
+        return print(MSG_NOT_INITIALIZED)
 
-    global current_agent_info
-
-    expId = current_agent_info['experimentId']
-    url = 'https://api.gateway.cybershuttle.org/api/v1/exp/terminate/' + expId
+    expId = current_agent.experimentId
+    url = api_base_url + '/api/v1/exp/terminate/' + expId
 
     decode = jwt.decode(access_token, options={"verify_signature": False})
     user_id = decode['preferred_username']
@@ -127,7 +155,7 @@ def terminate_agent(access_token, gateway_id='default'):
         # Parse the JSON response
         response_data = response.json()
         print('Agent terminated:', response_data)
-        current_agent_info = None
+        current_agent = None
     else:
         print('Failed to send termination request. Status code:', response.status_code)
         print('Response:', response.text)
@@ -135,19 +163,17 @@ def terminate_agent(access_token, gateway_id='default'):
 
 @register_cell_magic
 def run_remote(line, cell):
+    global current_agent
+    if not current_agent:
+        return print(MSG_NOT_INITIALIZED)
 
-    global current_agent_info
-    if not current_agent_info:
-        print("No agent was scheduled yet. Please run %init_remote cluster=<cluster> cpu=<cpu> memory=<memory mb> queue=<queue> walltime=<walltime minutes>")
-        return
-
-    url = 'https://api.gateway.cybershuttle.org/api/v1/agent/executejupyterrequest'
+    url = api_base_url + '/api/v1/agent/executejupyterrequest'
 
     data = {
         "sessionId": "session1",
         "keepAlive": True,
         "code": cell,
-        "agentId": current_agent_info["agentId"]
+        "agentId": current_agent.agentId
     }
 
     json_data = json.dumps(data)
@@ -159,7 +185,7 @@ def run_remote(line, cell):
         print("Cell execution failed. Error: " + error)
     if execution_id:
         while True:
-            url = "https://api.gateway.cybershuttle.org/api/v1/agent/executejupyterresponse/" + execution_id
+            url = api_base_url + "/api/v1/agent/executejupyterresponse/" + execution_id
             response = requests.get(url, headers={'Accept': 'application/json'})
             json_response = response.json()
             if json_response.get('available'):
@@ -180,7 +206,7 @@ def run_remote(line, cell):
                                 try:
                                     image_bytes = base64.b64decode(image_data)
                                     display(Image(data=image_bytes, format='png'))
-                                except base64.binascii.Error as e:
+                                except binascii.Error as e:
                                     print(f"Failed to decode image data: {e}")
                             # Ignoring any texts in the display data
                             # if 'text/plain' in data_obj:
@@ -195,7 +221,7 @@ def run_remote(line, cell):
                                     color: #a71d5d;
                                     background-color: #fdd;
                                     border: 1px solid #a71d5d;
-                                    padding: 10px;
+                                    padding: 5px;
                                     border-radius: 5px;
                                     font-family: Consolas, 'Courier New', monospace;
                                     white-space: pre-wrap;
@@ -217,7 +243,7 @@ def run_remote(line, cell):
                                 color: #a71d5d;
                                 background-color: #fdd;
                                 border: 1px solid #a71d5d;
-                                padding: 10px;
+                                padding: 5px;
                                 border-radius: 5px;
                                 font-family: Consolas, 'Courier New', monospace;
                             ">
@@ -246,10 +272,11 @@ def run_remote(line, cell):
                             try:
                                 image_bytes = base64.b64decode(image_data)
                                 display(Image(data=image_bytes, format='png'))
-                            except base64.binascii.Error as e:
+                            except binascii.Error as e:
                                 print(f"Failed to decode image data: {e}")
                 break
             time.sleep(1)
+
 
 @register_line_magic
 def cs_login(line):
@@ -259,10 +286,10 @@ def cs_login(line):
     except ValueError as e:
         print(f"Configuration error: {e}")
 
+
 @register_line_magic
 def init_remote(line):
-
-    if current_agent_info:
+    if current_agent:
         status = get_agent_status()
         if status:
             if status['agentUp']:
@@ -281,6 +308,7 @@ def init_remote(line):
     cpu_value = None
     queue_value = None
     walltime_value = None
+    group_value = ""
 
     # Iterate through the pairs to find the cluster value
     for pair in pairs:
@@ -294,8 +322,11 @@ def init_remote(line):
             queue_value = pair.split("=")[1]
         if pair.startswith("walltime="):
             walltime_value = pair.split("=")[1]
+        if pair.startswith("group="):
+            group_value = pair.split("=")[1]
 
-    submit_agent_job('CS_Agent', cluster_value, queue_value, cpu_value, memory_value, walltime_value, access_token)
+    submit_agent_job('CS_Agent', cluster_value, queue_value, cpu_value, memory_value, walltime_value, access_token, group_value)
+
 
 @register_line_magic
 def status_remote(line):
@@ -304,19 +335,70 @@ def status_remote(line):
         if status['agentUp']:
             print("Agent", status['agentId'], 'is running')
         else:
-            print("Agent", status['agentId'], 'is still prepairing. Please wait')
+            print("Agent", status['agentId'], 'is still preparing. Please wait')
+
 
 @register_line_magic
 def terminate_remote(line):
-    global current_agent_info
+    global current_agent
     access_token = get_access_token()
-    if current_agent_info:
+    if current_agent:
         terminate_agent(access_token)
 
 
+@register_line_magic
+def push_remote(line):
+    if not current_agent:
+        return print(MSG_NOT_INITIALIZED)
+    pairs = line.split()
+    remot_path = None
+    local_path = None
+    for pair in pairs:
+        if pair.startswith("source="):
+            local_path = pair.split("=")[1]
+        if pair.startswith("target="):
+            remot_path = pair.split("=")[1]
+    # validate paths
+    if not remot_path or not local_path:
+        return print("Please provide paths for both source and target")
+    # upload file
+    print(f"Pushing local:{local_path} to remote:{remot_path}")
+    url = f"{file_server_url}/upload/live/{current_agent.processId}/{remot_path}"
+    with open(local_path, "rb") as file:
+        files = {"file": file}
+        response = requests.post(url, files=files)
+    print(f"[{response.status_code}] Uploaded local:{local_path} to remote:{remot_path}")
+
+
+@register_line_magic
+def pull_remote(line):
+    if not current_agent:
+        return print(MSG_NOT_INITIALIZED)
+    pairs = line.split()
+    remot_path = None
+    local_path = None
+    for pair in pairs:
+        if pair.startswith("source="):
+            remot_path = pair.split("=")[1]
+        if pair.startswith("target="):
+            local_path = pair.split("=")[1]
+    # validate paths
+    if not remot_path or not local_path:
+        return print("Please provide paths for both source and target")
+    # download file
+    print(f"Pulling remote:{remot_path} to local:{local_path}")
+    url = f"{file_server_url}/download/live/{current_agent.processId}/{remot_path}"
+    response = requests.get(url)
+    with open(local_path, "wb") as file:
+        file.write(response.content)
+    print(f"[{response.status_code}] Downloaded remote:{remot_path} to local:{local_path}")
+
+
 def load_ipython_extension(ipython):
+    ipython.register_magic_function(cs_login)
     ipython.register_magic_function(init_remote)
     ipython.register_magic_function(status_remote)
     ipython.register_magic_function(terminate_remote)
     ipython.register_magic_function(run_remote)
-    ipython.register_magic_function(cs_login)
+    ipython.register_magic_function(push_remote)
+    ipython.register_magic_function(pull_remote)
