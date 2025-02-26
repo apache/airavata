bf5f68f4f83613ad3bb5c32b7b1c8df78af7cd35
yasith <yasith.j@icloud.com>
2025-02-25 23:28:36 -0600

    reorganize agent-framework related code. make airavata-magics a python package.

--- dev-tools/deployment/jupyterhub/user-container/MD/labconfig/bootstrap.sh
+++
@@ -1,2 +0,0 @@
-cd /home/jupyter-notebook-examples && git pull && cd -
-jupyter lab --config=/jupyter_lab_config.py --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=''
--- modules/agent-framework/airavata-agent/jupyter/deployments/i-guide/agent/build-container.sh
+++
@@ -1,6 +0,0 @@
-cd ../../../../
-env GOOS=linux GOARCH=amd64 go build
-cp airavata-agent jupyter/deployments/i-guide/agent/airavata-agent-linux
-cd jupyter/deployments/i-guide/agent
-docker build --platform linux/x86_64 -t cybershuttle/airavata-iguide-agent .
-docker push cybershuttle/airavata-iguide-agent
