494c742bf0dbf4be4d26a9be156823c34fcacc10
yasith <yasith@gatech.edu>
2025-03-17 06:48:34 +0000

    bump all jdk deps to 17. get devcontainer to working state. add launch.json for vscode integration. add openjpa runtimeoptimization tag to persistence.xml. bump mysql connector to 8.0.31. add dns tags to keystore generation script (browser https compatibility), move keystores to top-level and add build-time copy to project resources. fix Serializable warning in UserPK.java

--- keystores/generate_keystore.sh
+++ keystores/generate_keystore.sh
@@ -5,7 +5,8 @@ rm -f airavata.jks client_truststore.jks
 
 # Generate a PKCS12 keystore with a self-signed certificate
 keytool -genkey -keyalg RSA -alias selfsigned -keystore airavata.jks -storetype pkcs12 -storepass airavata -validity 360 -keysize 2048 \
-  -dname "CN=airavata.host, OU=airavata.host, O=airavata.host, L=airavata.host, ST=airavata.host, C=airavata.host"
+  -dname "CN=airavata.host, OU=airavata.host, O=airavata.host, L=airavata.host, ST=airavata.host, C=airavata.host" \
+  -ext san=dns:airavata.host
 
 # Also generate a JKS keystore with that certificate (for backward-compatibility)
 keytool -importkeystore -noprompt \
