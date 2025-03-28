cca00762dc1ffce292f5ee5f62f0c5e4c0a78424
lahiruj <lahirujayathilake@gmail.com>
2025-03-28 11:29:47 -0400

    initial research framework

---
+++ modules/research-framework/research-service/src/main/java/org/apache/airavata/research/ResearchServiceApplication.java
@@ -0,0 +1,13 @@
+package org.apache.airavata.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories()
+public class ResearchServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchServiceApplication.class, args);
+    }
+}
