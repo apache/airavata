93509da3a7c8b3ae4929f8c8e0dfceae9d8213e3
lahiruj <lahirujayathilake@gmail.com>
2025-03-31 04:00:20 -0400

    updated the notebook and docker files

--- dev-tools/deployment/jupyterhub/user-container/MD/build-container.sh
+++ dev-tools/deployment/jupyterhub/user-container/MD/build-container.sh
@@ -1,2 +1,2 @@
-docker build --platform linux/x86_64 -t cybershuttle/jupyterlab-base .
-docker push cybershuttle/jupyterlab-base
+docker build --platform linux/x86_64 -t cybershuttle/jupyterlab-base-sample .
+docker push cybershuttle/jupyterlab-base-sample
--- dev-tools/deployment/jupyterhub/user-container/MD/init.sh
+++ dev-tools/deployment/jupyterhub/user-container/MD/init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 TARGET_DIR="/home/jovyan/work"
-SHARED_TMP="/home/jovyan/shared_data_tmp"
-
 
 mkdir -p "$TARGET_DIR"
 
@@ -14,14 +12,6 @@ if [ ! -f "$TARGET_DIR/.initialized" ]; then
     chown -R jovyan:users "$TARGET_DIR"
 else
     echo "Docker default files already exist, skipping copy."
-fi
-
-
-if [ -d "$SHARED_TMP" ]; then
-    echo "Creating symlinks for shared read-only data inside $TARGET_DIR..."
-    for item in "$SHARED_TMP"/*; do
-        ln -s "$item" "$TARGET_DIR/$(basename "$item")"
-    done
 fi
 
 exec "$@"
