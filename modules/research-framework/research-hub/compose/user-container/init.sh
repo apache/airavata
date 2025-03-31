94e814e84338b531caf4fe7d74cbc673c907e4ea
lahiruj <lahirujayathilake@gmail.com>
2025-03-31 03:51:29 -0400

    jupyterhub instance to show sample notebooks

--- modules/research-framework/research-hub/compose/user-container/init.sh
+++ modules/research-framework/research-hub/compose/user-container/init.sh
@@ -7,7 +7,6 @@ mkdir -p "$TARGET_DIR"
 
 if [ ! -f "$TARGET_DIR/.initialized" ]; then
     echo "Copying default Docker files into the mounted workspace..."
-#    cp -r /tmp/default_data/. "$TARGET_DIR/"
     touch "$TARGET_DIR/.initialized"
     chown -R jovyan:users "$TARGET_DIR"
 
