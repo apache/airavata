1c6b5051324e099e5780a8fc66819a1f43582612
ganning127 <ganningxu@gmail.com>
2025-04-05 03:33:04 -0400

    Homepage + redirecting logins

--- modules/research-framework/portal/src/components/auth/UserLoginPage.tsx
+++ modules/research-framework/portal/src/components/auth/UserLoginPage.tsx
@@ -14,6 +14,9 @@ export const Login = () => {
     }
   }, [auth]);
 
+  const redirect =
+    new URLSearchParams(window.location.search).get("redirect") || "/";
+
   return (
     <Center height="100vh">
       <Stack
@@ -40,6 +43,7 @@ export const Login = () => {
               console.log("Login button clicked");
               setLoading(true);
               auth.signinRedirect({
+                redirect_uri: `${window.location.origin}${redirect}`,
                 extraQueryParams: {
                   prompt: "login",
                   kc_idp_hint: "oidc",
