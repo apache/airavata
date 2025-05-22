0b5cf3a5238a409cd72dbe717b6b9406976ddb21
Lahiru Jayathilake <lahirujayathilake@gmail.com>
2025-05-22 18:47:20 +0530

    research framework formatting and included missing license headers

--- modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/controller/ResourceController.java
+++ modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/controller/ResourceController.java
@@ -1,56 +1,59 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
+ * with the License. You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
 package org.apache.airavata.research.service.controller;
 
-import io.swagger.v3.oas.annotations.tags.Tags;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.airavata.research.service.dto.CreateResourceRequest;
 import org.apache.airavata.research.service.dto.ModifyResourceRequest;
 import org.apache.airavata.research.service.dto.ResourceResponse;
 import org.apache.airavata.research.service.enums.ResourceTypeEnum;
-import org.apache.airavata.research.service.enums.StatusEnum;
 import org.apache.airavata.research.service.handlers.ProjectHandler;
-import org.apache.airavata.research.service.model.UserContext;
-import org.apache.airavata.research.service.model.entity.*;
+import org.apache.airavata.research.service.handlers.ResourceHandler;
+import org.apache.airavata.research.service.model.entity.DatasetResource;
+import org.apache.airavata.research.service.model.entity.ModelResource;
+import org.apache.airavata.research.service.model.entity.NotebookResource;
+import org.apache.airavata.research.service.model.entity.Project;
+import org.apache.airavata.research.service.model.entity.RepositoryResource;
+import org.apache.airavata.research.service.model.entity.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.*;
-
-import org.apache.airavata.research.service.handlers.ResourceHandler;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/rf/resources")
 @Tag(name = "Resources", description = "Datasets, notebooks, repositories, models")
 public class ResourceController {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ResourceController.class);
 
     @org.springframework.beans.factory.annotation.Autowired
@@ -71,7 +74,7 @@ public class ResourceController {
     }
 
     @PostMapping("/repository")
-    public ResponseEntity<ResourceResponse> createRepositoryResource(@RequestBody CreateResourceRequest resourceRequest, @RequestParam(value="githubUrl") String repositoryUrl) {
+    public ResponseEntity<ResourceResponse> createRepositoryResource(@RequestBody CreateResourceRequest resourceRequest, @RequestParam(value = "githubUrl") String repositoryUrl) {
         ResourceResponse response = resourceHandler.createRepositoryResource(resourceRequest, repositoryUrl);
         return ResponseEntity.ok(response);
     }
@@ -100,7 +103,7 @@ public class ResourceController {
             summary = "Get dataset, notebook, or repository"
     )
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Resource> getResource(@PathVariable(value="id") String id) {
+    public ResponseEntity<Resource> getResource(@PathVariable(value = "id") String id) {
         return ResponseEntity.ok(resourceHandler.getResourceById(id));
     }
 
@@ -109,10 +112,10 @@ public class ResourceController {
     )
     @GetMapping("/")
     public ResponseEntity<Page<Resource>> getAllResources(
-            @RequestParam(value="pageNumber", defaultValue = "0") int pageNumber,
-            @RequestParam(value="pageSize", defaultValue = "10") int pageSize,
-            @RequestParam(value="type") ResourceTypeEnum[] types,
-            @RequestParam(value="tag", required = false) String[] tags
+            @RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+            @RequestParam(value = "type") ResourceTypeEnum[] types,
+            @RequestParam(value = "tag", required = false) String[] tags
     ) {
         List<Class<? extends Resource>> typeList = new ArrayList<>();
         for (ResourceTypeEnum resourceType : types) {
@@ -136,8 +139,8 @@ public class ResourceController {
     )
     @GetMapping("/search")
     public ResponseEntity<List<Resource>> searchResource(
-            @RequestParam(value="type") ResourceTypeEnum type,
-            @RequestParam(value="name", required = false) String name
+            @RequestParam(value = "type") ResourceTypeEnum type,
+            @RequestParam(value = "name", required = false) String name
     ) {
 
         List<Resource> resources = resourceHandler.getAllResourcesByTypeAndName(getResourceType(type), name);
@@ -148,7 +151,7 @@ public class ResourceController {
             summary = "Get projects associated with a resource"
     )
     @GetMapping(value = "/{id}/projects")
-    public ResponseEntity<List<Project>> getProjectsFromResourceId(@PathVariable(value="id") String id) {
+    public ResponseEntity<List<Project>> getProjectsFromResourceId(@PathVariable(value = "id") String id) {
         Resource resouce = resourceHandler.getResourceById(id);
         List<Project> projects;
         if (resouce.getClass() == RepositoryResource.class) {
