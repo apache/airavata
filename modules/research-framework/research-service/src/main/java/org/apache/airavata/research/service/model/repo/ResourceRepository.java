6c0942a4912d89164899c6bac4db52cc950b0fc0
ganning127 <ganningxu@gmail.com>
2025-06-14 01:27:05 -0700

    formatting

--- modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/model/repo/ResourceRepository.java
+++ modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/model/repo/ResourceRepository.java
@@ -1,25 +1,23 @@
 /**
-*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements. See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership. The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License. You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.airavata.research.service.model.repo;
 
-import java.util.List;
 import org.apache.airavata.research.service.model.entity.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,34 +26,49 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, String> {
 
-    @Query("SELECT r FROM #{#entityName} r WHERE TYPE(r) IN :types")
-    Page<Resource> findAllByTypes(@Param("types") List<Class<? extends Resource>> types, Pageable pageable);
+    @Query("""
+            SELECT r 
+            FROM #{#entityName} r 
+            WHERE TYPE(r) IN :types AND r.name LIKE CONCAT('%', :nameSearch, '%')
+            ORDER BY r.name
+            """)
+    Page<Resource> findAllByTypes(
+            @Param("types") List<Class<? extends Resource>> types,
+            @Param("nameSearch") String nameSearch,
+            Pageable pageable);
 
-    @Query(
-            """
-    SELECT r
-    FROM Resource r
-    JOIN r.tags t
-    WHERE r.class IN :typeList AND t.value IN :tags
-    GROUP BY r
-    HAVING COUNT(DISTINCT t.value) = :tagCount
-    """)
+
+    @Query("""
+            SELECT r
+            FROM Resource r
+            JOIN r.tags t
+            WHERE r.class IN :typeList
+              AND t.value IN :tags
+              AND r.name LIKE CONCAT('%', :nameSearch, '%')
+            GROUP BY r
+            HAVING COUNT(DISTINCT t.value) = :tagCount
+            ORDER BY r.name
+            """)
     Page<Resource> findAllByTypesAndAllTags(
             @Param("typeList") List<Class<? extends Resource>> typeList,
             @Param("tags") String[] tags,
             @Param("tagCount") long tagCount,
+            @Param("nameSearch") String nameSearch,
             Pageable pageable);
+
 
     @Query(
             """
-    SELECT r
-    FROM Resource r
-    WHERE TYPE(r) = :type
-    AND LOWER(r.name) LIKE LOWER(CONCAT('%', :name, '%'))
-    """)
+                    SELECT r
+                    FROM Resource r
+                    WHERE TYPE(r) = :type
+                    AND LOWER(r.name) LIKE LOWER(CONCAT('%', :name, '%'))
+                    """)
     List<Resource> findByTypeAndNameContainingIgnoreCase(
             @Param("type") Class<? extends Resource> type, @Param("name") String name);
 }
