63fd1e5ced543acc38b0a102839db3e81c7edcef
ganning127 <ganningxu@gmail.com>
2025-06-19 23:14:49 -0700

    spotless

--- modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/controller/LikesController.java
+++ modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/controller/LikesController.java
@@ -1,7 +1,27 @@
+/**
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements. See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership. The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 package org.apache.airavata.research.service.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import org.apache.airavata.research.service.handlers.ResourceHandler;
 import org.apache.airavata.research.service.model.entity.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +31,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rf/likes")
@@ -45,5 +63,4 @@ public class LikesController {
     public ResponseEntity<List<Resource>> getAllLikedResources(@PathVariable(value = "userId") String id) {
         return ResponseEntity.ok(resourceHandler.getAllLikedResources(id));
     }
-
 }
