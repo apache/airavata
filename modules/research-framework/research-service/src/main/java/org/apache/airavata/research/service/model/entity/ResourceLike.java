2ff5f31909da65114fb42adcab2b4a2f542784d1
ganning127 <ganningxu@gmail.com>
2025-06-19 23:14:56 -0700

    spotless

--- modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/model/entity/ResourceLike.java
+++ modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/model/entity/ResourceLike.java
@@ -1,3 +1,22 @@
+/**
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements. See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership. The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 package org.apache.airavata.research.service.model.entity;
 
 import jakarta.persistence.Column;
@@ -20,7 +39,6 @@ public class ResourceLike {
 
     @Column(name = "owner_id", nullable = false)
     private String ownerId;
-
 
     @ManyToOne(optional = false, fetch = FetchType.EAGER)
     @JoinColumn(name = "resource_id", nullable = false)
@@ -49,5 +67,4 @@ public class ResourceLike {
     public void setId(String id) {
         this.id = id;
     }
-
 }
