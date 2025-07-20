dbdd42c2a1ec07eaa80ada794368b23084b4cfd5
Ganning Xu <ganningxu@gmail.com>
2025-07-19 21:08:05 -0700

    Public private resources (#535)
    * backend support for public/private resources
    
    * spotless
    
    * fix bug with local threads maintaining state across requests

--- modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/config/AuthzTokenFilter.java
+++ modules/research-framework/research-service/src/main/java/org/apache/airavata/research/service/config/AuthzTokenFilter.java
@@ -69,47 +69,46 @@ public class AuthzTokenFilter extends OncePerRequestFilter {
                 || path.startsWith("/v3/api-docs")
                 || path.startsWith("/swagger-ui")
                 || path.startsWith("/swagger-resources")
-                || path.startsWith("/webjars/")
-                || path.startsWith("/api/v1/rf/resources/public");
+                || path.startsWith("/webjars/");
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
-        String authorizationHeader = request.getHeader("Authorization");
-        String xClaimsHeader = request.getHeader("X-Claims");
-
-        if (request.getMethod().equals("OPTIONS")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ") || xClaimsHeader == null) {
-            LOGGER.error("Missing or invalid Authorization header");
-            return;
-        }
-
         try {
-            String accessToken = authorizationHeader.substring(7); // Remove "Bearer " prefix
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, String> claimsMap = objectMapper.readValue(xClaimsHeader, new TypeReference<>() {});
+            String authorizationHeader = request.getHeader("Authorization");
+            String xClaimsHeader = request.getHeader("X-Claims");
 
-            AuthzToken authzToken = new AuthzToken();
-            authzToken.setAccessToken(accessToken);
-            authzToken.setClaimsMap(claimsMap);
-            UserContext.setAuthzToken(authzToken);
+            if (request.getMethod().equals("OPTIONS")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
-            UserProfile userProfile = airavataService.getUserProfile(
-                    authzToken, getClaim(authzToken, USERNAME_CLAIM), getClaim(authzToken, GATEWAY_CLAIM));
-            UserContext.setUser(userProfile);
-        } catch (Exception e) {
-            LOGGER.error("Invalid authorization data", e);
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid authorization data");
-            return;
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ") && xClaimsHeader != null) {
+                try {
+                    String accessToken = authorizationHeader.substring(7); // Remove "Bearer " prefix
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    Map<String, String> claimsMap = objectMapper.readValue(xClaimsHeader, new TypeReference<>() {});
+
+                    AuthzToken authzToken = new AuthzToken();
+                    authzToken.setAccessToken(accessToken);
+                    authzToken.setClaimsMap(claimsMap);
+                    UserContext.setAuthzToken(authzToken);
+
+                    UserProfile userProfile = airavataService.getUserProfile(
+                            authzToken, getClaim(authzToken, USERNAME_CLAIM), getClaim(authzToken, GATEWAY_CLAIM));
+                    UserContext.setUser(userProfile);
+                } catch (Exception e) {
+                    LOGGER.error("Invalid authorization data", e);
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid authorization data");
+                    return;
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        } finally {
+            UserContext.clear();
         }
-
-        filterChain.doFilter(request, response);
     }
 
     private static String getClaim(AuthzToken authzToken, String claimId) {
