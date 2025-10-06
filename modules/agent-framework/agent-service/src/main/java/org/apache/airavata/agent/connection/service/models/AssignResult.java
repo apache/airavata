a31c44e6d288982c656eb759a684246f5de0fe55
lahiruj <lahirujayathilake@gmail.com>
2025-10-05 21:52:27 -0400

    agent pull batch job unit assignment implementation

---
+++ modules/agent-framework/agent-service/src/main/java/org/apache/airavata/agent/connection/service/models/AssignResult.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.airavata.agent.connection.service.models;
+
+public sealed interface AssignResult permits AssignResult.Assigned, AssignResult.NoWork {
+
+    record Assigned(String jobUnitId, String resolvedCommand) implements AssignResult {
+    }
+
+    record NoWork(String reason) implements AssignResult {
+        public static final String EMPTY = "EMPTY";
+        public static final String EMPTY_ALL_DONE = "EMPTY_ALL_DONE";
+        public static final String NO_ASSIGNMENT = "NO_ASSIGNMENT";
+    }
+
+    static Assigned assigned(String id, String cmd) {
+        return new Assigned(id, cmd);
+    }
+
+    static NoWork empty() {
+        return new NoWork(NoWork.EMPTY);
+    }
+
+    static NoWork emptyAllDone() {
+        return new NoWork(NoWork.EMPTY_ALL_DONE);
+    }
+
+    static NoWork noAssignment() {
+        return new NoWork(NoWork.NO_ASSIGNMENT);
+    }
+}
