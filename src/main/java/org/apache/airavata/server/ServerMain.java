3ffd96e85837deb1c634d47e0cef926ad4be2e3d
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2026-07-23 00:03:49 -0400

    Adding main

---
+++ src/main/java/org/apache/airavata/server/ServerMain.java
@@ -0,0 +1,8 @@
+package org.apache.airavata.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("Starting Airavata Server...");
+        // Initialize and start the server components here
+    }
+}
